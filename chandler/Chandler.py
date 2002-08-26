4d7646139b042de2fb6b622045087653fbe710b7
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2002-08-26 14:24:05 +0000

    Initial revision
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/Chandler.py
@@ -0,0 +1,61 @@
+#!/bin/env python
+#----------------------------------------------------------------------------
+# Name:         Chandler.py
+# Author:       Open Source Application Foundation
+# Created:      XX/XX/XX
+# Copyright:    
+#----------------------------------------------------------------------------
+
+from wxPython.wx import *
+from wxPython.xrc import *
+
+class MyFrame(wxFrame):
+    def __init__(self, parent, id, title,
+        pos = wxPyDefaultPosition, size = wxPyDefaultSize,
+        style = wxDEFAULT_FRAME_STYLE ):
+        wxFrame.__init__(self, parent, id, title, pos, size, style)
+
+        resources = wxXmlResource ("resources/resources.xrc")
+        self.SetMenuBar (resources.LoadMenuBar ("MenuBar"))
+        self.SetToolBar (resources.LoadToolBar (self, "ToolBar"))
+
+        self.CreateStatusBar(1)
+        self.SetStatusText("Welcome!")
+        
+        # insert main window here
+        
+        # WDR: handler declarations for MyFrame
+        EVT_MENU(self, wxID_ABOUT, self.OnAbout)
+        EVT_MENU(self, XRCID ("menu_quit"), self.OnQuit)
+        EVT_CLOSE(self, self.OnCloseWindow)
+
+    def OnAbout(self, event):
+        dialog = wxMessageDialog(self, "Welcome to Chandler 0.01\n(C)opyright OSAF",
+            "About Chandler", wxOK|wxICON_INFORMATION )
+        dialog.CentreOnParent()
+        dialog.ShowModal()
+        dialog.Destroy()
+    
+    def OnQuit(self, event):
+        self.Close(true)
+    
+    def OnCloseWindow(self, event):
+        self.Destroy()
+    
+
+#----------------------------------------------------------------------------
+
+class MyApp(wxApp):
+    
+    def OnInit(self):
+        wxInitAllImageHandlers()
+        frame = MyFrame(None, -1, "Chandler", wxPoint(20,20), wxSize(500,340) )
+        frame.Show(true)
+        
+        return true
+
+#----------------------------------------------------------------------------
+
+app = MyApp(1)
+app.MainLoop()
+
---
+++ chandler/README-OSAF.txt
@@ -0,0 +1,10 @@
+Here are some brief instructions for running Chandler on Windows:
+
+I'd suggest download and installing WingIDE (http://www.WingIDE.com). You can try it out for 30 days free. Free licenses are also available for open source projects. Let me know if you want a license and I'll get one or you. You should also check out the WingIDE project and copy the new debugger I built with the version 7 Microsoft compiler: "WingIDE\bin\2.2.1\src\debug\server\dbgtracer22.pyd" and "WingIDE\bin\2.2.1\src\debug\server\dbgtracer22_d.pyd" over the top of the files with the same name in the WingIDE installation directories.
+
+If you have trouble learning WingIDE, give me a call and I'll guide you through the initial steps.
+
+E-mail me any problems you encounter so I can fix them.
+
+John Anderson
+john@osafoundation.org
