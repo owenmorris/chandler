926faba855211fdb5f1645a318bd2ef3d735c444
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2002-11-15 23:46:39 +0000

    Now using Exceptions
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@24 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/__hardhat__.py
+++ chandler/__hardhat__.py
@@ -1,21 +1,50 @@
 import os, hardhatlib
 
-manifest = {
-	    'name':'Chandler',
-	   }
+info = {
+        'name':'Chandler',
+       }
 
-def init(buildenv):
-    # print manifest['name']
-    # print buildenv['root']
-    return 1
+dependencies = (
+                'Python-2.2.2', 
+		'distutils',
+		'wxWindows',
+		'egenix-mx-base-2.0.4',
+		'ZODB4'
+	       )
+
 
 def build(buildenv):
-    return 0
+    hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE, info['name'], 
+     "Nothing to build")
+
+
+def clean(buildenv):
+    hardhatlib.log(buildenv, hardhatlib.HARDHAT_WARNING, info['name'], 
+     "Clean hasn't been set up yet")
+
 
 def run(buildenv):
     
-    path = buildenv['root'] + os.sep + 'debug' + os.sep + 'bin' + os.pathsep + os.environ['path']
-    path = buildenv['root'] + os.sep + 'release' + os.sep + 'bin' + os.pathsep + path
+    if buildenv['version'] == 'debug':
+        path = buildenv['root'] + os.sep + 'debug' + os.sep + 'bin' + 
+         os.pathsep + os.environ['path']
+        python = buildenv['python_d']
+
+
+    if buildenv['version'] == 'release':
+        path = buildenv['root'] + os.sep + 'release' + os.sep + 'bin' + 
+         os.pathsep + os.environ['path']
+        python = buildenv['python']
+
+
+    hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE, info['name'], 
+     "Setting path to " + path)
+    hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE, info['name'], 
+     "Using " + python)
     os.putenv('path', path)
-    exit_code = os.spawnl(os.P_WAIT, buildenv['python'], buildenv['python'], "Chandler.py")
-    return exit_code
+    exit_code = os.spawnl(os.P_WAIT, python, python, "Chandler.py")
+
+    if exit_code != 0:
+        hardhatlib.log(buildenv, hardhatlib.HARDHAT_ERROR, info['name'], 
+         "Chandler exited with code = " + str(exit_code))
+        raise hardhatlib.HardHatError
