68627c98e65cc36ee48b84ef12b8b6b75ce47be9
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2002-11-22 02:24:35 +0000

    Set LD_LIBRARY_PATH on unix
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@30 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/__hardhat__.py
+++ chandler/__hardhat__.py
@@ -27,12 +27,12 @@ def clean(buildenv):
 def run(buildenv):
     
     if buildenv['version'] == 'debug':
-        path = buildenv['root'] + os.sep + 'debug' + os.sep + 'bin' + os.pathsep + os.environ['path']
+        path = buildenv['root'] + os.sep + 'debug' + os.sep + 'bin' + os.pathsep + buildenv['path']
         python = buildenv['python_d']
 
 
     if buildenv['version'] == 'release':
-        path = buildenv['root'] + os.sep + 'release' + os.sep + 'bin' + os.pathsep + os.environ['path']
+        path = buildenv['root'] + os.sep + 'release' + os.sep + 'bin' + os.pathsep + buildenv['path']
         python = buildenv['python']
 
 
@@ -41,6 +41,20 @@ def run(buildenv):
     hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE, info['name'], 
      "Using " + python)
     os.putenv('path', path)
+
+
+    if buildenv['os'] == 'posix':
+	ld_library_path = os.environ['LD_LIBRARY_PATH']
+	if buildenv['version'] == 'debug':
+	    additional_path = buildenv['root'] + os.sep + 'debug' + os.sep + 'lib'
+	else:
+	    additional_path = buildenv['root'] + os.sep + 'release' + os.sep + 'lib'
+	ld_library_path = additional_path + os.pathsep + ld_library_path
+	hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE, info['name'], 
+	 "Setting LD_LIBRARY_PATH to " + ld_library_path)
+	os.putenv('LD_LIBRARY_PATH', ld_library_path)
+	
+
     exit_code = os.spawnl(os.P_WAIT, python, python, "Chandler.py")
 
     if exit_code != 0:
