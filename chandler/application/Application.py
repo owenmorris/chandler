f3105373a74e8a720c21382616f86917e9c28d15
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-02-13 22:55:37 +0000

    Bumped version because of incompatible object store
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@82 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Application.py
+++ chandler/application/Application.py
@@ -28,7 +28,7 @@ class Application(Persistent):
     wxApplication (see below). Notice that we derive it from Perisistent
     so that it is automatically saved across successive application executions
     """
-    VERSION = 3
+    VERSION = 4
     """
        PARCEL_IMPORT defines the import directory containing parcels
     relative to chandlerDirectory where os separators are replaced
