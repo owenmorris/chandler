b97d2b55def4f229c3cc8ac7395b2cc1680a1c6b
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-02-18 21:10:19 +0000

    Adding new distribution-building files
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@96 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/distrib/osx/Chandler.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+DYLD_LIBRARY_PATH=./lib DYLD_FRAMEWORK_PATH=./bin/Frameworks exec ./bin/Applications/Python.app/Contents/MacOS/python Chandler.py
---
+++ chandler/distrib/win/Chandler.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+set PATH=.\bin
+.\bin\python Chandler.py
