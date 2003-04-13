56bc8072a4e2e575a0e92a78d180f3b5bba52773
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-04-13 23:26:17 +0000

    Added comments to AttributeTemplate
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@365 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/repository/AttributeTemplate.py
+++ chandler/application/repository/AttributeTemplate.py
@@ -1,12 +1,17 @@
 """ AttributeTemplate, meta-information about an attribute.
+
+    AttributeTemplates are a Thing, and could have a
+    KindOfThing associated with them. For bootstrapping
+    purposes, we're skipping this step for now.
+    
+    @@@ This module is still in the "elaboration" phase, 
+    a draft to explore current thinking.
 """
 
 __revision__ = "$Revision$"
 __date__ = "$Date$"
 __copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
 __license__ = "OSAF"
-
-
 
 from application.repository.Thing import Thing
 from application.repository.Namespace import chandler
@@ -16,34 +21,78 @@ class AttributeTemplate(Thing):
         Thing.__init__(self, dict)
         
     def GetCardinality(self):
+        """ Gets the cardinality for this attribute.
+            1 => Only one value expected
+            None => Multiple values allowed/expected
+        """
         return self[chandler.cardinality]
 
     def SetCardinality(self, cardinality):
+        """ Sets the cardinality for this attribute.
+            1 => Only one value expected
+            None => Multiple values allowed/expected
+        """
         self[chandler.cardinality] = cardinality
     
     def GetRange(self):
+        """ Gets the range for this attribute, values this
+            attribute is allowed to have.
+            'string' => python string
+            'datetime' => mx.DateTime
+            [] => simple enumeration, a list of strings
+        """
         return self[chandler.range]
 
     def SetRange(self, range):
+        """ Sets the range for this attribute, values this
+            attribute is allowed to have.
+            'string' => python string
+            'datetime' => mx.DateTime
+            [] => simple enumeration, a list of strings
+        """        
         self[chandler.range] = range
     
     def GetRequired(self):
+        """ Returns a boolean.
+            True => attribute is required for the Thing
+            False => attribute is not required
+        """
         return self[chandler.required]
     
     def SetRequired(self, required):
+        """ Expects a boolean.
+            True => attribute is required for the Thing
+            False => attribute is not required
+        """
         self[chandler.required] = required
     
     def GetDefault(self):
+        """ Gets the default value, should be
+            consistent with range and cardinality.
+        """
         return self[chandler.default]
     
     def SetDefault(self, default):
+        """ Sets the default value, should be
+            consistent with range and cardinality.
+        """
         self[chandler.default] = default
         
     def GetDisplayName(self):
+        """ Gets the display name, a string that can
+            be used in a parcel viewer or other ui.
+        """
         return self[chandler.displayName]
     
     def SetDisplayName(self, value):
+        """ Sets the display name, a string that can
+            be used in a parcel viewer or other ui.
+        """
         self[chandler.displayName] = value
         
     def IsValid(self, value):
+        """ Use cardinality, range, required, etc. to
+            validate 'value' for this attribute.
+            @@@ Not implemented, default to True
+        """
         return True
