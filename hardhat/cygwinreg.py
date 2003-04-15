4ff9501aaa6f19294afb475d7c87992a781784f1
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-04-15 05:44:45 +0000

    A module to help os_win access the registry under cygwin.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@376 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ hardhat/cygwinreg.py
@@ -0,0 +1,49 @@
+#!/bin/env python
+
+import os, os.path, string
+
+HKEY_CLASSES_ROOT = "HKEY_CLASSES_ROOT"
+HKEY_LOCAL_MACHINE = "HKEY_LOCAL_MACHINE"
+HKEY_CURRENT_USER = "HKEY_CURRENT_USER"
+HKEY_USERS = "HKEY_USERS"
+
+_handles = []
+
+def checkForRegistry():
+    return os.path.exists("/proc/registry")
+
+
+def RegOpenKeyEx(base, key):
+    regpath = string.join(["/proc/registry", base, key], "/")
+    regpath = string.join(string.split(regpath, "\\"), "/")
+    if os.path.exists(regpath):
+	_handles.append(regpath)
+	return len(_handles)-1
+    raise RegError
+
+def RegEnumKey(handle, index):
+    keys = []
+    items = os.listdir(_handles[handle])
+    for item in items:
+	if os.path.isdir(_handles[handle]+"/"+item):
+	    keys.append(item)
+    keys.sort()
+    return keys[index]
+
+def RegEnumValue(handle, index):
+    values = []
+    items = os.listdir(_handles[handle])
+    for item in items:
+	if os.path.isfile(_handles[handle]+"/"+item):
+	    values.append(item)
+    values.sort()
+    if index >= len(values):
+	raise RegError
+    value = values[index]
+    valuefile = file(_handles[handle] + "/" + value, "r")
+    buf = valuefile.readline()
+    valuefile.close()
+    return ( value, buf, 1 )
+
+class RegError(Exception):
+    pass
