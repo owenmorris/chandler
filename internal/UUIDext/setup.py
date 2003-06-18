d7b821540bb425a0d8c87a4c1edc4726e5063ca7
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-06-18 22:57:12 +0000

    Wrapped the code in a "if __name__ == "__main__"
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@580 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/UUIDext/setup.py
+++ internal/UUIDext/setup.py
@@ -1,4 +1,8 @@
 
-from distutils.core import setup, Extension
-setup(name='UUIDext', ext_modules=[Extension('UUIDext', sources=['uuid.c',
-                                                                 'pyuuid.c'])])
+def main():
+    from distutils.core import setup, Extension
+    setup(name='UUIDext', ext_modules=[Extension('UUIDext', sources=['uuid.c',
+     'pyuuid.c'])])
+
+if __name__ == "__main__":
+    main()
