225abc311152b2d9a228fe26467297c2d2c119b6
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-06-18 20:36:24 +0000

    Wrapped everything in a main() to appease PyChecker
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@575 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/repository/simple_test.py
+++ chandler/application/repository/simple_test.py
@@ -8,51 +8,54 @@ __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 import unittest, sys, os, gettext
 
-gettext.install('Chandler')
+def main():
+    gettext.install('Chandler')
 
-# sys.path.append(os.environ['OSAFROOT'] + "\\Chandler")
+    # sys.path.append(os.environ['OSAFROOT'] + "\\Chandler")
 
-from application.repository.Repository import Repository
-from application.repository.Thing import Thing
-from application.repository.KindOfThing import KindOfThing
-from application.repository.AttributeTemplate import AttributeTemplate
-from application.repository.Namespace import chandler
+    from application.repository.Repository import Repository
+    from application.repository.Thing import Thing
+    from application.repository.KindOfThing import KindOfThing
+    from application.repository.AttributeTemplate import AttributeTemplate
+    from application.repository.Namespace import chandler
 
-from application.repository.Item import Item
-from application.repository.Event import Event
-from application.repository.Contact import Contact
-from application.repository.ContactMethod import ContactMethod
-from application.repository.ContactName import ContactName
+    from application.repository.Item import Item
+    from application.repository.Event import Event
+    from application.repository.Contact import Contact
+    from application.repository.ContactMethod import ContactMethod
+    from application.repository.ContactName import ContactName
 
-from mx import DateTime
+    from mx import DateTime
 
-repository = Repository()
-#repository.PrintTriples()
+    repository = Repository()
+    #repository.PrintTriples()
 
-event = Event()
-event.startTime = DateTime.today()
-event.endTime = DateTime.now()
-event.headline = 'Games night'
-repository.AddThing(event)
+    event = Event()
+    event.startTime = DateTime.today()
+    event.endTime = DateTime.now()
+    event.headline = 'Games night'
+    repository.AddThing(event)
 
-print "Duration: " + str(event.duration)
+    print "Duration: " + str(event.duration)
 
-contact = Contact('Person')
-contactName = ContactName(contact)
-contact.SetAttribute(chandler.contactName, contactName)
-repository.AddThing(contact)
+    contact = Contact('Person')
+    contactName = ContactName(contact)
+    contact.SetAttribute(chandler.contactName, contactName)
+    repository.AddThing(contact)
 
-repository.Commit()
-repository.PrintTriples()
+    repository.Commit()
+    repository.PrintTriples()
 
-contactName.SetAttribute(chandler.firstname, "Sally")
-lastName = contactName.GetAttribute(chandler.lastname)
+    contactName.SetAttribute(chandler.firstname, "Sally")
+    lastName = contactName.GetAttribute(chandler.lastname)
 
-print "Last Name: " + str(lastName)
+    print "Last Name: " + str(lastName)
 
-contact.PrintTriples()
-contactName.PrintTriples()
+    contact.PrintTriples()
+    contactName.PrintTriples()
 
+if __name__ == "__main__":
+    main()
 
 
 
