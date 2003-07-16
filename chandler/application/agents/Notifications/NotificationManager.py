52fbe4d30735b1eb5af709e57747907a945b54b4
andy <andy@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-07-16 01:19:15 +0000

    corrected copyright date from 2002 to 2003
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@693 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/agents/Notifications/NotificationManager.py
+++ chandler/application/agents/Notifications/NotificationManager.py
@@ -1,6 +1,6 @@
 __version__ = "$Revision$"
 __date__ = "$Date$"
-__copyright__ = "Copyright (c) 2002 Open Source Applications Foundation"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
 __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 """
