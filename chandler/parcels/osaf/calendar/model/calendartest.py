ec72f3d0629360fbf8950e99f93886c075aceae9
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-07-24 19:55:09 +0000

    tweaks to calendar data model unittest
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@727 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/calendar/model/calendartest.py
+++ chandler/parcels/osaf/calendar/model/calendartest.py
@@ -11,39 +11,47 @@ from OSAF.calendar.model.CalendarEvent import CalendarEventFactory
 from mx import DateTime
 
 class SimpleTest(unittest.TestCase):
-    """Clean repository tests. Creates a fresh repository, does not save."""
+    """Simple calendar data parcel tests"""
 
     def setUp(self):
-        self.rep = FileRepository('data')
+        """Creates a new repository and a factory. Loads the schema and calendar"""
+        self.rep = FileRepository('test')
         self.rep.loadPack('model/packs/schema.pack')
         self.rep.loadPack('parcels/OSAF/calendar/model/calendar.pack')
         self.factory = CalendarEventFactory(self.rep)
     
     def testFactoryBasics(self):
-        """ Just testing simple behavior, should see no exceptions"""
+        """Test that the basic factory method worked"""
         item = self.factory.NewItem()
         item.CalendarStartTime
         item.CalendarEndTime
+        foundItem = self.rep.find(item.getPath())
+        self.assert_(foundItem)
+        self.assertEqual(foundItem.getUUID(), item.getUUID())
 
     def testEventBasics(self):
+        """Test basic features of CalendarEvent class"""
         item = self.factory.NewItem()
         item.setAttribute("CalendarHeadline", "Test Event")
         self.assertEqual(item.CalendarHeadline, "Test Event")
         self.assertEqual(item.IsRemote(), False)
 
     def testGetDuration(self):
+        """Test the duration property, GET"""
         item = self.factory.NewItem()
         item.CalendarStartTime = DateTime.DateTime(2003, 1, 1, 10)
         item.CalendarEndTime = DateTime.DateTime(2003, 1, 1, 11, 30)
         self.assertEqual(item.CalendarDuration, DateTime.DateTimeDelta(0, 1.5))
 
     def testSetDuration(self):
+        """Test the duration property, SET"""
         item = self.factory.NewItem()
         item.CalendarStartTime = DateTime.DateTime(2003, 2, 2, 10)
         item.CalendarDuration = DateTime.DateTimeDelta(0, 1.5)
         self.assertEqual(item.CalendarEndTime, DateTime.DateTime(2003, 2, 2, 11, 30))
 
     def testChangeStartTime(self):
+        """Test ChangeStartTime"""
         item = self.factory.NewItem()
         item.CalendarStartTime = DateTime.DateTime(2003, 3, 3, 10)
         item.CalendarEndTime = DateTime.DateTime(2003, 3, 3, 11, 30)
@@ -53,6 +61,7 @@ class SimpleTest(unittest.TestCase):
         self.assertEqual(item.CalendarStartTime, DateTime.DateTime(2003, 3, 4, 12, 45))
 
     def tearDown(self):
+        # Note: to use for diagnosis if a test fails
         self.rep.save()
 
 if __name__ == "__main__":
