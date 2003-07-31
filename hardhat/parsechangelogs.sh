9d3845b8cbda21265437d8ca3d334742e6544977
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-07-31 20:23:38 +0000

    Now takes only one parameter:  a date to begin looking for changes
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@754 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- hardhat/parsechangelogs.sh
+++ hardhat/parsechangelogs.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-# two arguments:  the starting and ending CVS tags that bookend the date range
-# you are interested in (e.g., parsechangelogs.sh CHANDLER_M_01 CHANDLER_M_02)
-
 for file in `find . -name ChangeLog.txt`
 do
     echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
     echo Changes from $file
-    cvs diff -r $1 -r $2 -Bbc $file | grep "^\+" | cut --bytes=2-
+    cvs diff -D $1 -D now -Bbc $file | grep "^\+" | cut --bytes=2-
 done
