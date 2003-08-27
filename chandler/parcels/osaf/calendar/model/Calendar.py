f5f65484d766eb8b9452525e8f8f5696d4b6f03c
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-08-27 01:47:23 +0000

    Added new format for xml loading, added Calendar data object
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@907 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/parcels/osaf/calendar/model/Calendar.py
@@ -0,0 +1,28 @@
+""" Class used for Items of Kind Calendar
+"""
+
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+from model.item.Item import Item
+from mx import DateTime
+
+class CalendarFactory:
+    def __init__(self, rep):
+        self._container = rep.find("//Calendar")
+        self._kind = rep.find("//Schema/CalendarSchema/Calendar")
+        
+    def NewItem(self, name=""):
+        item = Location(None, self._container, self._kind)
+        item.setAttribute("name", name)
+
+        return item
+
+class Calendar(Item):
+    def __init__(self, name, parent, kind, **_kwds):
+        super(Location, self).__init__(name, parent, kind, **_kwds)
+
+    def IsRemote(self):
+        return False
