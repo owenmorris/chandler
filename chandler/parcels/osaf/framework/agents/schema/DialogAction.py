c862dd7ca96b8b6575974d2820aa686ba35bfb86
andy <andy@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-09-05 16:36:43 +0000

    oops, I forgot to check this new file in last night
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1068 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/parcels/osaf/framework/agents/schema/DialogAction.py
@@ -0,0 +1,44 @@
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+from wxPython.wx import *
+
+from application.agents.model.Action import Action
+
+"""
+The  DialogAction class is a subclass of Action that displays a dialog using text derived from
+a template and the passed in data object
+"""
+class DialogAction(Action):
+
+    # run this action synchronously with wxWindows
+    def UseWxThread(self):
+        return True
+
+    def _SubstituteAttributes(self, template, dataDict):
+        """
+          this string substitution utility loops for each attribute in the data dictionary,
+          substituting it in the template with it's value
+        """
+        result = template.replace("\\n", '\n')
+        for attribute in dataDict.keys():
+            value = str(dataDict[attribute])
+            pattern = '[' + str(attribute) + ']'
+            result = result.replace(pattern, value)
+            
+        return result
+    
+    def Execute(self, agent, data):
+        """
+           Use wxWindows to display a dialog, with text derived from a template specified by the action,
+           and data from the data parameter.   The data parameter is a dictionary associating values with keys
+        """
+        template = self.actionValue
+        message = self._SubstituteAttributes(template, data)
+        
+        wxMessageBox(message)
+
+        return True
+    
