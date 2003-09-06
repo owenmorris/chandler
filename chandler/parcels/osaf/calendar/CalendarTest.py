e9520e85761b3e19b39e41d812cbbd4cf449148a
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-09-06 06:19:02 +0000

    Forgot to add file
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1083 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/parcels/osaf/calendar/CalendarTest.py
@@ -0,0 +1,41 @@
+"""
+The CalendarTest class contains methods to help testing/debugging, including a method to generate sample events.
+"""
+
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import random
+
+from mx import DateTime
+
+from OSAF.calendar.model.CalendarEvent import CalendarEvent
+from OSAF.calendar.model.CalendarEvent import CalendarEventFactory
+
+HEADLINES = ["Dinner", "Lunch", "Meeting", "Movie", "Games"]
+
+DURATIONS = [30, 60, 90, 120, 150, 180]
+
+class CalendarTest:
+    def __init__(self, repository):
+        self.factory = CalendarEventFactory(repository)
+
+    def generateEvent(self, days):
+        event = self.factory.NewItem()
+        event.headline = random.choice(HEADLINES)
+
+        # Choose random days, hours
+        startDelta = DateTime.DateTimeDelta(random.randint(0, days),
+                                            random.randint(0, 24))
+        
+        event.startTime = DateTime.now() + startDelta
+
+        # Choose random minutes
+        event.duration = DateTime.DateTimeDelta(0, 0, random.choice(DURATIONS))
+
+    def generateEvents(self, count, days):
+        """ Generate _count_ events over the next _days_ number of days """
+        for index in range(count):
+            self.generateEvent(days)
