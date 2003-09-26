4f2a6270f3381fae41f4a803ce0102ac00591895
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-09-26 22:00:04 +0000

    Added Web Services sample parcel.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1174 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/parcels/osaf/stockquote/StockQuote.py
@@ -0,0 +1,30 @@
+__author__ = "Heikki Toivonen"
+__version__ = "$Revision$"
+__date__ = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__ = "OSAF"
+
+from application.Application import app
+from application.ViewerParcel import *
+from SOAPpy import WSDL
+
+class StockQuoteModel(ViewerParcel): 
+    def __init__(self): 
+        ViewerParcel.__init__(self)
+                
+class wxStockQuoteViewer(wxViewerParcel): 
+    def OnInit(self):
+        self.quoteLabel = XRCCTRL(self, "QuoteLabel")
+        self.stockSymbol = XRCCTRL(self, "StockSymbol")
+        EVT_MENU(self, XRCID('GetQuoteMenuItem'), self.OnGetQuote)
+        EVT_BUTTON(self, XRCID('GetQuoteButton'), self.OnGetQuote)
+
+    def OnGetQuote(self, event):
+        if not hasattr (self, "proxy"):
+            self.proxy = WSDL.Proxy('http://services.xmethods.net/soap/urn:xmethods-delayed-quotes.wsdl')
+        stockSymbol = self.stockSymbol.GetValue()
+        self.quoteLabel.Clear() # Allows user to notice something happened
+        self.quoteLabel.SetLabel('$' + str(self.proxy.getQuote(stockSymbol)))
+
+
+    
---
+++ chandler/parcels/osaf/stockquote/__init__.py
@@ -0,0 +1,1 @@
+parcelClass = "StockQuote.StockQuoteModel"
