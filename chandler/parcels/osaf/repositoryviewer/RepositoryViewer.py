464097a3941694ddd1a5ef4b8bc01b6addc03b23
skinner <skinner@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-10-17 01:08:29 +0000

    applied patch from Geoff Gerrietts to add relationship links in repository viewer detail view
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1221 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/repositoryviewer/RepositoryViewer.py
+++ chandler/parcels/osaf/repositoryviewer/RepositoryViewer.py
@@ -17,12 +17,95 @@ from application.ViewerParcel import wxViewerParcel
 
 from application.Application import app
 from model.util.Path import Path
+from model.item.Item import Item
 import os
 from application.SplashScreen import SplashScreen
 
 class RepositoryViewer(ViewerParcel):
     def __init__(self):
         ViewerParcel.__init__(self)
+
+class wxRepositoryViewerDetail(wxHtmlWindow):
+
+    def OnLinkClicked(self, wx_linkinfo):
+        uri = wx_linkinfo.GetHref()
+        an_item = app.repository.getRoots()[0]
+        item = an_item.find(uri)
+        self.DisplayItem(item)
+
+    def _formatReference(self, ref):
+        """ formats the a reference attribute to be clickable, etcetera
+        """
+        url = ref.getItemPath()
+        if ref.hasAttributeValue('kind'):
+            kind = ref.kind.getItemName()
+        else:
+            kind = "(Kind not found)"
+        # Originally I was masking the fallback to itemName here just like in
+        # the listview, but that doesn't work for many of the more primitive
+        # repository items, so I stopped doing that.
+        dn = str(ref.getItemDisplayName())
+
+        # Escape < and > for HTML display
+        kind = kind.replace("<", "&lt;").replace(">", "&gt;")
+        dn = dn.replace("<", "&lt;").replace(">", "&gt;")
+
+        return "<a href=\"%(url)s\">%(kind)s: %(dn)s</a>" % locals()
+
+    def DisplayItem(self, item):
+        """Display the given Item's details in an HTML window.
+        """
+        displayName = item.getItemDisplayName()
+                
+        if item.hasAttributeValue('kind'):
+            kind = item.kind.getItemName()
+        else:
+            kind = "Kind not found"
+        
+        htmlString = "<html><body><h5>%s: %s</h5><ul>" % (kind, displayName)
+        htmlString = htmlString + "<li><b>Path:</b> %s" % item.getItemPath()
+        htmlString = htmlString + "<li><b>UUID:</b> %s" % item.getUUID()
+        htmlString = htmlString + "</ul><h5>Attributes</h5><ul>"
+
+        # We build tuples (name, formatted) for all value-only, then
+        # all reference-only. Then we concatenate the two lists and sort
+        # the result, and append that to the htmlString.
+        value_attrs = []
+        for k, v in item.iterAttributes(valuesOnly=True):
+            if isinstance(v, dict):
+                tmpList = ["<li><b>%s:</b></li><ul>" % k]
+                for attr in v:
+                    attrString = str(attr)
+                    attrString = attrString.replace("<", "&lt;")
+                    attrString = attrString.replace(">", "&gt;")
+                    tmpList.append("<li>%s</li>" % attrString)
+                tmpList.append("</ul>")
+                value_attrs.append((k, "".join(tmpList)))
+            else:
+                value = str(v)
+                value = value.replace("<", "&lt;")
+                value = value.replace(">", "&gt;")
+                value_attrs.append((k,"<li><b>%s: </b>%s</li>" % (k, value)))
+
+        ref_attrs = []
+        for k, v in item.iterAttributes(referencesOnly=True):
+            if isinstance(v, dict) or isinstance(v, list):
+                tmpList = ["<li><b>%s:</b></li><ul>" % k]
+                for attr in v:
+                    tmpList.append("<li>%s</li>" % self._formatReference(attr))
+                tmpList.append("</ul>")
+                ref_attrs.append((k, "".join(tmpList)))
+            else:
+                value = self._formatReference(v)
+                ref_attrs.append((k,"<li><b>%s: </b>%s</li>" % (k, value)))
+
+        all_attrs = ref_attrs + value_attrs
+        all_attrs.sort()
+        dyn_html = "".join([y for x, y in all_attrs])
+
+        all_html = "%s%s</ul></body></html>" % (htmlString, dyn_html)
+        
+        self.SetPage(all_html)
 
 class wxRepositoryViewer(wxViewerParcel):
     def OnInit(self):
@@ -54,7 +137,7 @@ class wxRepositoryViewer(wxViewerParcel):
         EVT_TREE_SEL_CHANGED(self, self.treeCtrl.GetId(), self.OnSelChanged)
         
         # Set up detail view
-        self.detail = wxHtmlWindow(self.splitter, -1, 
+        self.detail = wxRepositoryViewerDetail(self.splitter, -1, 
                                    style=wxNO_FULL_REPAINT_ON_RESIZE | wxSUNKEN_BORDER)
         self.detail.SetPage("<html><body><h5>Item Viewer</h5></body></html>")
         self.splitter.SplitHorizontally(self.treeCtrl, self.detail, 200)
@@ -81,30 +164,8 @@ class wxRepositoryViewer(wxViewerParcel):
     def DisplayItem(self, item):
         """Display the given Item's details in an HTML window.
         """
-        htmlString = "<html><body><h5>Item</h5><ul>"
-        htmlString = htmlString + "<li><b>Path:</b> %s" % item.getItemPath()
-        htmlString = htmlString + "<li><b>UUID:</b> %s" % item.getUUID()
-        htmlString = htmlString + "</ul><h5>Attributes</h5><ul>"
-        for attribute in item.iterAttributes():
-            key = attribute[0]
-
-            if isinstance(attribute[1], dict):
-                htmlString = htmlString + ("<li><b>%s:</b></li><ul>" % key)
-                for attr in attribute[1]:
-                    attrString = str(attr)
-                    attrString = attrString.replace("<", "&lt;")
-                    attrString = attrString.replace(">", "&gt;")
-                    htmlString = htmlString + ("<li>%s</li>" % attrString)
-                htmlString = htmlString + ("</ul>")
-            else:
-                value = str(attribute[1])
-                value = value.replace("<", "&lt;")
-                value = value.replace(">", "&gt;")
-                htmlString = htmlString + ("<li><b>%s: </b>%s</li>" % (key, value))
-
-        htmlString = htmlString + "</ul></body></html>"
         
-        self.detail.SetPage(htmlString)
+        self.detail.DisplayItem(item)
         
         
     def LoadTree(self, item=None, path=None, parent=None):
@@ -133,7 +194,9 @@ class wxRepositoryViewer(wxViewerParcel):
         """Populates the tree's table with details of this particular item.
         """
 
-        displayName = item.getItemDisplayName()
+        displayName = str(item.getItemDisplayName())
+        if displayName == str(item.getItemName()):
+            displayName = "(unnamed)"
                 
         if item.hasAttributeValue('kind'):
             kind = item.kind.getItemName()
