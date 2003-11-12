a503ff261b064179e93e855f45147844842e7393
pavlov <pavlov@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-11-12 00:31:02 +0000

    a sample condition class that checks to see if a list of people are available on jabber
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1317 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/application/agents/test_agents/AvailableCondition.py
@@ -0,0 +1,26 @@
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import application.Application
+from application.agents.model.Condition import Condition
+
+PEOPLE_NEEDED = ['pavlov1234@jabber.org']
+class AvailableCondition(Condition):
+
+    def GetNotifications(self):
+        return []
+
+    def IsSatisfied(self, notification):
+        BASE_PATH = '//parcels/application/ChandlerJabber/'
+        repository = application.Application.app.repository
+        repository.commit()
+        for i in PEOPLE_NEEDED:
+            item = repository.find(BASE_PATH + i)
+            if not item:
+                return False
+            if item.type != 'available':
+                return False
+
+        return True
