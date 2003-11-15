e2f876c6f126b43efb3ce91060bdf97f822f843e
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-11-15 22:53:22 +0000

    First appearance of blocks
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1351 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/blocks/CharacterStyle.py
+++
@@ -1,8 +0,0 @@
-
-from repository.item.Item import Item
-
-class CharacterStyle(Item):
-
-    def __init__(self, **args):
-        super (CharacterStyle, self).__init__ (**args)
- 
--- chandler/parcels/osaf/framework/blocks/Style.py
+++
@@ -1,8 +0,0 @@
-
-from repository.item.Item import Item
-
-class Style(Item):
-
-    def __init__(self, **args):
-        super (Style, self).__init__ (**args)
- 
