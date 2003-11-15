8e0e6889bd83c265c9cea6f7cd1b9bbb4d5f960b
pavlov <pavlov@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-11-15 06:32:38 +0000

    Making the code smarter about fetching new items.  General cleanup and addition of RSSFeed kind
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1345 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/examples/zaobao/RSSData.py
+++ chandler/parcels/osaf/examples/zaobao/RSSData.py
@@ -1,60 +1,84 @@
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
 from repository.item.Item import Item
+from repository.util.Path import Path
 from application.Application import app
 import mx.DateTime
+import types
 
 BASE_PATH = '//Parcels/OSAF/examples/zaobao'
 RSSITEM_KIND_PATH = BASE_PATH + '/RSSItem'
 
-class RSSChannel(Item):
-    def Update(self, url, data):
-        # set the url
-        self.setAttributeValue('rssURL', url)
+def SetAttribute(self, data, attr, nattr=None, encoding=None):
+    if not nattr:
+        nattr = attr
+    value = data.get(attr)
+    if value:
+        if encoding:
+            value = unicode(value, encoding)
+        self.setAttributeValue(nattr, value)
 
-        etag = data.get('etag', None)
-        if etag:
-            self.setAttributeValue('etag', etag)
+def SetAttributes(self, data, attributes, encoding):
+    if type(attributes) == types.DictType:
+        for attr, nattr in attributes.items():
+            SetAttribute(self, data, attr, nattr=nattr, encoding=encoding)
+    elif type(attributes) == types.ListType:
+        for attr in attributes:
+            SetAttribute(self, data, attr, encoding=encoding)
 
-        channel = data['channel']
+class RSSFeed(Item):
+    def Update(self, data):
+        chanKind = app.repository.find(BASE_PATH + '/RSSChannel')
+
+        # get the encoding
         encoding = data.get('encoding', 'latin_1')
 
-        # bunch of attributes
-        for attr in ['creator', 'description', 'link', 'title', \
-                     'category', 'language', 'encoding']:
-            value = channel.get(attr, None)
-            if value:
-                self.setAttributeValue(attr, unicode(value, encoding))
+        # set etag
+        SetAttribute(self, data, 'etag')
 
         # set lastModified
-        try:
-            lastModified = mx.DateTime.DateTimeFrom(channel['date'])
-        except KeyError:
-            modifiedDate = data.get('modified')
-            if modifiedDate and len(modifiedDate) > 5:
-                #FIXME, need to store the whole date
-                lastModified = apply(mx.DateTime.DateTime, modifiedDate[0:3])
-            else:
-                lastModified = None
-        if lastModified:
+        modified = data.get('modified')
+        if modified:
+            lastModified = mx.DateTime.mktime(modified)
             self.setAttributeValue('lastModified', lastModified)
+
+        # update the feed's channel
+        if not self.hasAttributeValue('channel'):
+            self.channel = chanKind.newItem(None, self)
+        self.channel.Update(data['channel'], data['items'], encoding)
+
+
+class RSSChannel(Item):
+    def Update(self, data, items, encoding):
+        # fill in the item
+        attrs = {'title':'displayName'}
+        SetAttributes(self, data, attrs, encoding)
+
+        attrs = ['description', 'creator', 'link', 'category', 'language', 'date']
+        SetAttributes(self, data, attrs, encoding)
 
         # make children
         itemKind = app.repository.find(RSSITEM_KIND_PATH)
-        for itemData in data['items']:
+        for itemData in items:
             item = itemKind.newItem(None, self)
             item.Update(itemData, encoding)
             self.addValue('items', item)
 
-        #items = self.getItemsFromParseData(parserData)
-        #self.updateItems(items)
-        #self.setAttributeValue('isUnread',len(items) > 0)
-
 class RSSItem(Item):
     def Update(self, data, encoding):
-        for attr in ['creator', 'description', 'link', 'title', \
-                     'category']:
-            value = data.get(attr, None)
-            if value:
-                self.setAttributeValue(attr, unicode(value, encoding))
+        #for i,v in data.items():
+        #    print i
+        #print '---'
+
+        # fill in the item
+        attrs = {'title':'displayName'}
+        SetAttributes(self, data, attrs, encoding)
+
+        attrs = ['description', 'creator', 'link', 'category', 'date']
+        SetAttributes(self, data, attrs, encoding)
 
         try:
             date = mx.DateTime.DateTimeFrom(data['date'])
--- chandler/parcels/osaf/examples/zaobao/ZaoBaoAgent.py
+++ chandler/parcels/osaf/examples/zaobao/ZaoBaoAgent.py
@@ -1,10 +1,15 @@
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
 from repository.item.Item import Item
 from application.Application import app
 from application.agents.model.Action import Action
 
 import feedparser
 
-_defaultBlogs = [ "http://www.pavlov.net/blog/index.rdf", \
+_defaultBlogs = [ "http://www.pavlov.net/blog/rss10.rdf", \
                   "http://blogs.osafoundation.org/news/index.rdf", \
                   "http://blogs.osafoundation.org/devnews/index.rdf", \
                   "http://blogs.osafoundation.org/zaobao/index.rdf", \
@@ -16,28 +21,36 @@ _defaultBlogs = [ "http://www.pavlov.net/blog/index.rdf", \
                   "http://diveintomark.org/xml/rss.xml"]
 
 BASE_PATH = '//Parcels/OSAF/examples/zaobao'
-RSSCHANNEL_KIND_PATH = BASE_PATH + '/RSSChannel'
-
-DATA_NAME = 'Data'
-DATA_PATH = BASE_PATH + '/' + DATA_NAME
 
 class UpdateAction(Action):
     def Execute(self, agent, notification):
         repository = app.repository
 
-        chanKind = repository.find(RSSCHANNEL_KIND_PATH)
+        for feed in self.__getFeeds():
+            etag = feed.getAttributeValue('etag', default=None)
+            lastModified = feed.getAttributeValue('lastModified', default=None)
+            if lastModified:
+                modified = lastModified.tuple()
+            else:
+                modified = None
+            data = feedparser.parse(feed.link, etag, modified)
+            feed.Update(data)
 
-        parent = repository.find(DATA_PATH)
-        if not parent:
-            parent = Item(DATA_NAME, repository.find(BASE_PATH), None)
+        repository.commit()
+
+    def __getFeeds(self):
+        repository = app.repository
+        feedKind = repository.find(BASE_PATH + '/RSSFeed')
+
+        feeds = []
+        parent = repository.find(BASE_PATH)
 
         for url in _defaultBlogs:
             urlhash = str(hash(url))
+            item = repository.find(BASE_PATH + '/' + urlhash)
+            if not item:
+                item = feedKind.newItem(urlhash, parent)
+                item.link = url
+            feeds.append(item)
 
-            chan = repository.find(DATA_PATH + '/' + urlhash)
-            if not chan:
-                chan = chanKind.newItem(urlhash, parent)
-            data = feedparser.parse(url)
-            chan.Update(url, data)
-
-        repository.commit()
+        return feeds
