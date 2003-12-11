3f078909a644523b236e27b06dbfc0739915a590
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-12-11 22:51:28 +0000

    Testing bonsai
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1501 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/stockquote/StockQuote.py
+++ chandler/parcels/osaf/stockquote/StockQuote.py
@@ -4,8 +4,6 @@ __date__ = "$Date$"
 __copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
 __license__ = "OSAF"
 
-# testing bonsai..
-
 from application.Application import app
 from application.ViewerParcel import *
 from SOAPpy import WSDL
