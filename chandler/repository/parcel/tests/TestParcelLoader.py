5178b9fffcb42b50d9a301c247120385c03ff6f3
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-12-29 21:53:14 +0000

    Added additional checks, and a sub-kind
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1556 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/parcel/tests/TestParcelLoader.py
+++ chandler/repository/parcel/tests/TestParcelLoader.py
@@ -23,18 +23,50 @@ class SimpleParcelLoaderTestCase(ParcelLoaderTestCase.ParcelLoaderTestCase):
         LoadParcels.LoadParcels(parcelDir, self.rep)
         self.rep.commit()
 
+        # Ensure the simple Parcel was created with the right Kind and attrs
         simpleParcel = self.rep.find("//parcels/simple")
         self.assertEqual(simpleParcel.kind,
          self.rep.find('//Schema/Core/Parcel'))
+        self.assertEqual(simpleParcel.displayName, "Simple")
+        self.assertEqual(simpleParcel.description, 
+         "Simple Parcel Loader Test Schema")
+        self.assertEqual(simpleParcel.version, "0.1")
+        self.assertEqual(simpleParcel.author, 
+         "Open Source Applications Foundation")
 
+        # Ensure testAttribute was created with the right Kind and attrs
         testAttribute = self.rep.find("//parcels/simple/TestAttribute")
         self.assertEqual(testAttribute.kind,
          self.rep.find('//Schema/Core/Attribute'))
+        self.assertEqual(testAttribute.type, 
+         self.rep.find('//Schema/Core/String'))
+        self.assertEqual(testAttribute.displayName, "Test Attribute")
+        self.assertEqual(testAttribute.cardinality, "single")
 
+        # Ensure testKind was created with the right Kind and attrs
         testKind = self.rep.find("//parcels/simple/TestKind")
         self.assertEqual(testKind.kind,
          self.rep.find('//Schema/Core/Kind'))
+        self.assertEqual(testKind.displayName, "Test Kind")
+        self.assertEqual(testKind.displayAttribute, "TestAttribute")
 
+        # Ensure testAttribute is an attribute of testKind (and vice-versa)
+        self.assert_(testKind.attributes.has_key(testAttribute.getUUID()))
+        self.assert_(testAttribute.kinds.has_key(testKind.getUUID()))
+
+        # Ensure subKind was created with the right Kind and attrs
+        subKind = self.rep.find("//parcels/simple/SubKind")
+        self.assertEqual(subKind.kind,
+         self.rep.find('//Schema/Core/Kind'))
+        self.assertEqual(subKind.displayName, "Subclass Test Kind")
+
+        # Ensure testKind and subKind have the correct inheritence links
+        self.assert_(subKind.superKinds.has_key(testKind.getUUID()))
+        self.assert_(testKind.subKinds.has_key(subKind.getUUID()))
+
+        # for i in subKind.iterAttributes():
+        # for i in subKind.inheritedAttributes.keys():
+        #     print i
 
 if __name__ == "__main__":
     unittest.main()
