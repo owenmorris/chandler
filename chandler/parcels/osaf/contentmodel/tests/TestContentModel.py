7b9d03b5e31243f4565b698da0caacd90f7201d1
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-12-30 02:49:36 +0000

    Added to the content model test
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1561 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/contentmodel/tests/TestContentModel.py
+++ chandler/parcels/osaf/contentmodel/tests/TestContentModel.py
@@ -8,6 +8,7 @@ __copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
 __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 import unittest, os
+
 import repository.persistence.XMLRepository as XMLRepository
 import repository.parcel.LoadParcels as LoadParcels
 import OSAF.contentmodel.ContentModel as ContentModel
@@ -37,19 +38,23 @@ class ContentItemTest(unittest.TestCase):
         LoadParcels.LoadParcels(self.parceldir, self.rep)
 
     def testContentItem(self):
+        # Check that the globals got created by the parcel
         self.assert_(ContentModel.ContentItemParent)
         self.assert_(ContentModel.ContentItemKind)
         self.assert_(ContentModel.ProjectKind)
         self.assert_(ContentModel.GroupKind)
 
-        genericContentItem = ContentModel.ContentItem()
-        genericProject = ContentModel.Project()
-        genericGroup = ContentModel.Group()
+        # Construct a sample item
+        genericContentItem = ContentModel.ContentItem("genericContentItem")
+        genericProject = ContentModel.Project("genericProject")
+        genericGroup = ContentModel.Group("genericGroup")
 
+        # Check that each item was created
         self.assert_(genericContentItem)
         self.assert_(genericProject)
         self.assert_(genericGroup)
 
+        # Check each item's parent, make sure it has a path
         self.assertEqual(genericContentItem.getItemParent(),
                          ContentModel.ContentItemParent)
         self.assertEqual(genericProject.getItemParent(),
@@ -57,10 +62,22 @@ class ContentItemTest(unittest.TestCase):
         self.assertEqual(genericGroup.getItemParent(),
                          ContentModel.ContentItemParent)
         
-        self.assert_(genericContentItem.getItemPath())
-        self.assert_(genericProject.getItemPath())
-        self.assert_(genericGroup.getItemPath())
+        self.assertEqual(repr(genericContentItem.getItemPath()),
+                         '//userdata/contentitems/genericContentItem')
+        self.assertEqual(repr(genericProject.getItemPath()),
+                         '//userdata/contentitems/genericProject')
+        self.assertEqual(repr(genericGroup.getItemPath()),
+                         '//userdata/contentitems/genericGroup')
 
+        # These attributes should be empty, but should not be missing
+        self.assert_(not genericContentItem.projects)
+        self.assert_(not genericContentItem.groups)
+        self.assert_(not genericProject.name)
+        self.assert_(not genericProject.itemsInProject)
+        self.assert_(not genericGroup.name)
+        self.assert_(not genericGroup.itemsInGroup)
+
+        # Set and test simple attributes
         genericContentItem.displayName = "Test Content Item"
         genericProject.name = "Test Project"
         genericGroup.name = "Test Group"
@@ -69,8 +86,9 @@ class ContentItemTest(unittest.TestCase):
         self.assertEqual(genericGroup.name, "Test Group")
         self.assertEqual(genericContentItem.displayName, "Test Content Item")
 
-        genericContentItem.addValue('projects', genericProject)
-        genericGroup.addValue('itemsInGroup', genericContentItem)
+        # Link the contentItem to the project
+        #genericContentItem.addValue('projects', genericProject)
+        genericContentItem.projects.append(genericProject)
 
         self.assertEqual(len(genericProject.itemsInProject), 1)
         for item in genericProject.itemsInProject:
@@ -80,6 +98,9 @@ class ContentItemTest(unittest.TestCase):
         for item in genericContentItem.projects:
             self.assertEqual(item, genericProject)
 
+        # Link the group to the contentItem
+        genericGroup.addValue('itemsInGroup', genericContentItem)
+
         self.assertEqual(len(genericContentItem.groups), 1)
         for item in genericContentItem.groups:
             self.assertEqual(item, genericGroup)
@@ -88,7 +109,9 @@ class ContentItemTest(unittest.TestCase):
         for item in genericGroup.itemsInGroup:
             self.assertEqual(item, genericContentItem)
 
-        self.rep.commit()
+        self._reopenRepository()
+        project = self.rep.find("//userdata/contentitems/genericProject")
+        self.assertEqual(len(project.itemsInProject), 1)
 
     def tearDown(self):
         self.rep.close()
