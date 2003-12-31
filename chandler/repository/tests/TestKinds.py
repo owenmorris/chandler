f5d9672015912d6032c5e281a5858187cb7e14ce
twl <twl@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2003-12-31 02:17:50 +0000

    Initial test case for kinds Exercises bug#1103 discovered by Morgen
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1567 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/repository/tests/TestKinds.py
@@ -0,0 +1,33 @@
+"""
+Unit tests for kinds
+"""
+
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import RepositoryTestCase, os, unittest
+
+from repository.item.Item import Item
+from repository.schema.Attribute import Attribute
+from repository.schema.Kind import Kind
+
+class KindTest(RepositoryTestCase.RepositoryTestCase):
+    """ Test Kinds  """
+    
+    def testToXML(self):
+        """ toXML on kinds is broken """
+        kind = self.rep.find('//Schema/Core/Kind')
+        xml = kind.toXML()
+        self.failIf(xml is None)
+        
+        
+
+if __name__ == "__main__":
+#    import hotshot
+#    profiler = hotshot.Profile('/tmp/TestItems.hotshot')
+#    profiler.run('unittest.main()')
+#    profiler.close()
+    unittest.main()
+        
