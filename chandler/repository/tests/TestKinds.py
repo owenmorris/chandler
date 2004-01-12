7f9b5f7b5d0afb0cc01e6b76c6b030e353323240
twl <twl@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-01-12 06:27:39 +0000

    Real unit tests for Kinds
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1642 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/tests/TestKinds.py
+++ chandler/repository/tests/TestKinds.py
@@ -15,13 +15,103 @@ from repository.schema.Kind import Kind
 
 class KindTest(RepositoryTestCase.RepositoryTestCase):
     """ Test Kinds  """
-    
-    def testToXML(self):
 
-        kind = self.rep.find('//Schema/Core/Kind')
-        xml = kind.toXML()
+    def setUp(self):
+        super(KindTest, self).setUp()
+
+        self.kind = self._find("//Schema/Core/Kind")
+        self.itemKind = self._find("//Schema/Core/Item")
+        self.attrKind = self.itemKind.getAttribute('kind').kind
+
+        self.kind1 = self.kind.newItem('kind1',self.rep)
+        self.kind1Attr1 = Attribute('k1a1', self.rep, self.attrKind)
+        self.kind1Attr1.cardinality = 'list'
+        self.kind1Attr1.otherName = 'owner'
+        self.kind1.addValue('attributes', self.kind1Attr1, alias='k1a1')
+
+        kind1Attr1Bad = Attribute('k1a1bad', self.kind1, self.attrKind)
+        kind1Attr1Bad.cardinality = 'list'
+        kind1Attr1Bad.otherName = 'owner'
+        self.kind1.addValue('attribute', kind1Attr1Bad, alias='k1a1bad')
+        
+        self.kind2 = self.kind.newItem('kind2', self.kind1)
+
+    
+    def testBasic(self):
+        """ Test basic Kind methods """
+        kindClass = self.kind.getItemClass()
+        itemKindClass = self.itemKind.getItemClass()
+        self.assertEquals(kindClass.__name__,"Kind")
+        self.assertEquals(itemKindClass.__name__,"Item")
+
+    def testResolve(self):
+        """ Test child attribute and aliased attribute resolution """
+        # resolve a non-child, non aliased attribute
+        self.assert_(self.kind1.resolve('attributes') is None)
+        # resolve a non existent attribute
+        self.assert_(self.kind1.resolve('bogus') is None)
+
+        # resolve a child non attribute
+        self.assertEquals(self.kind1.resolve('kind2'), self.kind2.getUUID())
+
+        # resolve an attribute (alias)
+        self.assert_(self.kind1.resolve('k1a1') is self.kind1Attr1.getUUID())
+
+    def testGetAttribute(self):
+        """ Test getAttribute and has Attribute """
+        
+        # an attribute that is a child but not on the atts list should
+        # not be returned
+        self.assert_(self.kind1.getAttribute('k1a1bad') is None)
+        self.assert_(not self.kind1.hasAttribute('k1a1bad'))
+
+        # basic getAttribute and hasAttribute
+        self.assert_(self.kind1.hasAttribute('k1a1'))
+        self.assert_(self.kind1.getAttribute('k1a1') is self.kind1Attr1)
+
+        # add an inherited attribute, kind2 inherits from kind1
+        self.kind2.addValue('superKinds', self.kind1)
+
+        # retreive an inherited attribute
+        self.assert_(self.kind2.hasAttribute('k1a1'))
+        self.assert_(self.kind2.getAttribute('k1a1') is not None)
+
+    def testIsAlias(self):
+        """ A kind is not an alias """
+        self.assert_(not self.kind1.isAlias())
+    
+    def testIsSubKindOf(self):
+        """ Test IsSubKindOf on multiple super kinds """
+        # make kind2 a subkind of kind1
+        self.kind2.addValue('superKinds', self.kind1)
+        self.assert_(self.kind2.isSubKindOf(self.itemKind))
+        self.assert_(self.kind2.isSubKindOf(self.kind1))
+
+
+    def testToXML(self):
+        """ Non  realistic test of to XML """
+        xml = self.kind.toXML()
         self.failIf(xml is None)
         
+    def testRekinding(self):
+        # rekind an item
+        # re super kind a kind
+        # we do this in TestReferenceAttributes.py but should do it for real here
+        #@@@TODO we need to define what it means to rekind first
+
+#         self.kind2.addValue('superKinds', self.kind1)
+
+#         item = self.kind2.newItem('item', self.rep)
+#         item.setValue('k1a1','value')
+#         self.assertEquals(len(item.k1a1),1)
+
+#         newKind = self.kind.newItem('newKind', self.rep)
+#         item.kind = newKind
+#         self.assert_('value' in item.k1a1)
+
+#         newSuperKind = self.kind.newItem('newSuperKind', self.rep)
+
+        pass
         
 
 if __name__ == "__main__":
