7f525e4a76e5812ff08c45f6fb39457c240dd9db
twl <twl@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-01-12 06:39:04 +0000

    Add test for aliases
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1644 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/repository/tests/TestAlias.py
@@ -0,0 +1,91 @@
+"""
+Unit tests for Aliases
+"""
+
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import RepositoryTestCase, os, unittest
+
+import mx.DateTime
+import repository.schema.Types
+import repository.item.PersistentCollections
+
+from repository.schema.Attribute import Attribute
+from repository.util.Path import Path
+from repository.util.SingleRef import SingleRef
+from mx.DateTime import DateTime, DateTimeDelta, ISO, RelativeDateTime
+
+
+class AliasTest(RepositoryTestCase.RepositoryTestCase):
+    """ Test Aliases """
+
+    def setUp(self):
+        super(AliasTest, self).setUp()
+
+        self.kind = self._find(self._KIND_KIND)
+        self.itemKind = self._find(self._ITEM_KIND)
+        self.attrKind = self.itemKind.getAttribute('kind').kind
+        self.newKind = self.kind.newItem('newKind', self.rep)
+        self.typeKind = self._find('//Schema/Core/Type')
+
+        self.aliasKind = self._find('//Schema/Core/Alias')
+
+        self.alias = self.aliasKind.newItem('alias', self.rep)
+
+        self.dateTimeType = self._find('//Schema/Core/DateTime')
+        self.alias.addValue('types',self.dateTimeType)
+
+        self.intType = self._find('//Schema/Core/Integer')
+        self.alias.addValue('types',self.intType)
+
+        self.dateTimeString = '2004-01-08 12:34:56-0800'
+        self.dateTime = mx.DateTime.ISO.ParseDateTime(self.dateTimeString)
+
+    def testIsAlias(self):
+        self.assert_(self.alias.isAlias())
+        self.assert_(not self.dateTimeType.isAlias())
+        self.assert_(not self.intType.isAlias())
+
+    def testType(self):
+
+        self.assert_(self.alias.type(1.43) is None)
+        self.assert_(self.alias.type(2.4+8j) is None)
+        self.assert_(self.alias.type(True) is None)
+        self.assert_(self.alias.type(self.alias.getUUID()) is None)
+
+        self.assert_(self.alias.type(12) is not None)
+        self.assert_(self.alias.type(self.dateTime) is not None)
+
+    def testRecognizes(self):
+
+        self.assert_(not self.alias.recognizes(1.43))
+        self.assert_(not self.alias.recognizes(2.4+8j))
+        self.assert_(not self.alias.recognizes(True))
+        self.assert_(not self.alias.recognizes(self.alias.getUUID()))
+
+        self.assert_(self.alias.recognizes(12))
+        self.assert_(self.alias.recognizes(self.dateTime))
+        pass
+
+    def testAliases(self):
+
+#        newKind = self.kind.newItem('newKind', self.rep)
+#        print k
+        for a in self.alias.iterAttributes():
+#            print a
+            a1 =  self.kind.getAttribute(a[0])
+            print a1
+            if a1 is not None and a1.hasAttributeValue('types'):
+                print a1.types
+
+
+if __name__ == "__main__":
+#    import hotshot
+#    profiler = hotshot.Profile('/tmp/TestItems.hotshot')
+#    profiler.run('unittest.main()')
+#    profiler.close()
+    unittest.main()
+        
