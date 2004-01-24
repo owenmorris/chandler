e28402bdacc8833f8e786ad04331ed6f26625520
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-01-24 01:05:46 +0000

    Removed more defunct viewer parcels
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1751 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/installedParcels/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-  All parcels must define parcelClass in their __init__.py to tell the
-parcel loader which class defines the parcel. It is a string of the
-form "file.class", where file is the name of a python file (without the
-.py extension) and class is the class within the python file that is
-the parcel class.
-"""
-
-parcelClass = "InstalledParcelsViewer.InstalledParcelsViewer"
