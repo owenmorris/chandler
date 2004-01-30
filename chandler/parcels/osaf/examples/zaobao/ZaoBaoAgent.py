278de5ed7484df3c0d2856fea962e552ac175b78
pavlov <pavlov@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-01-30 02:01:42 +0000

    adding some code that will soon commit on the main thread..
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@1813 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/examples/zaobao/ZaoBaoAgent.py
+++ chandler/parcels/osaf/examples/zaobao/ZaoBaoAgent.py
@@ -3,6 +3,7 @@ __date__      = "$Date$"
 __copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
 __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
+import application.Globals as Globals
 from repository.item.Item import Item
 from repository.item.Query import KindQuery
 from OSAF.framework.agents.schema.Action import Action
@@ -32,19 +33,29 @@ def UpdateChannel(chan):
         else:
             modified = None
         data = feedparser.parse(chan.url, etag, modified)
+
+        if data['status'] == 404:
+            return
+
         chan.Update(data)
     except:
         pass
 
+def MainThreadCommit():
+    Globals.repository.commit()
+
 class UpdateAction(Action):
     def Execute(self, agent, notification):
         repository = self.getRepository()
+
         repository.commit()
         #print 'Updating feeds...'
         for feed in self.__getFeeds():
             UpdateChannel(feed)
         repository.commit()
         #print 'Updated feeds'
+
+        #Globals.application.PostAsyncEvent(MainThreadCommit)
 
     def __getFeeds(self):
 
