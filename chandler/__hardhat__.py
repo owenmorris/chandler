b6f10a737388a1d118a562cad6d958029c53b832
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-03-09 00:26:39 +0000

    We do need msvcp dlls
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2015 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/__hardhat__.py
+++ chandler/__hardhat__.py
@@ -115,6 +115,10 @@ def build(buildenv):
         os.chdir("win")
         if buildenv['version'] == 'release':
             hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE,
+             info['name'], "Copying MSVCP71.DLL to release/bin")
+            hardhatlib.copyFile("msvcp71.dll", buildenv['root'] + \
+             os.sep + "release" + os.sep + "bin")
+            hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE,
              info['name'], "Copying MSVCR71.DLL to release/bin")
             hardhatlib.copyFile("msvcr71.dll", buildenv['root'] + \
              os.sep + "release" + os.sep + "bin")
@@ -123,6 +127,10 @@ def build(buildenv):
             hardhatlib.copyFile("RunRelease.bat", buildenv['root'] + \
              os.sep + "release")
         if buildenv['version'] == 'debug':
+            hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE,
+             info['name'], "Copying MSVCP71D.DLL to debug/bin")
+            hardhatlib.copyFile("msvcp71d.dll", buildenv['root'] + \
+             os.sep + "debug" + os.sep + "bin")
             hardhatlib.log(buildenv, hardhatlib.HARDHAT_MESSAGE,
              info['name'], "Copying MSVCR71D.DLL to debug/bin")
             hardhatlib.copyFile("msvcr71d.dll", buildenv['root'] + \
