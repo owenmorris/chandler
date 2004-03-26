47f11cbfb5366c4482da9e3c1a50fa3037c9c4d8
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-03-26 19:21:31 +0000

    Work in progress to get SSL server running in Chandler.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2101 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/crypto/tests/parcels/pkitest/PKITest.py
+++ chandler/crypto/tests/parcels/pkitest/PKITest.py
@@ -1,6 +1,13 @@
+from application import Globals
 from OSAF.framework.blocks.ContainerBlocks import BoxContainer
 from repository.parcel.Parcel import Parcel
-from M2Crypto import SSL
+from M2Crypto import SSL, Rand, threading
+from PyEGADS import egads
+import thread
+from socket import *
+# XXX
+import server3
+import client3
 
 class PKITestViewParcel(Parcel):
 
@@ -18,8 +25,23 @@ class PKITestViewParcel(Parcel):
 
 class PKITestView(BoxContainer):
 
-    def OnStartStoppServerEvent(self, notification):
-        pass
+    def OnStartStopServerEvent(self, notification):
+        serverPort = Globals.repository.find('//parcels/pkitest/views/PKITestView/ServerPortText')
+        wxServerPortText = Globals.association[serverPort.getUUID( )]
+        sPort = wxServerPortText.GetValue()
+   
+        threading.init()
+        Rand.load_file('randpool.dat', -1) 
+        ctx = server3.setup_server_ctx()
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sock.bind(('', int(sPort)))
+        sock.listen(5)
+        while 1:
+            conn, addr = sock.accept()
+            thread.start_new_thread(server3.server_thread, (ctx, conn, addr))
+        Rand.save_file('randpool.dat')
+        threading.cleanup()
 
     def OnConnectDisconnectClientEvent(self, notification):
         pass
