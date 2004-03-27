b05952f8a3ccaea98070e28c6da356fe2b663963
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-03-27 00:14:59 +0000

    - Bug #1356 Subclass View for chandler specific handlers
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2104 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/blocks/Views.py
+++ chandler/parcels/osaf/framework/blocks/Views.py
@@ -3,17 +3,11 @@ __date__ = "$Date$"
 __copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
 __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
-import logging
 import application.Globals as Globals
-from Block import *
 from ContainerBlocks import *
 from MenuBlocks import MenuEntry
 from OSAF.framework.notifications.Notification import Notification
 from wxPython.wx import *
-from wxPython.html import *
-
-import OSAF.framework.utils.imports.OutlookContacts as OutlookContacts
-import OSAF.contentmodel.tests.GenerateItems as GenerateItems
 
 class View(BoxContainer):
     def dispatchEvent (self, notification):
@@ -117,59 +111,3 @@ class View(BoxContainer):
                     return
             block = block.parentBlock
 
-
-    """
-      Some placeholders for handling events.
-    """
-    def OnQuitEvent (self, notification):
-        Globals.wxApplication.mainFrame.Close ()
-        
-    def OnUndoEventUpdateUI (self, notification):
-        notification.data ['Text'] = 'Undo Command\tCtrl+Z'
-        notification.data ['Enable'] = False
-
-    def OnRedoEventUpdateUI (self, notification):
-        notification.data ['Enable'] = False
-
-    def OnCutEventUpdateUI (self, notification):
-        notification.data ['Enable'] = False
-
-    def OnCopyEventUpdateUI (self, notification):
-        notification.data ['Enable'] = False
-
-    def OnPasteEventUpdateUI (self, notification):
-        notification.data ['Enable'] = False
-        
-    def OnPreferencesEventUpdateUI (self, notification):
-        notification.data ['Enable'] = False
-        
-    # Test Methods
-
-    def OnGenerateContentItems(self, notification):
-        GenerateItems.GenerateCalendarEvents(5, 30)
-        GenerateItems.GenerateContacts(5)
-        Globals.repository.commit()
-
-    def OnGenerateCalendarEvents(self, notification):
-        GenerateItems.GenerateCalendarEvents(10, 30)
-        Globals.repository.commit()
-
-    def OnGenerateContacts(self, notification):
-        GenerateItems.GenerateContacts(10)
-        Globals.repository.commit()
-
-    def OnImportContacts(self, notification):
-        x=OutlookContacts.OutlookContacts().processFile()
-
-    def OnGenerateNotes(self, notification):
-        GenerateItems.GenerateNotes(10)
-        Globals.repository.commit()
-
-    def OnCheckRepository(self, notification):
-
-        repository = Globals.repository
-        repository.logger.info('Checking repository...')
-        if repository.check():
-            repository.logger.info('Check completed successfully')
-        else:
-            repository.logger.info('Check completed with errors')
---
+++ chandler/parcels/osaf/views/main/Main.py
@@ -0,0 +1,69 @@
+__version__ = "$Revision$"
+__date__ = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import application.Globals as Globals
+from ContainerBlocks import *
+from OSAF.framework.notifications.Notification import Notification
+from wxPython.wx import *
+
+import OSAF.framework.utils.imports.OutlookContacts as OutlookContacts
+import OSAF.contentmodel.tests.GenerateItems as GenerateItems
+
+class Main(View):
+    """
+      Main Chandler view contains event handlers for Chandler
+    """
+    def OnQuitEvent (self, notification):
+        Globals.wxApplication.mainFrame.Close ()
+        
+    def OnUndoEventUpdateUI (self, notification):
+        notification.data ['Text'] = 'Undo Command\tCtrl+Z'
+        notification.data ['Enable'] = False
+
+    def OnRedoEventUpdateUI (self, notification):
+        notification.data ['Enable'] = False
+
+    def OnCutEventUpdateUI (self, notification):
+        notification.data ['Enable'] = False
+
+    def OnCopyEventUpdateUI (self, notification):
+        notification.data ['Enable'] = False
+
+    def OnPasteEventUpdateUI (self, notification):
+        notification.data ['Enable'] = False
+        
+    def OnPreferencesEventUpdateUI (self, notification):
+        notification.data ['Enable'] = False
+        
+    # Test Methods
+
+    def OnGenerateContentItems(self, notification):
+        GenerateItems.GenerateCalendarEvents(5, 30)
+        GenerateItems.GenerateContacts(5)
+        Globals.repository.commit()
+
+    def OnGenerateCalendarEvents(self, notification):
+        GenerateItems.GenerateCalendarEvents(10, 30)
+        Globals.repository.commit()
+
+    def OnGenerateContacts(self, notification):
+        GenerateItems.GenerateContacts(10)
+        Globals.repository.commit()
+
+    def OnImportContacts(self, notification):
+        x=OutlookContacts.OutlookContacts().processFile()
+
+    def OnGenerateNotes(self, notification):
+        GenerateItems.GenerateNotes(10)
+        Globals.repository.commit()
+
+    def OnCheckRepository(self, notification):
+
+        repository = Globals.repository
+        repository.logger.info('Checking repository...')
+        if repository.check():
+            repository.logger.info('Check completed successfully')
+        else:
+            repository.logger.info('Check completed with errors')
