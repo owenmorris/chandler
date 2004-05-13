f2ecb5841b4023e845688fb8db41899e51d824d7
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-05-13 20:54:09 +0000

    Removing Run* scripts, launcher and dlls which have now moved to internal directory.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2430 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/distrib/win/RunDebug.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-setlocal
-cd ..\Chandler
-set PATH=..\debug\bin
-..\debug\bin\python_d Chandler.py %*
-endlocal
--- chandler/distrib/win/RunRelease.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-setlocal
-cd ..\Chandler
-set PATH=..\release\bin
-..\release\bin\python -O Chandler.py %*
-endlocal
