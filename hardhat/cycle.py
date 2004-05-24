4b151b5e143d97518df15b43b29cf99151356837
markie <markie@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-05-24 01:16:28 +0000

    New wrapper for tinderbox
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2495 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ hardhat/cycle.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import hardhatutil, time, smtplib, os, sys
+from optparse import OptionParser
+
+whereAmI = os.path.dirname(os.path.abspath(hardhatutil.__file__))
+
+homeDir = os.environ['HOME']
+buildDir = os.path.join(homeDir, "tinderbuild")
+stopFile = os.path.join(buildDir, "stop")
+
+def main():
+    print "Test script 1"
+    
+    parser = OptionParser(usage="%prog [options] buildName", version="%prog 1.0")
+    parser.add_option("-t", "--toAddr", action="store", type="string", dest="toAddr",
+      default="buildreport@osafoundation.org", help="Where to mail script reports\n"
+      " [default] buildreport@osafoundation.org")
+    parser.add_option("-p", "--project", action="store", type="string", dest="project",
+      default="newchandler", help="Name of script to use (without .py extension)\n"
+      "[default] newchandler")
+    parser.add_option("-o", "--output", action="store", type="string", dest="outputDir",
+      default=os.path.join(os.environ['HOME'],"output"), help="Name of temp output directory\n"
+      " [default] ~/output")
+    parser.add_option("-a", "--alert", action="store", type="string", dest="alertAddr",
+      default="buildman@osafoundation.org", help="E-mail to notify on build errors \n"
+      " [default] buildman@osafoundation.org")
+    parser.add_option("-s", "--script", action="store", dest="doScript",
+      default="tindertest.py", help="Script to run for the build\n"
+      " [default] tindertest.py")
+      
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.print_help()
+        parser.error("You must at least provide a name for your build")
+
+    buildName = args[0]
+#    print "options ", options 
+#     print " Build script ", options.doScript
+#     print "args ", args[0] 
+    
+    prevStartInt = 0
+    curDir = os.path.abspath(os.getcwd())
+
+    path = os.environ.get('PATH', os.environ.get('path'))
+    cvsProgram = hardhatutil.findInPath(path, "cvs")
+#    print "cvs =", cvsProgram
+
+    go = 1
+    # Main loop
+
+    if os.path.exists(stopFile):
+        os.remove(stopFile)
+
+    while go:
+    
+        os.chdir(curDir)
+
+        startInt = int(time.time())
+
+        if ((startInt - (5 * 60)) < prevStartInt):
+            print "Sleeping 5 minutes (" + buildName + ")"
+            time.sleep(5 * 60)
+            # re-fetch start time now that we've slept
+            startInt = int(time.time())
+
+        nowString = time.strftime("%Y-%m-%d %H:%M:%S")
+        startTime = str(startInt)
+        prevStartInt = startInt
+        
+        # check CVS for any new hardhat script
+        try:
+            # bring this hardhat directory up to date
+            outputList = hardhatutil.executeCommandReturnOutputRetry(
+             [cvsProgram, "update", "-D'"+ nowString + "'"])
+#            hardhatutil.dumpOutputList(outputList)
+        except:
+            hardhatutil.dumpOutputList(outputList)
+            raise TinderbuildError, "Error updating HardHat"
+
+        
+        try:
+            # launch the real build script
+            outputList = hardhatutil.executeCommandReturnOutput(
+             [os.path.join(curDir, options.doScript), "-o", options.outputDir, 
+             "-a", options.alertAddr, "-t", options.toAddr, 
+             "-p", options.project, args[0] ])
+            hardhatutil.dumpOutputList(outputList)
+        except:
+            hardhatutil.dumpOutputList(outputList)
+            raise TinderbuildError, "Failed to launch build script"
+
+        if os.path.exists(stopFile):
+            go = 0
+
+class TinderbuildError(Exception):
+    def __init__(self, args=None):
+        self.args = args
+
+main()    
