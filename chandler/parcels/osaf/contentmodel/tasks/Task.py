a684357defaad407f31133cd19a2a418ef780a3a
lisa <lisa@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-06-14 17:43:34 +0000

    Separating Task and Event kinds into two separate parcels within contentmodel, as well as two different kinds (previously both CalendarEvent).
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2633 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/parcels/osaf/contentmodel/tasks/Task.py
@@ -0,0 +1,49 @@
+
+""" Class used for Items of Kind Task
+"""
+
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import repository.parcel.Parcel as Parcel
+import repository.item.Item as Item
+import mx.DateTime as DateTime
+import osaf.contentmodel.ContentModel as ContentModel
+import application.Globals as Globals
+
+
+class TaskParcel(Parcel.Parcel):
+    def startupParcel(self):
+        super(TaskParcel, self).startupParcel()
+        self._setUUIDs()
+
+    def onItemLoad(self):
+        super(TaskParcel, self).onItemLoad()
+        self._setUUIDs()
+
+    def _setUUIDs(self):
+        taskKind = self['Task']
+        TaskParcel.taskKindID = taskKind.itsUUID
+        
+    def getTaskKind(cls):
+        assert cls.taskKindID, "Task parcel not yet loaded"
+        return Globals.repository[cls.taskKindID]
+
+    getTaskKind = classmethod(getTaskKind)
+
+    taskKindID = None
+    
+class Task(ContentModel.ContentItem):
+
+    def __init__(self, name=None, parent=None, kind=None):
+        if not parent:
+            parent = ContentModel.ContentModel.getContentItemParent()
+        if not kind:
+            kind = Globals.repository.findPath("//parcels/osaf/contentmodel/tasks/Task")
+        super(Task, self).__init__(name, parent, kind)
+
+        self.whoAttribute = "requestor"
+        self.aboutAttribute = "headline"
+        self.dateAttribute = "dueDate"
---
+++ chandler/parcels/osaf/contentmodel/tests/TestTask.py
@@ -0,0 +1,65 @@
+"""
+Unit tests for task
+"""
+
+__revision__  = "$ $"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import unittest, os
+
+import osaf.contentmodel.calendar.Calendar as Calendar
+import osaf.contentmodel.tasks.Task as Task
+import osaf.contentmodel.tests.TestContentModel as TestContentModel
+import osaf.contentmodel.tests.GenerateItems as GenerateItems
+
+import mx.DateTime as DateTime
+
+from repository.util.Path import Path
+
+""" LMDTODO Add tests for getAbout, getDisplay?, whoAttribute """
+
+class TaskTest(TestContentModel.ContentModelTestCase):
+    """ Test Task """
+
+    def testTask(self):
+        """ Simple test for creating instances of tasks """
+
+        def _verifyTask(task):
+            self.assert_(task != None)
+            self.assertEqual(task.headline, "test headline")
+            self.assertEqual(task.getAttributeValue('headline'),
+                              "test headline")
+            self.assertEqual(task.getItemDisplayName(), "test headline")
+
+            self.assertEqual(task.importance, 'important')
+            self.assertEqual(task.getAttributeValue('importance'), 'important')
+            self.assertEqual(task.getAbout(), "test headline")
+            self.assertEqual(task.getWho(), ' ')
+            self.assertEqual(task.getDate(), ' ')
+        
+        self.loadParcel("osaf/contentmodel/tasks")
+
+        # Check that the globals got created by the parcel
+        taskPath = Path('//parcels/osaf/contentmodel/tasks')
+        self.assert_(Task.TaskParcel.getTaskKind() != None)
+        self.assert_(self.rep.find(Path(taskPath, 'Task')) != None)
+
+        self.assertEqual(Task.TaskParcel.getTaskKind(),
+                         self.rep.find(Path(taskPath, 'Task')))
+
+        # Construct A Sample Item
+        taskItem = Task.Task("TestTask")
+        taskItem.headline = "test headline"
+        taskItem.importance = "important"
+
+        self._reopenRepository()
+
+        contentItemParent = self.rep.findPath("//userdata/contentitems")
+
+        taskItem2 = contentItemParent.getItemChild("TestTask")
+        _verifyTask(taskItem2)
+
+if __name__ == "__main__":
+    unittest.main()
