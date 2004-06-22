5fc274aae81372790b4bc7a9ecca0bb6d4e6b6e7
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-06-22 18:23:22 +0000

    Cleaned up code no longer needed in ParcelLoaderTestCase (since it's been moved up to RepositoryTestCase), and added more .cvsignore stuff
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2697 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/tests/ParcelLoaderTestCase.py
+++ chandler/application/tests/ParcelLoaderTestCase.py
@@ -9,7 +9,6 @@ __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 import unittest, os, sys
 
-# from repository.persistence.XMLRepository import XMLRepository
 import repository.tests.RepositoryTestCase as RepositoryTestCase
 
 import application
@@ -19,10 +18,6 @@ class ParcelLoaderTestCase(RepositoryTestCase.RepositoryTestCase):
 
     def setUp(self):
 
-        # Globals.chandlerDirectory = os.path.join(os.environ['CHANDLERHOME'],
-        #  "chandler")
-        # self.parcelPath = [os.path.join(Globals.chandlerDirectory, "parcels")]
-
         super(ParcelLoaderTestCase, self)._setup(self)
 
         self.testdir = os.path.join(self.rootdir, 'chandler', 'application',
@@ -30,20 +25,9 @@ class ParcelLoaderTestCase(RepositoryTestCase.RepositoryTestCase):
 
         super(ParcelLoaderTestCase, self)._openRepository(self)
 
-        # repodir = os.path.join(self.testdir, '__repository__')
-        # self.rep = XMLRepository(repodir)
-        # self.rep.create()
-        # bootstrapPack = os.path.join(Globals.chandlerDirectory,
-        #  'repository', 'packs', 'schema.pack')
-        # self.rep.loadPack(bootstrapPack)
-
         Globals.repository = self.rep
 
         # Notification manager is now needed for Item Collections(?):
-        from osaf.framework.notifications.NotificationManager import NotificationManager
-        Globals.notificationManager = NotificationManager()
-        # self.manager = application.Parcel.Manager.getManager(repository=self.rep,path=self.parcelPath)
+        # from osaf.framework.notifications.NotificationManager import NotificationManager
+        # Globals.notificationManager = NotificationManager()
 
-    # def tearDown(self):
-    #     self.rep.close()
-    #     self.rep.delete()
