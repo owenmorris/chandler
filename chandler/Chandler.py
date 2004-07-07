5fb6607d59d758e8b36516cfc27701b4cf0f6651
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-07-07 20:52:06 +0000

    message was used before it was defined.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2801 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/Chandler.py
+++ chandler/Chandler.py
@@ -1,6 +1,6 @@
 __version__ = "$Revision$"
 __date__ = "$Date$"
-__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
 __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 def main():
@@ -42,7 +42,7 @@ def main():
         type, value, stack = sys.exc_info()
         formattedBacktrace = "".join (traceback.format_exception (type, value, stack))
 
-        message = "Chandler encountered an unexpected problem %s.\n\n%s" % (message, formattedBacktrace)
+        message = "Chandler encountered an unexpected problem.\n\n%s" % (formattedBacktrace)
         logging.exception(message)
         # @@@ 25Issue - Cannot create wxItems if the app failed to initialize
         dialog = wx.MessageDialog(None, message, "Chandler", wx.OK | wx.ICON_INFORMATION)
