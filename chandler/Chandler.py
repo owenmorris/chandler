1f0515955490cacac329bf0ef55dfe791afc96f9
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-07-07 00:42:08 +0000

    - Improved error handling on Chandler failures by including a backtrace in preparation of removing error message from launcher
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2797 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/Chandler.py
+++ chandler/Chandler.py
@@ -3,44 +3,46 @@ __date__ = "$Date$"
 __copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
 __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
-import sys
-if __debug__ and '-wing' in sys.argv:
-    """
-      Check for -wing command line argument; if specified, try to connect to
-    an already-running WingIDE instance.  See:
-      http://wiki.osafoundation.org/bin/view/Main/DebuggingChandler#wingIDE
-    for details.
-    """
-    import wingdbstub
-import logging
-import wx
-from application.Application import wxApplication
-
 def main():
-
-    """
-      The details of unhandled exceptions are now handled by the logger,
-    and logged to a file: chandler.log
-    """
-    handler = logging.FileHandler('chandler.log')
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    handler.setFormatter(formatter)
-    root = logging.getLogger()
-    root.addHandler(handler)
-
     try:
+        import sys
+        if __debug__ and '-wing' in sys.argv:
+            """
+              Check for -wing command line argument; if specified, try to connect to
+            an already-running WingIDE instance.  See:
+              http://wiki.osafoundation.org/bin/view/Main/DebuggingChandler#wingIDE
+            for details.
+            """
+            import wingdbstub
+        import logging, sys, traceback
+        import wx
+        from application.Application import wxApplication
+
+        message = "while trying to start"
+
+        """
+          The details of unhandled exceptions are now handled by the logger,
+        and logged to a file: chandler.log
+        """
+        handler = logging.FileHandler('chandler.log')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler.setFormatter(formatter)
+        root = logging.getLogger()
+        root.addHandler(handler)
         """
           Don't redirect stdio to a file. useBestVisual, uses best screen
         resolutions on some old computers. See wxApp.SetUseBestVisual
         """
-        exceptionMessage = "while trying to start."
         application = wxApplication(redirect=False, useBestVisual=True)
 
-        exceptionMessage = "and had to shut down."
+        message = "and had to shut down"
         application.MainLoop()
 
-    except Exception, e:
-        message = "Chandler encountered an unexpected problem %s" % exceptionMessage
+    except Exception, exception:
+        type, value, stack = sys.exc_info()
+        formattedBacktrace = "".join (traceback.format_exception (type, value, stack))
+
+        message = "Chandler encountered an unexpected problem %s.\n\n%s" % (message, formattedBacktrace)
         logging.exception(message)
         # @@@ 25Issue - Cannot create wxItems if the app failed to initialize
         dialog = wx.MessageDialog(None, message, "Chandler", wx.OK | wx.ICON_INFORMATION)
