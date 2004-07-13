48773a26cc039d9fb5146e89cea81e8758e9c656
pavlov <pavlov@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-07-13 09:00:26 +0000

    adding ItemCollection unit tests
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2858 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/parcels/osaf/contentmodel/tests/TestItemCollection.py
@@ -0,0 +1,51 @@
+"""
+Unit tests for item collections
+"""
+
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2003 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import unittest, os, random
+
+import osaf.contentmodel.ItemCollection as ItemCollection
+import osaf.contentmodel.tests.TestContentModel as TestContentModel
+import osaf.contentmodel.tests.GenerateItems as GenerateItems
+
+class TestItemCollection(TestContentModel.ContentModelTestCase):
+
+    def testCollectionEvents(self):
+
+        self.loadParcel("http://osafoundation.org/parcels/osaf/contentmodel/calendar")
+
+        ic = ItemCollection.NamedCollection()
+        for index in range(100):
+            item = GenerateItems.GenerateCalendarEvent(100)
+            ic.include(item)
+
+            # test __contains__
+            self.assert_(item in ic)
+
+        # test __len__
+        self.assert_(len(ic) > 0)
+
+        # pick a random item in the item collection
+        item = random.choice(ic)
+
+        # test index
+        index = ic.index(item)
+
+        # test __getitem__
+        self.assert_(item == ic[index])
+
+        del index
+
+        # test exclude
+        ic.exclude(item)
+        self.assert_(item not in ic)
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
