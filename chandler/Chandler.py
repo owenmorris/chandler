43917b15779f6996a6e71aa981a5195397384fed
donn <donn@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-07-22 03:43:36 +0000

    reverted Chandler.py to 1.40
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@2950 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/Chandler.py
+++ chandler/Chandler.py
@@ -1,60 +1,91 @@
+#!bin/env python
+
+"""The chandler application.  The application itself simply tracks open
+ChandlerWindows."""
+
+__author__ = "Jed Burgess"
 __version__ = "$Revision$"
 __date__ = "$Date$"
-__copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
-__license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+__copyright__ = "Copyright (c) 2002 Open Source Applications Foundation"
+__license__ = "OSAF"
 
-def main():
-        #try:
-        message = "while trying to start."
 
-        import sys, logging, traceback, wx
-        if __debug__ and '-wing' in sys.argv:
-            """
-              Check for -wing command line argument; if specified, try to connect to
-            an already-running WingIDE instance.  See:
-              http://wiki.osafoundation.org/bin/view/Main/DebuggingChandler#wingIDE
-            for details.
-            """
-            import wingdbstub
-        from application.Application import wxApplication
+from wxPython.wx import *
+from wxPython.xrc import *
 
-        """
-          The details of unhandled exceptions are now handled by the logger,
-        and logged to a file: chandler.log
-        """
-        handler = logging.FileHandler('chandler.log')
-        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-        handler.setFormatter(formatter)
-        root = logging.getLogger()
-        root.addHandler(handler)
-        """
-          Don't redirect stdio to a file. useBestVisual, uses best screen
-        resolutions on some old computers. See wxApp.SetUseBestVisual
-        """
-        application = wxApplication(redirect=False, useBestVisual=True)
+from application.ChandlerWindow import ChandlerWindow
 
-        message = "and had to shut down."
-        application.MainLoop()
-        """
-    except Exception, exception:
-        type, value, stack = sys.exc_info()
-        formattedBacktrace = "".join (traceback.format_exception (type, value, stack))
+import sys
+import os
 
-        message = "Chandler encountered an unexpected problem %s\n\n%s" % (message, formattedBacktrace)
-        logging.exception(message)
-        # @@@ 25Issue - Cannot create wxItems if the app failed to initialize
-        dialog = wx.MessageDialog(None, message, "Chandler", wx.OK | wx.ICON_INFORMATION)
-        dialog.ShowModal()
-        dialog.Destroy()
+STARTING_X_POS = 20
+STARTING_Y_POS = 20
+NEW_WINDOW_DELTA_X = 40
+NEW_WINDOW_DELTA_Y = 40
+COMPONENTS_DIRECTORY = "./components"
+
+class osafApp(wxApp):
+    def OnInit(self):
+        """Does basic housecleaning setup.  Gets a list of all of the
+        available  components and creates a ChandlerWindow."""
+        wxInitAllImageHandlers()
+        self.componentStrings = self.__GetComponentList()
+        frame = ChandlerWindow(None, self, self.componentStrings, 
+                               pos = wxPoint(STARTING_X_POS, STARTING_Y_POS))
+        self.frames = [frame]
+        frame.Show(true)
         
-        #Reraising the exception, so wing catches it.
-        raise
+        return true
 
-    # import tools.timing
-    # print "\nTiming results:\n"
-    # tools.timing.results()
-        """
+    def __GetComponentList(self):
+        """Gets a list of each of the components for Chandler.  Among other
+        things, this will include the email, calendar, and contacts
+        components.  In order for a component to be successfully loaded, it
+        must have it's own folder in the components directory of Chandler, 
+        that folder must contain an __init__.py file, and it must have a file
+        that subclasses ComponentLoader and is named 'Foldername'Loader.py.
+
+        Returns a list of tuples, with each tuple containing the information
+        necessary to load a single component.  The first item in the tuple is
+        the name of the class that subclasses ComponentLoader and the second
+        item is the path to that component."""
+        sys.path.append(COMPONENTS_DIRECTORY)
+        componentDirectory = os.listdir(COMPONENTS_DIRECTORY)
+
+        componentStrings = []
+        for package in componentDirectory:
+            if package != 'CVS':                
+                loaderName = string.capwords(package) + 'Loader'
+                path = package + '.' + loaderName
+                componentStrings.append((loaderName, path))
+        return componentStrings    
+    
+    def OpenNewViewer(self, location):
+        """Create a new ChandlerWindow and add it to the list of open
+        windows.  Offsets the new window slightly so that they are not
+        directly overlayed."""
+        location.x += NEW_WINDOW_DELTA_X
+        location.y += NEW_WINDOW_DELTA_Y
+        frame = ChandlerWindow(None, self,  self.componentStrings, pos = location)
+        self.frames.append(frame)
+        frame.Show(true)    
         
+    def RemoveWindow(self, win):
+        """Remove the window from the list of open windows.  Should only be
+        called right before the window is closed."""
+        self.frames.remove(win)        
+
+    def QuitApp(self):
+        """Quit the application by closing all open windows."""
+        windowsToClose = []
+        # Make a copy of self.frames because it will change as we close
+        #  individual windows
+        for frame in self.frames:
+            windowsToClose.append(frame)
+        for frame in windowsToClose:
+             frame.Close(true)
+
 if __name__=="__main__":
-    main()
+    app = osafApp()
+    app.MainLoop()
 
