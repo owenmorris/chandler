2d3904d5c26b19c439ee0bd437da6fa429f2ef87
bkirsch <bkirsch@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-08-11 23:37:58 +0000

    Added a mail message generation routine to comon.py
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3077 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/mail/common.py
+++ chandler/parcels/osaf/mail/common.py
@@ -3,4 +3,21 @@ __date__      = "$Date$"
 __copyright__ = "Copyright (c) 2004 Open Source Applications Foundation"
 __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
-""" Contains functionality shared across the Mail Domain (SMTP, IMAP4, POP3) """
+""" Contains common functionality shared across the Mail Domain (SMTP, IMAP4, POP3) """
+
+
+import email as email
+import email.Message as Message
+import email.Utils as Utils
+
+
+def getChandlerTransportMessage():
+    message = """Subject: ***FOR CHANDLER INTERNAL USE - DO NOT DELETE ***
+
+    This message is used for Chandler to Chandler communication and is
+    not intended to be viewed by the user. Please do not delete this message
+    as Chandler will manage this email automatically for you.
+
+    """
+
+    return email.message_from_string(message)
