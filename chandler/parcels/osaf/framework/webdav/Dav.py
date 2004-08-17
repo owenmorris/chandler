96b4489d43b381156317c2b2fe9bc5669284b7c4
pavlov <pavlov@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-08-17 18:42:24 +0000

    removing old Import/Export code and the unneeded/broken get/put/syncCollection methods
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3124 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/webdav/Dav.py
+++ chandler/parcels/osaf/framework/webdav/Dav.py
@@ -4,7 +4,7 @@ from repository.item.Item import Item
 from repository.util.URL import URL
 
 import application.Globals as Globals
-import Import, Export, Sync
+import Sync
 
 """
  * If I make ItemCollections use a refcollection under the hood as a real attribute
@@ -53,80 +53,16 @@ class DAV(object):
         self.sync(item)
 
     def sync(self, item):
-        return Sync.syncItem(self, item)
-
+        """
+        clouds = item.itsKind.getClouds('default')
+        for cloud in clouds:
+            for i in cloud.getItems(item):
+                Sync.syncItem(self, i)
+        """
+        Sync.syncItem(self, item)
 
     etag = property(_getETag)
     lastModified = property(_getLastModified)
-
-
-    def getCollection(self):
-        # this code can't work.
-        """ gives back a new ItemCollection """
-        collection = self.get()
-
-        # XXX i really don't like duplicating the code in Import.py
-        listXmlGoop = collection._getAttribute('http://www.osafoundation.org/', 'items')
-        nodes = Import.makeAndParse(listXmlGoop)
-
-        for node in nodes:
-            item = DAV(node.content).get()
-            collection.add(item)
-
-        # figure out properties of the collection itself.. I should share code
-        # with Import.py here..
-        # get a listing of all items in the collection... propfind depth 1
-
-        # make a new ItemCollection based on the properties of the dav
-        # collection        if not changed:
-
-        # for each resource found in the dav collection, get it and add it to
-        # our itemcollection
-
-
-
-    def putCollection(self, itemCollection):
-        """
-        returns a url to a webdav collection containing
-        all of the items in 'itemCollection'
-        """
-        # just put the item collection as a normal item first
-        self.put(itemCollection)
-
-        # then attatch extra data to it
-        itemList = '<o:items xmlns:o="http://www.osafoundation.org/">'
-
-        for item in itemCollection:
-            itemURL = self.url.join(item.itsUUID.str16())
-            DAV(itemURL).put(item)
-            itemList = itemList + '<itemref>' + unicode(itemURL) + '</itemref>'
-
-        itemList = itemList + '</o:items>'
-
-        self.newConnection().setprops2(unicode(self.url), itemList)
-
-        return self.url
-
-
-
-
-    def syncCollection(self, itemCollection):
-        """ gives back a new ItemCollection """
-        # this will update the itemcollection if needed
-        self.sync(itemCollection)
-
-
-        # if the collection item itself hasn't changed, sync up the current
-        # items in the collection
-        for item in itemCollection:
-            DAV(item.sharedURL).sync(item)
-            
-            listXmlGoop = collection._getAttribute('http://www.osafoundation.org/', 'items')
-            nodes = Import.makeAndParse(listXmlGoop)
-
-            for node in nodes:
-                item = DAV(node.content).get()
-                collection.add(item)
 
 
 class DAVConnection(davlib.DAV):
