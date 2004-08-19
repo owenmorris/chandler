c46959808fae3e3c8f808033553fb74ac39fd17d
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-08-19 18:26:12 +0000

    Adding first pass at the accounts preferences dialog, which supports imap and smtp (but not ssl until we finalize the decision on the mail model). Moved the utility dialogs that used to be in Application.py into the dialogs package.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3139 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/application/dialogs/AccountPreferences.py
@@ -0,0 +1,207 @@
+import os
+import wx
+import wx.xrc
+import application.Globals
+from application.Globals import repository as repo
+from application.Globals import parcelManager as pm
+from repository.item.Query import KindQuery
+
+# Used to lookup the mail model parcel:
+MAIL_MODEL = "http://osafoundation.org/parcels/osaf/contentmodel/mail"
+
+# Used to map form fields to item attributes:
+PANELS = {
+    "IMAP" : {
+        "fields" : {
+            "IMAP_DESCRIPTION" : {
+                "attr" : "displayName",
+                "type" : "string",
+            },
+            "IMAP_EMAIL_ADDRESS" : {
+                "attr" : "emailAddress",
+                "type" : "string",
+            },
+            "IMAP_FULL_NAME" : {
+                "attr" : "fullName",
+                "type" : "string",
+            },
+            "IMAP_SERVER" : {
+                "attr" : "host",
+                "type" : "string",
+            },
+            "IMAP_USERNAME" : {
+                "attr" : "username",
+                "type" : "string",
+            },
+            "IMAP_PASSWORD" : {
+                "attr" : "password",
+                "type" : "string",
+            },
+            "IMAP_PORT" : {
+                "attr" : "port",
+                "type" : "integer",
+            },
+            # "IMAP_USE_SSL" : {
+            #     "attr" : "useSSL",
+            #     "type" : "boolean",
+            # },
+        },
+        "id" : "IMAPPane",
+    },
+    "SMTP" : {
+        "fields" : {
+            "SMTP_DESCRIPTION" : {
+                "attr" : "displayName",
+                "type" : "string",
+            },
+            "SMTP_SERVER" : {
+                "attr" : "host",
+                "type" : "string",
+            },
+            "SMTP_PORT" : {
+                "attr" : "port",
+                "type" : "integer",
+            },
+            # "SMTP_USE_SSL" : {
+            #     "attr" : "useSSL",
+            #     "type" : "boolean",
+            # },
+            "SMTP_USE_AUTH" : {
+                "attr" : "useAuth",
+                "type" : "boolean",
+            },
+            "SMTP_USERNAME" : {
+                "attr" : "username",
+                "type" : "string",
+            },
+            "SMTP_PASSWORD" : {
+                "attr" : "password",
+                "type" : "string",
+            },
+        },
+        "id" : "SMTPPane",
+    },
+}
+
+class AccountPreferencesDialog(wx.Dialog):
+    def __init__(self, parent, resources):
+        pre = wx.PreDialog()
+        self.resources = resources
+        resources.LoadOnDialog(pre, parent, 'AccountPrefsDialog')
+        self.this = pre.this
+
+        self.accountsList = wx.xrc.XRCCTRL(self, "ACCOUNTS_LIST")
+        self.detailsPanel = wx.xrc.XRCCTRL(self, "DETAILS_PANEL")
+        self.currentIndex = None # the list index of account in detail panel
+        self.currentPanelType = None
+        self.data = [ ]
+
+        wx.EVT_BUTTON( self, wx.xrc.XRCID( "OK_BUTTON" ), self.OnOk )
+        wx.EVT_BUTTON( self, wx.xrc.XRCID( "CANCEL_BUTTON" ), self.OnCancel )
+        wx.EVT_LISTBOX( self, wx.xrc.XRCID( "ACCOUNTS_LIST" ),
+         self.OnAccountSel )
+
+        self.__PopulateAccountsList()
+
+    def __PopulateAccountsList(self):
+        """ Find all AccountBase items and put them in the list; also build
+            up a data structure with the applicable attribute values we'll
+            be editing. """
+
+        # Make sure we're sync'ed with any changes other threads have made
+        repo.commit()
+
+        accountKind = pm.lookup(MAIL_MODEL, "AccountBase")
+        i = 0
+        for item in KindQuery().run([accountKind]):
+            values = { }
+            for (field, desc) in \
+             PANELS[item.accountType]['fields'].iteritems():
+                values[field] = item.getAttributeValue(desc['attr'])
+            self.data.append( { "item" : item, "values" : values } )
+            self.accountsList.Append(item.itsName)
+            i += 1
+
+        if i > 0:
+            self.accountsList.SetSelection(0)
+            self.__SwapDetailPane(0)
+
+
+    def __ApplyChanges(self):
+        """ Take the data from the list and apply the values to the items. """
+
+        # First store the current form values to the data structure
+        self.__StoreFormData(self.currentPanelType, self.currentPanel,
+         self.data[self.currentIndex]['values'])
+
+        for account in self.data:
+            item = account['item']
+            values = account['values']
+            for (field, desc) in \
+             PANELS[item.accountType]['fields'].iteritems():
+                item.setAttributeValue(desc['attr'], values[field])
+
+        repo.commit()
+
+    def __SwapDetailPane(self, index):
+        """ Given an index into the account list, store the current pane's
+            (if any) contents to the data list, destroy current pane, determine
+            type of pane to pull in, load it, populated it. """
+
+        if self.currentIndex != None:
+            # Get current form data and tuck it away
+            self.__StoreFormData(self.currentPanelType, self.currentPanel,
+             self.data[self.currentIndex]['values'])
+            self.detailsPanel.DestroyChildren()
+
+        self.currentIndex = index
+        self.currentPanelType = self.data[index]['item'].accountType
+        self.currentPanel = self.resources.LoadPanel(self.detailsPanel,
+         PANELS[self.currentPanelType]['id'])
+        self.__FetchFormData(self.currentPanelType, self.currentPanel,
+         self.data[index]['values'])
+
+    def __StoreFormData(self, panelType, panel, data):
+        for field in PANELS[panelType]['fields'].keys():
+            control = wx.xrc.XRCCTRL(panel, field)
+            valueType = PANELS[panelType]['fields'][field]['type']
+            if valueType == "string":
+                val = control.GetValue()
+            elif valueType == "boolean":
+                val = (control.GetValue() == True)
+            elif valueType == "integer":
+                val = int(control.GetValue())
+            data[field] = val
+
+    def __FetchFormData(self, panelType, panel, data):
+        for field in PANELS[panelType]['fields'].keys():
+            control = wx.xrc.XRCCTRL(panel, field)
+            valueType = PANELS[panelType]['fields'][field]['type']
+            if valueType == "string":
+                control.SetValue(data[field])
+            elif valueType == "boolean":
+                control.SetValue(data[field])
+            elif valueType == "integer":
+                control.SetValue(str(data[field]))
+
+    def OnOk(self, evt):
+        self.__ApplyChanges()
+        self.EndModal(True)
+
+    def OnCancel(self, evt):
+        self.EndModal(False)
+
+    def OnAccountSel(self, evt):
+        # Huh? This is always False!
+        # if not evt.IsSelection(): return
+
+        sel = evt.GetSelection()
+        self.__SwapDetailPane(sel)
+
+def ShowAccountPreferencesDialog(parent):
+        xrcFile = os.path.join(application.Globals.chandlerDirectory,
+         'application', 'dialogs', 'AccountPreferences_wdr.xrc')
+        resources = wx.xrc.XmlResource(xrcFile)
+        frame = AccountPreferencesDialog(parent, resources)
+        val = frame.ShowModal()
+        frame.Destroy()
---
+++ chandler/application/dialogs/Util.py
@@ -0,0 +1,198 @@
+__version__ = "$Revision$"
+__date__ = "$Date$"
+__copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
+__license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import wx
+
+# A helper method and class for allowing the user to modify an item's attributes
+
+def promptForItemValues(frame, title, item, attrList):
+    """ Given an item and a list of attributes, display a modal dialog with
+        a text field per attribute, with each field populated directly from
+        the item's attribute values.  If the user OK's the dialog, the new
+        values are applied to the item's attributes.
+
+        @param frame: A wx parent frame
+        @type frame: wx frame
+        @param title: The title string for the dialog
+        @type title: String
+        @param item:  A chandler item
+        @type item:  Item
+        @param attrList: A list of dictionaries, each one having the following
+         keys::
+
+            "attr": an attribute name
+            "label": a label to display for the field
+            "password": an optional key, set to True if you want this field to be displayed like a password (with asterisks)
+
+    """
+    win = ItemValuesDialog(frame, -1, title, item, attrList)
+    win.CenterOnScreen()
+    val = win.ShowModal()
+
+    if val == wx.ID_OK:
+        # Assign the new values
+        win.AssignNewValues()
+
+    win.Destroy()
+    return val == wx.ID_OK
+
+class ItemValuesDialog(wx.Dialog):
+    def __init__(self, parent, ID, title, item, attrList, size=wx.DefaultSize,
+     pos=wx.DefaultPosition, style=wx.DEFAULT_DIALOG_STYLE):
+
+        # Instead of calling wx.Dialog.__init__ we precreate the dialog
+        # so we can set an extra style that must be set before
+        # creation, and then we create the GUI dialog using the Create
+        # method.
+        pre = wx.PreDialog()
+        pre.Create(parent, ID, title, pos, size, style)
+
+        # This next step is the most important, it turns this Python
+        # object into the real wrapper of the dialog (instead of pre)
+        # as far as the wxPython extension is concerned.
+        self.this = pre.this
+
+        # Now continue with the normal construction of the dialog
+        # contents
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        textControls = []
+        for valueDict in attrList:
+            box = wx.BoxSizer(wx.HORIZONTAL)
+
+            label = wx.StaticText(self, -1, valueDict["label"])
+            box.Add(label, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+            if valueDict.get("password", False):
+                text = wx.TextCtrl(self, -1,
+                 item.getAttributeValue(valueDict["attr"]), size=(200,-1),
+                 style=wx.TE_PASSWORD)
+            else:
+                text = wx.TextCtrl(self, -1,
+                 item.getAttributeValue(valueDict["attr"]), size=(200,-1))
+            box.Add(text, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+            sizer.AddSizer(box, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+            textControls.append(text)
+
+        line = wx.StaticLine(self, -1, size=(20,-1), style=wx.LI_HORIZONTAL)
+        sizer.Add(line, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.RIGHT|wx.TOP, 5)
+
+        box = wx.BoxSizer(wx.HORIZONTAL)
+
+        btn = wx.Button(self, wx.ID_OK, " OK ")
+        btn.SetDefault()
+        box.Add(btn, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+        btn = wx.Button(self, wx.ID_CANCEL, " Cancel ")
+        box.Add(btn, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+        sizer.Add(box, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+
+        self.SetSizer(sizer)
+        self.SetAutoLayout(True)
+        sizer.Fit(self)
+
+        # Store these, using attribute names that hopefully wont collide with
+        # any wx attributes
+        self.chandlerTextControls = textControls
+        self.chandlerItem = item
+        self.chandlerAttrs = attrList
+
+    def AssignNewValues(self):
+        i = 0
+        for (valueDict) in self.chandlerAttrs:
+            self.chandlerItem.setAttributeValue(valueDict["attr"],
+             self.chandlerTextControls[i].GetValue())
+            i += 1
+
+# A simple "prompt-the-user-for-a-string" dialog
+
+
+def promptUser(frame, title, message, value):
+    """ Prompt the user to enter in a string.  Return None if cancel is hit.
+
+        @param frame: A wx parent frame
+        @type frame: wx frame
+        @param title: The title string for the dialog
+        @type title: String
+        @param message:  A message prompting the user for input
+        @type item:  String
+        @param value:  A value to populate the text field with
+        @type item:  String
+
+    """
+    win = promptUserDialog(frame, -1, title, message, value)
+    win.CenterOnScreen()
+    val = win.ShowModal()
+
+    if val == wx.ID_OK:
+        # Assign the new values
+        value = win.GetValue()
+    else:
+        value = None
+
+    win.Destroy()
+    return value
+
+class promptUserDialog(wx.Dialog):
+    def __init__(self, parent, ID, title, message, value, isPassword=False,
+     size=wx.DefaultSize, pos=wx.DefaultPosition,
+     style=wx.DEFAULT_DIALOG_STYLE):
+
+        # Instead of calling wx.Dialog.__init__ we precreate the dialog
+        # so we can set an extra style that must be set before
+        # creation, and then we create the GUI dialog using the Create
+        # method.
+        pre = wx.PreDialog()
+        pre.Create(parent, ID, title, pos, size, style)
+
+        # This next step is the most important, it turns this Python
+        # object into the real wrapper of the dialog (instead of pre)
+        # as far as the wxPython extension is concerned.
+        self.this = pre.this
+
+        # Now continue with the normal construction of the dialog
+        # contents
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        label = wx.StaticText(self, -1, message)
+        sizer.Add(label, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+
+        box = wx.BoxSizer(wx.HORIZONTAL)
+
+        if isPassword:
+            text = wx.TextCtrl(self, -1, value, size=(200,-1),
+             style=wx.TE_PASSWORD)
+        else:
+            text = wx.TextCtrl(self, -1, value, size=(200,-1))
+
+        box.Add(text, 1, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+        sizer.AddSizer(box, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+
+        line = wx.StaticLine(self, -1, size=(20,-1), style=wx.LI_HORIZONTAL)
+        sizer.Add(line, 0, wx.GROW|wx.ALIGN_CENTER_VERTICAL|wx.RIGHT|wx.TOP, 5)
+
+        box = wx.BoxSizer(wx.HORIZONTAL)
+
+        btn = wx.Button(self, wx.ID_OK, " OK ")
+        btn.SetDefault()
+        box.Add(btn, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+        btn = wx.Button(self, wx.ID_CANCEL, " Cancel ")
+        box.Add(btn, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+        sizer.Add(box, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+
+        self.SetSizer(sizer)
+        self.SetAutoLayout(True)
+        sizer.Fit(self)
+
+        # Store these, using attribute names that hopefully wont collide with
+        # any wx attributes
+        self.textControl = text
+
+    def GetValue(self):
+        return self.textControl.GetValue()
