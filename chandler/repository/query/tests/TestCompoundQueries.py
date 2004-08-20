80ddc8ff32a0e8520102f22bbb28e9f4d3dda477
twl <twl@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-08-20 23:12:15 +0000

    put out the fire
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3180 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/query/tests/TestCompoundQueries.py
+++ chandler/repository/query/tests/TestCompoundQueries.py
@@ -44,3 +44,4 @@ if __name__ == "__main__":
 #    profiler.run('unittest.main()')
 #    profiler.close()
 #    unittest.main()
+    pass
--- chandler/repository/query/tests/TestSimpleQueries.py
+++ chandler/repository/query/tests/TestSimpleQueries.py
@@ -92,3 +92,4 @@ if __name__ == "__main__":
 #    profiler.run('unittest.main()')
 #    profiler.close()
 #    unittest.main()
+    pass
