c91d2f6d78e0b4b62a23ccd210b346c495bf309e
bkirsch <bkirsch@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-08-24 20:22:02 +0000

    added ability to call test api's for sending and receiving mail in a debug module
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3239 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/mail/MailTasks.py
+++ chandler/parcels/osaf/mail/MailTasks.py
@@ -4,15 +4,8 @@ __copyright__ = "Copyright (c) 2004 Open Source Applications Foundation"
 __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 import osaf.framework.tasks.Action as Action
-import application.Globals as Globals
-import osaf.contentmodel.mail.Mail as Mail
-import repository.item.Query as Query
-import imap as imap
-import smtp as smtp
-import common as common
-import message as message
+import debug as debug
 import logging as logging
-import twisted.internet.defer as defer
 
 
 class IMAPDownloadAction(Action.Action):
@@ -28,67 +21,9 @@ class IMAPDownloadAction(Action.Action):
         @type task: C{osaf.framework.tasks.Task.Task}
         @return: C{None}
         """
-
-        accountKind = Mail.MailParcel.getIMAPAccountKind()
-        printed = False
-
-        for account in Query.KindQuery().run([accountKind]):
-
-            if not printed:
-                logging.info("IMAP MAIL TASK CHECKING FOR NEW MAIL")
-                printed = True
-
-            imap.IMAPDownloader(account).getMail()
+        debug.downloadIMAPMail()
 
 
 class SMTPSendAction(Action.Action):
     def Execute(self, task):
-        accountKind = Mail.MailParcel.getSMTPAccountKind()
-        account = None
-
-        for acc in Query.KindQuery().run([accountKind]):
-            account = acc
-            break
-
-        m = Mail.MailMessage()
-
-        ea = Mail.EmailAddress()
-        ea.emailAddress = "brian@abc.com"
-        #ea.fullName = "Brian Kirsch"
-
-
-        ea1 = Mail.EmailAddress()
-        ea1.emailAddress = "bkiro"
-        #ea1.fullName = "Brian Kirsch"
-
-        ea2 = Mail.EmailAddress()
-        ea2.emailAddress = "bbi.com"
-        #ea2.fullName = "Brian Kirsch"
-
-        ea3 = Mail.EmailAddress()
-        ea3.emailAddress = "bill@test.com"
-
-        ea4 = Mail.EmailAddress()
-        ea4.emailAddress = "brian@yahoo.com"
-        #ea.fullName = "Brian Kirsch"
-
-        m.toAddress.append(ea1)
-        m.toAddress.append(ea2)
-        m.toAddress.append(ea3)
-        m.toAddress.append(ea4)
-       # m.toAddress.append(ea1)
-        #m.ccAddress.append(ea2)
-        #m.bccAddress.append(ea)
-
-        m.fromAddress = ea
-        #m.replyToAddress = ea
-        m.subject = "This is a Test From SMTPSenderAction"
-        m.body = message.strToText(m, "body", "This is some body Text")
-
-        Globals.repository.commit()
-
-        smtp.SMTPSender(account, m).sendMail()
-
-        account = None
-        m = None
-
+        debug.sendSMTPMessage()
---
+++ chandler/parcels/osaf/mail/debug.py
@@ -0,0 +1,66 @@
+__revision__  = "$Revision$"
+__date__      = "$Date$"
+__copyright__ = "Copyright (c) 2004 Open Source Applications Foundation"
+__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+
+import application.Globals as Globals
+import osaf.contentmodel.mail.Mail as Mail
+import repository.item.Query as Query
+import imap as imap
+import smtp as smtp
+import common as common
+import message as message
+import logging as logging
+
+def downloadIMAPMail():
+    accountKind = Mail.MailParcel.getIMAPAccountKind()
+
+    for account in Query.KindQuery().run([accountKind]):
+        imap.IMAPDownloader(account).getMail()
+
+
+def sendSMTPMessage():
+    accountKind = Mail.MailParcel.getSMTPAccountKind()
+    account = None
+
+    for acc in Query.KindQuery().run([accountKind]):
+        account = acc
+        break
+
+    m = Mail.MailMessage()
+
+    ea = Mail.EmailAddress()
+    ea.emailAddress = "brian@saofoundation.org"
+    ea.fullName = "Brian Kirsch"
+
+    ea1 = Mail.EmailAddress()
+    ea1.emailAddress = "bkirsch@osafoundation.org"
+    #ea1.fullName = "Brian Kirsch"
+
+    ea2 = Mail.EmailAddress()
+    ea2.emailAddress = "bbi.com"
+    #ea2.fullName = "Brian Kirsch"
+
+    ea3 = Mail.EmailAddress()
+    ea3.emailAddress = "bill@test.com"
+
+    ea4 = Mail.EmailAddress()
+    ea4.emailAddress = "brian@yahoo.com"
+    #ea.fullName = "Brian Kirsch"
+
+    m.toAddress.append(ea1)
+    #m.toAddress.append(ea2)
+    #m.toAddress.append(ea3)
+    #m.toAddress.append(ea4)
+    # m.toAddress.append(ea1)
+    #m.ccAddress.append(ea2)
+    #m.bccAddress.append(ea)
+
+    m.fromAddress = ea
+    #m.replyToAddress = ea
+    m.subject = "This is a Test From SMTPSenderAction"
+    m.body = message.strToText(m, "body", "This is some body Text")
+
+    Globals.repository.commit()
+
+    smtp.SMTPSender(account, m).sendMail()
