e976c53a092f2c3e313e22f090701314f08872fe
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-10-13 17:38:42 +0000

    A helper module for document generation, and any other program that wants to make use of the repository outside of Chandler.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3728 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/application/Util.py
@@ -0,0 +1,91 @@
+"""
+@copyright: Copyright (c) 2004 Open Source Applications Foundation
+@license: U{http://osafoundation.org/Chandler_0.1_license_terms.htm}
+"""
+
+"""
+application.Util is a module for setting up various parts of the Chandler
+framework
+"""
+
+import os
+import application.Globals as Globals
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def repository(directory, destroy=False):
+    """
+    Create an instance of a repository (or open one if existing)
+
+    If the repository hasn't had its //Schema loaded, the schema.pack will
+    get loaded.  Also the notification manager will be initialized.
+
+    @param directory: The directory to use for the repository files
+    @type directory: string
+    @param destroy: If True, wipe out existing repository first (default=False)
+    @type directory: boolean
+    @return: the repository object
+    """
+
+    from repository.persistence.XMLRepository import XMLRepository
+    rep = XMLRepository(directory)
+
+    kwds = { 'create' : True, 'recover' : True, }
+    if destroy:
+        rep.create(**kwds)
+    else:
+        rep.open(**kwds)
+
+    if rep.findPath("//Schema") is None:
+        bootstrapPack = os.path.join(Globals.chandlerDirectory, 'repository',
+         'packs', 'schema.pack')
+        rep.loadPack(bootstrapPack)
+    Globals.repository = rep
+
+    # Notification manager is now needed for Item Collections(?):
+    from osaf.framework.notifications.NotificationManager \
+     import NotificationManager
+    Globals.notificationManager = NotificationManager()
+
+def logger(file):
+    """
+    Set up the logging handler
+
+    @param file: path to the file to log to
+    @type file: string
+    """
+
+    import logging
+    handler = logging.FileHandler(file)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+    logging.getLogger().addHandler(handler)
+
+def parcels():
+    """
+    Load all parcels under the 'parcels' directory
+    """
+
+    from application.Parcel import Manager
+    parcelPath = [os.path.join(Globals.chandlerDirectory, "parcels")]
+    manager = Manager.getManager(path=parcelPath)
+    manager.loadParcels()
+
+def setup(directory, destroy=False):
+    """
+    Prepare the repository, logger, and parcels
+
+    @param directory: the directory to set Globals.chandlerDirectory to
+    @type directory: string
+    @param destroy: If True, wipe out existing repository first (default=False)
+    @type destroy: boolean
+    @return: the repository object
+    """
+
+    Globals.chandlerDirectory = directory
+    logger(os.path.join(directory, 'chandler.log'))
+    repository(os.path.join(directory, '__repository__'), destroy)
+    parcels()
+    return Globals.repository
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
