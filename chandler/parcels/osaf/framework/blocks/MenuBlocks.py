3349749fc50d51a9290167e268b93de60c73c4ea
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-10-20 20:25:09 +0000

    MenuBlocks.py is unused
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3815 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/blocks/MenuBlocks.py
+++
@@ -1,6 +0,0 @@
-__version__ = "$Revision$"
-__date__ = "$Date$"
-__copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
-__license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
-
-""" This file is deprecated.  Menu classes are now found in DynamicContainerBlocks.py"""
