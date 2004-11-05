e50f97c90159b03b1fced23126c9f95d29c41d72
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-11-05 19:42:49 +0000

    Unit tests for new extension functionality.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3899 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/m2crypto/tests/test_x509.py
+++ internal/m2crypto/tests/test_x509.py
@@ -8,7 +8,7 @@ RCS_id='$Id$'
 
 import unittest
 import os
-from M2Crypto import X509, EVP, RSA, Rand
+from M2Crypto import X509, EVP, RSA, Rand, m2
 
 class X509TestCase(unittest.TestCase):
 
@@ -25,6 +25,13 @@ class X509TestCase(unittest.TestCase):
         name=x.get_subject()
         name.C = "UK"
         name.CN = "OpenSSL Group"
+        ext1 = X509.X509_Extension('subjectAltName', 'DNS:foobar.example.com')
+        ext2 = X509.X509_Extension('nsComment', 'Hello there')
+        extstack = X509.X509_Extension_Stack()
+        extstack.push(ext1)
+        extstack.push(ext2)
+        assert(extstack[1].get_name() == 'nsComment')
+        x.add_extensions(extstack)
         x.sign(pk,'md5')
         return x, pk
 
@@ -35,6 +42,36 @@ class X509TestCase(unittest.TestCase):
         os.remove('tmp_request.pem')
         assert req.as_pem() == req2.as_pem()
         assert req.as_text() == req2.as_text()
+
+    def check_mkcert(self):
+        req, pk = self.mkreq(512, 0, 365)
+        pkey = req.get_pubkey()
+        assert(req.verify(pkey))
+        sub = req.get_subject()
+        cert = X509.X509()
+        cert.set_serial_number(1)
+        cert.set_version(2)
+        cert.set_subject(sub)
+        issuer = X509.X509_Name()
+        issuer.CN = 'The Issuer Monkey'
+        issuer.O = 'The Organization Otherwise Known as My CA, Inc.'
+        cert.set_issuer(issuer)
+        cert.set_pubkey(EVP.PKey(pkey))
+        cert.set_pubkey(EVP.PKey(cert.get_pubkey()))
+        notBefore = m2.x509_get_not_before(cert.x509)
+        notAfter  = m2.x509_get_not_after(cert.x509)
+        m2.x509_gmtime_adj(notBefore, 0)
+        days = 30
+        m2.x509_gmtime_adj(notAfter, 60*60*24*days)
+        cert.add_ext(
+            X509.X509_Extension('subjectAltName', 'DNS:foobar.example.com'))
+        ext = X509.X509_Extension('nsComment', 'M2Crypto generated certificate')
+        ext.set_critical(0)
+        cert.add_ext(ext)
+        cert.sign(pk, 'sha1')
+        assert(cert.get_ext('subjectAltName').get_name() == 'subjectAltName')
+        assert(cert.get_ext_at(0).get_name() == 'subjectAltName')
+        assert(cert.get_ext_at(0).get_value() == 'DNS:foobar.example.com')
 
 def suite():
     return unittest.makeSuite(X509TestCase, 'check')
