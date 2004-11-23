7ddb8b727c7fef0f92d55073ad18dbe33e9cc609
bkirsch <bkirsch@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-11-23 22:13:34 +0000

    Added some additional error checking
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3976 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/wakeup/WakeupCaller.py
+++ chandler/parcels/osaf/framework/wakeup/WakeupCaller.py
@@ -8,15 +8,22 @@ import twisted.internet.error as error
 import chandlerdb.util.UUID as UUID
 import repository.item.Item as Item
 import mx.DateTime as DateTime
+import repository.item.ItemError as ItemError
 
 """
 Notes:
-   1. Could just have the class name as an argument with out having 
-      to make it an item
+   2. Add in enabled check each time
+
+Error Checking:
+1. Test that add and remove work ok
 """
+
+class WakeupCallException(Exception):
+    """Exception class thrown if errors occur in WakeupCaller"""
+
 class WakeupCall(Item.Item):
     def receiveWakeupCall(self):
-        pass
+        raise NotImplementedError
 
 class WakeupCaller(TwistedRepositoryViewManager.RepositoryViewManager):
     MAX_POOL_SIZE = 15
@@ -27,15 +34,30 @@ class WakeupCaller(TwistedRepositoryViewManager.RepositoryViewManager):
         self.wakeupCallies = {}
 
     def startup(self):
-        if __debug__:
-            self.printCurrentView("startup")
-
         reactor.callFromThread(self.execInView, self.__startup)
 
-    def __startup(self):
-        if __debug__:
-            self.printCurrentView("__startup")
+    def shutdown(self):
+        reactor.callFromThread(self.execInView, self.__shutdown)
 
+    def askForWakeupCall(self, wakeupCall):
+        if __debug__:
+            self.printCurrentView("askForWakeupCall")
+
+        if not isinstance(wakeupCall, WakeupCall) or not self.__isValid(wakeupCall):
+            raise WakeupCallException("Item must be of type WakeupCall with a delay greater than 0 and an Item Class")
+
+        reactor.callFromThread(self.execInView, self.__askForWakeupCall, wakeupCall)
+
+    def cancelWakeupCall(self, wakeupCall):
+        if __debug__:
+            self.printCurrentView("cancelWakeupCall")
+
+        if not isinstance(wakeupCall, WakeupCall) or self.wakeupCallies[wakeupCall.itsUUID] is None:
+            raise WakeupCallException("Item must be of type WakeupCall and already registered with the WakeupCaller")
+
+        reactor.callFromThread(self.execInView, self.__cancelWakeupCall, wakeupCall)
+
+    def __startup(self):
         self.__populate()
         size = self.wakeupCallies.__len__()
 
@@ -44,84 +66,77 @@ class WakeupCaller(TwistedRepositoryViewManager.RepositoryViewManager):
 
         reactor.suggestThreadPoolSize(size)
 
-        for item in self.wakeupCallies.values():
-            if not item.enabled:
+        for wakeupCall in self.wakeupCallies.values():
+            if not wakeupCall.enabled:
                 continue
 
-            if item.callOnStartup:
-                reactor.callInThread(item.receiveWakeupCall)
+            if wakeupCall.callOnStartup:
+                reactor.callInThread(wakeupCall.receiveWakeupCall)
 
-            item.handle = reactor.callLater(item.delay.seconds, self.execInView, 
-                                            self.__triggerEvent, item.itsUUID)
+            wakeupCall.handle = reactor.callLater(wakeupCall.delay.seconds, self.execInView,
+                                                  self.__triggerEvent, wakeupCall.itsUUID)
 
-    def askForWakeupCall(self, item):
-        #Do assert checking here
-        if __debug__:
-            self.printCurrentView("askForWakeupCall")
-
-        reactor.callFromThread(self.execInView, self.__askForWakeupCall, item)
-
-    def __askForWakeupCall(self, item):
-        if not item.enabled:
+    def __askForWakeupCall(self, wakeupCall):
+        if not wakeupCall.enabled:
             return
 
-        self.wakeupCallies[item.itsUUID] = item
+        self.wakeupCallies[wakeupCall.itsUUID] = wakeupCall
 
-        if item.callOnStartup:
-            reactor.callInThread(item.receiveWakeupCall)
+        if wakeupCall.callOnStartup:
+            reactor.callInThread(wakeupCall.receiveWakeupCall)
 
-        item.handle = reactor.callLater(item.delay.seconds, self.execInView,
-                                        self.__triggerEvent, item.itsUUID)
+        wakeupCall.handle = reactor.callLater(wakeupCall.delay.seconds, self.execInView,
+                                              self.__triggerEvent, wakeupCall.itsUUID)
 
-    def cancelWakeupCall(self, item):
-        #Do assert checking here
-        if __debug__:
-            self.printCurrentView("cancelWakeupCall")
 
-        reactor.callFromThread(self.execInView, self.__cancelWakeupCall, item)
+    def __cancelWakeupCall(self, wakeupCall):
+        if wakeupCall.handle is not None:
+           wakeupCall.handle.cancel()
 
-    def __cancelWakeupCall(self, item):
-        item = self.wakeupCallies[item.itsUUID]
-
-        if item is not None:
-            if item.handle is not None:
-               item.handle.cancel()
-
-            del self.wakeupCallies[item.itsUUID]
+        del self.wakeupCallies[wakeupCall.itsUUID]
 
     def __triggerEvent(self, uuid):
-        if __debug__:
-            self.printCurrentView("__triggerEvent")
+        wakeupCall = self.wakeupCallies[uuid]
+        assert wakeupCall is not None
 
-        item = self.wakeupCallies[uuid]
-        assert item is not None
+        reactor.callInThread(wakeupCall.receiveWakeupCall)
 
-        reactor.callInThread(item.receiveWakeupCall)
-
-        if item.repeat:
-            item.handle = reactor.callLater(item.delay.seconds, self.execInView,
-                                            self.__triggerEvent, item.itsUUID)
+        if wakeupCall.repeat:
+            wakeupCall.handle = reactor.callLater(wakeupCall.delay.seconds, self.execInView,
+                                                  self.__triggerEvent, wakeupCall.itsUUID)
 
         else:
-            item.handle = None
-
-    def shutdown(self):
-        if __debug__:
-            self.printCurrentView("shutdown")
-
-        reactor.callFromThread(self.execInView, self.__shutdown)
+            wakeupCall.handle = None
 
     def __shutdown(self):
-        if __debug__:
-            self.printCurrentView("__shutdown")
+        for wakeupCall in self.wakeupCallies.values():
+            if wakeupCall.handle is not None:
+                wakeupCall.handle.cancel()
 
-        for item in self.wakeupCallies.values():
-            if item.handle is not None:
-                item.handle.cancel()
-
-            del self.wakeupCallies[item.itsUUID]
+            del self.wakeupCallies[wakeupCall.itsUUID]
 
     def __populate(self):
         wakeupKind = Globals.repository.findPath('//parcels/osaf/framework/wakeup/WakeupCall')
-        for wakeup in Query.KindQuery().run([wakeupKind]):
-            self.wakeupCallies[wakeup.itsUUID] = wakeup
+
+        for wakeupCall in Query.KindQuery().run([wakeupKind]):
+            if not self.__isValid(wakeupCall):
+                error  = "An invalid WakeupCall was found with UUID: %s." % wakeupCall.itsUUID
+                error += "The WakeupCall must specify and Item Class and have a delay value greater than 0"
+
+                self.log.error(error)
+
+            else:
+                self.wakeupCallies[wakeupCall.itsUUID] = wakeupCall
+
+    def __isValid(self, wakeupCall):
+        if wakeupCall is None or wakeupCall.delay.seconds <= 0:
+            return False
+
+        try:
+            wakeupCall.receiveWakeupCall
+
+        except ItemError.NoSuchAttributeError:
+            return False
+
+        else:
+            return True
