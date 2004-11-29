c83fdf637e6bcd710dbbb6605354f24fb41f3728
bkirsch <bkirsch@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-11-29 20:15:03 +0000

    added some docs
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3985 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/wakeup/WakeupCaller.py
+++ chandler/parcels/osaf/framework/wakeup/WakeupCaller.py
@@ -10,14 +10,6 @@ import repository.item.Item as Item
 import mx.DateTime as DateTime
 import repository.item.ItemError as ItemError
 
-"""
-Notes:
-   2. Add in enabled check each time
-
-Error Checking:
-1. Test that add and remove work ok
-"""
-
 class WakeupCallException(Exception):
     """Exception class thrown if errors occur in WakeupCaller"""
 
@@ -26,38 +18,40 @@ class WakeupCall(Item.Item):
         raise NotImplementedError
 
 class WakeupCaller(TwistedRepositoryViewManager.RepositoryViewManager):
+    """
+      This core Chandler Service is called on Chandler Startup by Application.py.
+      The WakeupCaller loads all Items of kind WakeupCall (see osaf/framework/wakeup/parcel.xml for
+      schema definition). Each WakeupCall items recieveWakupCall method will be called at the
+      interval specified.
+    """
     MAX_POOL_SIZE = 15
 
     def __init__(self):
-        """Create a unique view string"""
+        #Create a unique view string
         super(WakeupCaller, self).__init__(Globals.repository, "WC_%s" % (str(UUID.UUID())))
         self.wakeupCallies = {}
 
     def startup(self):
+        """
+          Loads all items of kind WakeupCall. Each WakeupCall will get
+          its receiveWakupCall method executed at the interval it specifes.
+        """
         reactor.callFromThread(self.execInView, self.__startup)
 
     def shutdown(self):
+        """
+          Shuts down the WakeupCaller and unregisters all WakeupCall Items
+        """
         reactor.callFromThread(self.execInView, self.__shutdown)
 
-    def askForWakeupCall(self, wakeupCall):
-        if __debug__:
-            self.printCurrentView("askForWakeupCall")
+    def refresh(self):
+        """
+           Reloads all WakeupCall items from the Repository. This allows
+           runtime changes to WakeupCall items.
+        """
+        reactor.callFromThread(self.execInView, self.__refresh)
 
-        if not isinstance(wakeupCall, WakeupCall) or not self.__isValid(wakeupCall):
-            raise WakeupCallException("Item must be of type WakeupCall with a delay greater than 0 and an Item Class")
-
-        reactor.callFromThread(self.execInView, self.__askForWakeupCall, wakeupCall)
-
-    def cancelWakeupCall(self, wakeupCall):
-        if __debug__:
-            self.printCurrentView("cancelWakeupCall")
-
-        if not isinstance(wakeupCall, WakeupCall) or self.wakeupCallies[wakeupCall.itsUUID] is None:
-            raise WakeupCallException("Item must be of type WakeupCall and already registered with the WakeupCaller")
-
-        reactor.callFromThread(self.execInView, self.__cancelWakeupCall, wakeupCall)
-
-    def __startup(self):
+    def __startup(self, callOnStartup=True):
         self.__populate()
         size = self.wakeupCallies.__len__()
 
@@ -70,30 +64,26 @@ class WakeupCaller(TwistedRepositoryViewManager.RepositoryViewManager):
             if not wakeupCall.enabled:
                 continue
 
-            if wakeupCall.callOnStartup:
+            if callOnStartup and wakeupCall.callOnStartup:
                 reactor.callInThread(wakeupCall.receiveWakeupCall)
 
             wakeupCall.handle = reactor.callLater(wakeupCall.delay.seconds, self.execInView,
                                                   self.__triggerEvent, wakeupCall.itsUUID)
 
-    def __askForWakeupCall(self, wakeupCall):
-        if not wakeupCall.enabled:
-            return
+    def __shutdown(self):
+        for wakeupCall in self.wakeupCallies.values():
+            if wakeupCall.handle is not None:
+                wakeupCall.handle.cancel()
 
-        self.wakeupCallies[wakeupCall.itsUUID] = wakeupCall
+            del self.wakeupCallies[wakeupCall.itsUUID]
 
-        if wakeupCall.callOnStartup:
-            reactor.callInThread(wakeupCall.receiveWakeupCall)
+    def __refresh(self):
+        self.__shutdown()
+        self.view.refresh()
 
-        wakeupCall.handle = reactor.callLater(wakeupCall.delay.seconds, self.execInView,
-                                              self.__triggerEvent, wakeupCall.itsUUID)
-
-
-    def __cancelWakeupCall(self, wakeupCall):
-        if wakeupCall.handle is not None:
-           wakeupCall.handle.cancel()
-
-        del self.wakeupCallies[wakeupCall.itsUUID]
+        # When reloading wakeupCalls indicate to ignore callOnStartup flag
+        # since this is a refresh
+        self.__startup(False)
 
     def __triggerEvent(self, uuid):
         wakeupCall = self.wakeupCallies[uuid]
@@ -107,13 +97,6 @@ class WakeupCaller(TwistedRepositoryViewManager.RepositoryViewManager):
 
         else:
             wakeupCall.handle = None
-
-    def __shutdown(self):
-        for wakeupCall in self.wakeupCallies.values():
-            if wakeupCall.handle is not None:
-                wakeupCall.handle.cancel()
-
-            del self.wakeupCallies[wakeupCall.itsUUID]
 
     def __populate(self):
         wakeupCallKind = Globals.repository.findPath('//parcels/osaf/framework/wakeup/WakeupCall')
