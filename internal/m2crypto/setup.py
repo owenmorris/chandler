70ddaee31f4f43166c64df54fee538e5d103830d
vajda <vajda@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-12-01 00:36:34 +0000

    added support for python 2.4
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@3997 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/m2crypto/setup.py
+++ internal/m2crypto/setup.py
@@ -16,55 +16,58 @@ from distutils.command import build_ext
 # XXX for Chandler.
 from distutils.sysconfig import get_config_var
 
-# This copy of swig_sources is from Python 2.2.
 
-def swig_sources (self, sources):
+if 'swig=' not in [opt[0] for opt in build_ext.build_ext.user_options]:
 
-    """Walk the list of source files in 'sources', looking for SWIG
-    interface (.i) files.  Run SWIG on all that are found, and
-    return a modified 'sources' list with SWIG source files replaced
-    by the generated C (or C++) files.
-    """
+    # This copy of swig_sources is from Python 2.2.
+    def swig_sources (self, sources):
 
-    new_sources = []
-    swig_sources = []
-    swig_targets = {}
+        """Walk the list of source files in 'sources', looking for SWIG
+        interface (.i) files.  Run SWIG on all that are found, and
+        return a modified 'sources' list with SWIG source files replaced
+        by the generated C (or C++) files.
+        """
 
-    # XXX this drops generated C/C++ files into the source tree, which
-    # is fine for developers who want to distribute the generated
-    # source -- but there should be an option to put SWIG output in
-    # the temp dir.
+        new_sources = []
+        swig_sources = []
+        swig_targets = {}
 
-    if self.swig_cpp:
-        target_ext = '.cpp'
-    else:
-        target_ext = '.c'
+        # XXX this drops generated C/C++ files into the source tree, which
+        # is fine for developers who want to distribute the generated
+        # source -- but there should be an option to put SWIG output in
+        # the temp dir.
 
-    for source in sources:
-        (base, ext) = os.path.splitext(source)
-        if ext == ".i":             # SWIG interface file
-            new_sources.append(base + target_ext)
-            swig_sources.append(source)
-            swig_targets[source] = new_sources[-1]
+        if self.swig_cpp:
+            target_ext = '.cpp'
         else:
-            new_sources.append(source)
+            target_ext = '.c'
 
-    if not swig_sources:
+        for source in sources:
+            (base, ext) = os.path.splitext(source)
+            if ext == ".i":             # SWIG interface file
+                new_sources.append(base + target_ext)
+                swig_sources.append(source)
+                swig_targets[source] = new_sources[-1]
+            else:
+                new_sources.append(source)
+
+        if not swig_sources:
+            return new_sources
+
+        swig = self.find_swig()
+        swig_cmd = [swig, "-python", "-ISWIG"]
+        if self.swig_cpp:
+            swig_cmd.append("-c++")
+
+        for source in swig_sources:
+            target = swig_targets[source]
+            self.announce("swigging %s to %s" % (source, target))
+            self.spawn(swig_cmd + ["-o", target, source])
+
         return new_sources
 
-    swig = self.find_swig()
-    swig_cmd = [swig, "-python", "-ISWIG"]
-    if self.swig_cpp:
-        swig_cmd.append("-c++")
+    build_ext.build_ext.swig_sources = swig_sources
 
-    for source in swig_sources:
-        target = swig_targets[source]
-        self.announce("swigging %s to %s" % (source, target))
-        self.spawn(swig_cmd + ["-o", target, source])
-
-    return new_sources
-
-build_ext.build_ext.swig_sources = swig_sources
 
 my_inc = os.path.join(os.getcwd(), 'SWIG')
 
