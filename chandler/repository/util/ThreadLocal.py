af36c0c77fd7741c0f7971f5c2bcadc67d9b4baf
vajda <vajda@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2004-12-13 20:55:51 +0000

    made ThreadLocal use threading.local() on python 2.4
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@4071 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/util/ThreadLocal.py
+++ chandler/repository/util/ThreadLocal.py
@@ -5,49 +5,55 @@ __copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
 __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 
-import threading
+import sys, threading
 
 
-class ThreadLocal(object):
-    """
-    A class using thread local storage for its attributes.
+if sys.version_info[:2] >= (2, 4):
 
-    All attributes on this class have thread local values.
-    """
+    def ThreadLocal():
+        return threading.local()
+
+else:
+
+    from weakref import WeakKeyDictionary
+
+    class ThreadLocal(object):
+        """
+        A class using thread local storage for its attributes.
     
-    __slots__ = ()
+        All attributes on this class have thread local values.
+        """
+        
+        def __getattr__(self, name):
     
-
-    def __getattr__(self, name):
-
-        try:
-            return self._getCurrentThreadLocals()[name]
-        except KeyError:
-            raise AttributeError, name
-
-    def __setattr__(self, name, value):
-
-        self._getCurrentThreadLocals()[name] = value
-
-    def __delattr__(self, name):
-
-        try:
-            del self._getCurrentThreadLocals()[name]
-        except KeyError:
-            raise AttributeError, name
-
-    def _getCurrentThreadLocals(self):
-
-        currentThread = threading.currentThread()
-
-        try:
-            threadLocals = currentThread.threadLocals[self]
-
-        except AttributeError:
-            threadLocals = {}
-            currentThread.threadLocals = { self: threadLocals }
-
-        except KeyError:
-            currentThread.threadLocals[self] = threadLocals = {}
-
-        return threadLocals
+            try:
+                return self._getCurrentThreadLocals()[name]
+            except KeyError:
+                raise AttributeError, name
+    
+        def __setattr__(self, name, value):
+    
+            self._getCurrentThreadLocals()[name] = value
+    
+        def __delattr__(self, name):
+    
+            try:
+                del self._getCurrentThreadLocals()[name]
+            except KeyError:
+                raise AttributeError, name
+    
+        def _getCurrentThreadLocals(self):
+    
+            currentThread = threading.currentThread()
+    
+            try:
+                threadLocals = currentThread.threadLocals[self]
+    
+            except AttributeError:
+                currentThread.threadLocals = WeakKeyDictionary()
+                currentThread.threadLocals[self] = threadLocals = {}
+    
+            except KeyError:
+                currentThread.threadLocals[self] = threadLocals = {}
+    
+            return threadLocals
