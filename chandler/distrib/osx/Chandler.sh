f18633894bfdb0013f51d7f099c327ea376ce15a
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-01-05 04:48:42 +0000

    Not sure if this is used (I didn't see it in the test distribution I did) but since everything else is using 2.4 for python, figured this should also :)
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@4159 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/distrib/osx/Chandler.sh
+++ chandler/distrib/osx/Chandler.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DYLD_LIBRARY_PATH=./lib DYLD_FRAMEWORK_PATH=./bin/Library/Frameworks exec ./bin/Library/Frameworks/Python.framework/Versions/2.3/Resources/Python.app/Contents/MacOS/python Chandler.py
+DYLD_LIBRARY_PATH=./lib DYLD_FRAMEWORK_PATH=./bin/Library/Frameworks exec ./bin/Library/Frameworks/Python.framework/Versions/2.4/Resources/Python.app/Contents/MacOS/python Chandler.py
