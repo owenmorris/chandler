9ac6d5b5b276e26a4004a56665fcc368aee5b82f
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-01-05 23:21:07 +0000

    I can debug Chandler with Komodo (albeit slooowly)! Thanks to shanec from ActiveState.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@4162 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/Chandler.py
+++ chandler/Chandler.py
@@ -1,20 +1,29 @@
 __version__ = "$Revision$"
 __date__ = "$Date$"
-__copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
+__copyright__ = "Copyright (c) 2003-2005 Open Source Applications Foundation"
 __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 def main():
     message = "while trying to start."
-    
+
     def realMain():
         if __debug__ and '-wing' in sys.argv:
             """
               Check for -wing command line argument; if specified, try to connect to
             an already-running WingIDE instance.  See:
-              http://wiki.osafoundation.org/bin/view/Chandler/DebuggingChandler#wingIDE". 
+              http://wiki.osafoundation.org/bin/view/Chandler/DebuggingChandler#wingIDE".
             for details.
             """
             import wingdbstub
+        if '-komodo' in sys.argv:
+            """
+            Check for -komodo command line argument; if specified, try to connect to
+            an already-running Komodo instance.  See:
+              http://wiki.osafoundation.org/bin/view/Chandler/DebuggingChandler#Debugging_with_ActiveState_Komod".
+            for details.
+            """
+            import dbgp.client
+            dbgp.client.brk()
         from application.Application import wxApplication
 
         """
@@ -54,14 +63,14 @@ def main():
         except Exception, exception:
             type, value, stack = sys.exc_info()
             formattedBacktrace = "".join (traceback.format_exception (type, value, stack))
-    
+
             message = "Chandler encountered an unexpected problem %s\n\n%s" % (message, formattedBacktrace)
             logging.exception(message)
             # @@@ 25Issue - Cannot create wxItems if the app failed to initialize
             dialog = wx.MessageDialog(None, message, "Chandler", wx.OK | wx.ICON_INFORMATION)
             dialog.ShowModal()
             dialog.Destroy()
-            
+
             #Reraising the exception, so wing catches it.
             raise
 
