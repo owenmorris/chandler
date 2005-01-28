a0ff922b5751aa4643651fe66d30c5fa60a722c0
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-01-28 19:09:07 +0000

    Smart file copy can prevent overwriting files and can specify permissions on the destination.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@4347 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/crypto/util.py
@@ -0,0 +1,23 @@
+"""
+Utilities for crypto and other security conscious operations.
+
+@copyright: Copyright (c) 2005 Open Source Applications Foundation
+@license:   http://osafoundation.org/Chandler_0.1_license_terms.htm
+"""
+
+import os, shutil
+
+def copyfile(src, dst, flags=os.O_CREAT | os.O_EXCL, mode=0700):
+    """
+    Smarter copy. You can specify flags and mode on the destination file.
+
+    @param src:   Source file name
+    @param dst:   Destination file name
+    @param flags: Flags on the dst file. Default flags prevent overwriting.
+    @param mode:  Permissions for dst file. Defaults to user only.
+    """
+    fdst = os.fdopen(os.open(dst, flags | os.O_WRONLY, mode), 'w')
+    fsrc = os.fdopen(os.open(src, os.O_RDONLY))
+    shutil.copyfileobj(fsrc, fdst)
+    fsrc.close()
+    fdst.close()
