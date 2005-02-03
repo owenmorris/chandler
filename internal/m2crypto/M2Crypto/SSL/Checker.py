fbbdeb46d4149d01b1b1ac544c7d357c19b4a142
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-02-03 22:09:51 +0000

    Bugs 2411, 2412, must allow * in commonName/subjectAltName, and better error reporting in case match failed.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@4409 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/m2crypto/M2Crypto/SSL/Checker.py
+++ internal/m2crypto/M2Crypto/SSL/Checker.py
@@ -8,6 +8,7 @@ All rights reserved.
 RCS_id='$Id$'
 
 from M2Crypto import util, EVP
+import re
 
 class SSLVerificationError(Exception):
     pass
@@ -26,6 +27,7 @@ class Checker:
         self.host = host
         self.fingerprint = peerCertHash
         self.digest = peerCertDigest
+        self.numericIpMatch = re.compile('^[0-9]+(\.[0-9]+)*$')
 
     def __call__(self, peerCert, host=None):
         if peerCert is None:
@@ -35,6 +37,13 @@ class Checker:
             self.host = host
         
         if self.fingerprint:
+            if self.digest not in ('sha1', 'md5'):
+                raise ValueError, 'unsupported digest "%s"' %(self.digest)
+
+            if (self.digest == 'sha1' and len(self.fingerprint) != 40) or \
+               (self.digest == 'md5' and len(self.fingerprint) != 32):
+                raise WrongCertificate, 'peer certificate fingerprint length does not match'
+            
             der = peerCert.as_der()
             md = EVP.MessageDigest(self.digest)
             md.update(der)
@@ -45,13 +54,12 @@ class Checker:
         if self.host:
             hostValidationPassed = False
 
-            # XXX See RFC 2818 (and maybe 3280) for matching rules
-
             # XXX subjectAltName might contain multiple fields
             # subjectAltName=DNS:somehost
             try:
-                if peerCert.get_ext('subjectAltName').get_value() != 'DNS:' + self.host:
-                    raise WrongHost, 'subjectAltName does not match host'
+                subjectAltName = peerCert.get_ext('subjectAltName').get_value()
+                if not self._match(self.host, subjectAltName, True):
+                    raise WrongHost, 'subjectAltName does not match host. Expected "DNS:%s", got "%s".' %(self.host, subjectAltName)
                 hostValidationPassed = True
             except LookupError:
                 pass
@@ -59,9 +67,82 @@ class Checker:
             # commonName=somehost
             if not hostValidationPassed:
                 try:
-                    if peerCert.get_subject().CN != self.host:
-                        raise WrongHost, 'peer certificate commonName does not match host'
+                    commonName = peerCert.get_subject().CN
+                    if not self._match(self.host, commonName):
+                        raise WrongHost, 'peer certificate commonName does not match host. Expected "%s", got "%s".' %(self.host, commonName)
                 except AttributeError:
                     raise WrongHost, 'no commonName in peer certificate'
 
         return True
+
+    def _match(self, host, certHost, subjectAltName=False):
+        """
+        >>> check = Checker()
+        >>> check._match(host='my.example.com', certHost='DNS:my.example.com', subjectAltName=True)
+        True
+        >>> check._match(host='my.example.com', certHost='DNS:*.example.com', subjectAltName=True)
+        True
+        >>> check._match(host='my.example.com', certHost='DNS:m*.example.com', subjectAltName=True)
+        True
+        >>> check._match(host='my.example.com', certHost='DNS:m*ample.com', subjectAltName=True)
+        False
+        >>> check._match(host='my.example.com', certHost='my.example.com')
+        True
+        >>> check._match(host='my.example.com', certHost='*.example.com')
+        True
+        >>> check._match(host='my.example.com', certHost='m*.example.com')
+        True
+        >>> check._match(host='my.example.com', certHost='m*.EXAMPLE.com')
+        True
+        >>> check._match(host='my.example.com', certHost='m*ample.com')
+        False
+        >>> check._match(host='my.example.com', certHost='*.*.com')
+        False
+        >>> check._match(host='1.2.3.4', certHost='1.2.3.4')
+        True
+        >>> check._match(host='1.2.3.4', certHost='*.2.3.4')
+        False
+        >>> check._match(host='1234', certHost='1234')
+        True
+        """
+        # XXX See RFC 2818 and 3280 for matching rules, this is not
+        # XXX yet complete.
+
+        host = host.lower()
+        certHost = certHost.lower()
+
+        if subjectAltName:
+            if certHost[:4] != 'dns:':
+                return False
+            certHost = certHost[4:]
+        
+        if host == certHost:
+            return True
+
+        if certHost.count('*') > 1:
+            # Not sure about this, but being conservative
+            return False
+
+        if self.numericIpMatch.match(host) or \
+               self.numericIpMatch.match(certHost.replace('*', '')):
+            # Not sure if * allowed in numeric IP, but think not.
+            return False
+
+        if certHost.find('\\') > -1:
+            # Not sure about this, maybe some encoding might have these.
+            # But being conservative for now, because regex below relies
+            # on this.
+            return False
+
+        # Massage certHost so that it can be used in regex
+        certHost = certHost.replace('.', '\.')
+        certHost = certHost.replace('*', '[^\.]*')
+        if re.compile('^%s$' %(certHost)).match(host):
+            return True
+
+        return False
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
