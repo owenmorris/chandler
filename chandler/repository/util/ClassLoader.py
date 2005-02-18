c0af0d50ea75412cecad9ee8346db4f01a82ab09
vajda <vajda@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-02-18 03:00:34 +0000

    - fixed bug in error reporting code in ClassLoader.py
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@4593 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/util/ClassLoader.py
+++ chandler/repository/util/ClassLoader.py
@@ -21,7 +21,8 @@ class ClassLoader(object):
             raise
         except:
             logging.getLogger('repository').exception('Importing class %s.%s failed', module, name)
-            raise ImportError, (sys.exc_value, sys.exc_traceback)
+            # yes, this is valid python, a traceback can be the second argument
+            raise ImportError, sys.exc_value, sys.exc_traceback
 
         try:
             return getattr(m, name)
