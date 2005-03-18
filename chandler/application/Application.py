6b37d44cbf5a76136377b98526e0fbe540b3a6a7
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-03-18 22:18:44 +0000

    Testing commitslist, bug numbers should now have links to bugzilla. For example, here should be a link to bug 2005 (even though I am actually just fixing the copyright year to include 2005).\nThe commitslist improvement is bug 2694.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@4850 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Application.py
+++ chandler/application/Application.py
@@ -1,6 +1,6 @@
 __version__ = "$Revision$"
 __date__ = "$Date$"
-__copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
+__copyright__ = "Copyright (c) 2003-2005 Open Source Applications Foundation"
 __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 import gettext, os, sys, threading, time
