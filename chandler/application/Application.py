bd6b754a62b7213ad0d8ebe7e842bab814cbec2a
davids <davids@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-05-02 07:30:19 +0000

    correction to prior checkin r=TBD
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@5187 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Application.py
+++ chandler/application/Application.py
@@ -76,7 +76,7 @@ class MainFrame(wx.Frame):
         super (MainFrame, self).__init__(*arguments, **keywords)
 
         # useful in debugging Mac background drawing problems
-        self.MacSetMetalAppearance(True)
+        # self.MacSetMetalAppearance(True)
 
         self.SetBackgroundColour (wx.SystemSettings_GetColour(wx.SYS_COLOUR_3DFACE))
         self.Bind(wx.EVT_CLOSE, self.OnClose)
