ff235d9cc698ac7a4d40a805fb41715fa0a973a5
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-05-24 20:08:16 +0000

    Schema API: add more unit tests of cache resets, and ensure that the doctests don't leak state to other tests.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@5430 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/tests/TestSchemaAPI.py
+++ chandler/application/tests/TestSchemaAPI.py
@@ -2,8 +2,47 @@ import unittest
 this_module = "application.tests.TestSchemaAPI"     # change this if it moves
 
 from application import schema
+from repository.schema import Types
 
 
+class Dummy(schema.Item):
+    """Just a test fixture"""
+    attr = schema.One(Types.String)
+
+
+class SchemaTestCase(unittest.TestCase):
+    """Reset the schema API between unit tests"""
+    def setUp(self):
+        schema.reset()  # clear schema state before starting
+
+
+class SchemaTests(SchemaTestCase):
+
+    def testResetCache(self):
+        # Parcel/kind/attr caches should be cleared between resets
+        parcel1 = schema.parcel_for_module(this_module)
+        kind1 = Dummy._schema_kind
+        attr1 = Dummy.attr._schema_attr
+
+        old = schema.reset()
+        parcel2 = schema.parcel_for_module(this_module)
+        kind2 = Dummy._schema_kind
+        attr2 = Dummy.attr._schema_attr
+
+        self.failIf(parcel2 is parcel1)
+        self.failIf(kind2 is kind1)
+        self.failIf(attr2 is attr1)
+
+        # But switching back to an old state should restore the cache
+        schema.reset(old)
+        parcel3 = schema.parcel_for_module(this_module)
+        kind3 = Dummy._schema_kind
+        attr3 = Dummy.attr._schema_attr
+        self.failUnless(parcel3 is parcel1)
+        self.failUnless(attr3 is attr1)
+        self.failUnless(attr3 is attr1)
+
+        
 def test_schema_api():
     import doctest
     return doctest.DocFileSuite(
