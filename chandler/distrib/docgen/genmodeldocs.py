0b0f160532faf50cdd8d0bd479ce8b808f7c3fb6
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-05-31 21:51:16 +0000

    Fix for the case where initialValue is a tuple
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@5524 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/distrib/docgen/genmodeldocs.py
+++ chandler/distrib/docgen/genmodeldocs.py
@@ -430,7 +430,7 @@ def RenderItem(item, urlRoot):
             else:
                 result += "<td valign=top>N/A</td>\n"
             if attribute.hasLocalAttributeValue('initialValue'):
-                result += "<td valign=top>%s</td>\n" % (attribute.initialValue)
+                result += "<td valign=top>%s</td>\n" % (attribute.initialValue,)
             else:
                 result += "<td valign=top>N/A</td>\n"
             if attribute.required: result += "<td valign=top>Yes</td>\n"
