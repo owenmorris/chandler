a1d19cfe70fb273b2f5585010de037812f7c13a6
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-06-15 21:41:18 +0000

    Schema API: Enhance status checking script to analyze attributes and clouds in more detail.  By default, it won't print these details, but you can give it class or module names on the command line and it will print any extra info available for them.  (Note: this will now show some classes as no longer being "good", because it previously didn't check for matching cloud configurations, and now it does.)
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@5678 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/schema_status.py
+++ chandler/schema_status.py
@@ -3,10 +3,11 @@ from application.Parcel import Manager as ParcelManager
 from application.Parcel import Parcel
 from repository.schema.Kind import Kind
 from repository.persistence.RepositoryView import NullRepositoryView
+import sys, logging, os
 
-import gettext, logging, os
-gettext.install('chandler', 'locale')
-
+# pre-import any command line arguments so errors can be reported sooner
+report_on = [(arg, schema.importString(arg))for arg in sys.argv[1:]]
+  
 for chatty in 'Parcel', 'Inbound':
     # Silence talkative loggers
     logger = logging.getLogger(chatty)
@@ -42,18 +43,26 @@ unloadable = []
 non_schema = []
 diff_supers = []
 diff_attrs = []
+diff_clouds = []
 diff_path = []
 missing = []
 derived_non_schema = []
 all = set()
 bad = set()
 imports_needed = {}
+details = {}
+
+def mismatch(classname,how,what):
+    details.setdefault(classname,{}).setdefault(how,[]).append(what)
 
 def name_of(cls):
     return "%s.%s" % (
         getattr(cls,'__module__','*generated*'),cls.__name__
     )
 
+def item_names_of(item,attr):
+    return set(thing.itsName for thing in getattr(item,attr,()))
+    
 for cls, kinds in classKinds.items():
     classname = name_of(cls)
     all.add(classname)
@@ -94,7 +103,8 @@ for cls, kinds in classKinds.items():
         continue
 
     _hash = item.hashItem()
-    attrs = set(attr.itsName for attr in item.attributes)
+    attrs = item_names_of(item,'attributes')
+    clouds = item_names_of(item,'clouds')
     supers = [sk.itsName for sk in item.superKinds]
 
     for kind in kinds:
@@ -103,8 +113,26 @@ for cls, kinds in classKinds.items():
         if kind.itsName<>item.itsName:
             print
             print classname, "has a different name than", kind.itsPath
-        if attrs<>set(attr.itsName for attr in getattr(kind,'attributes',())):
+
+        if attrs<>item_names_of(kind,'attributes'):
             diff_attrs.append(classname)
+        else:
+            for attr in attrs:
+                a_item = item.attributes.getByAlias(attr)
+                a_kind = kind.attributes.getByAlias(attr)
+                if a_item.hashItem() <> a_kind.hashItem():
+                    mismatch(classname,"attributes",attr)
+
+        if clouds<>item_names_of(kind,'clouds'):
+            diff_clouds.append(classname)
+        else:
+            for cloud in clouds:
+                e_item = item_names_of(item.clouds.getByAlias(cloud),'endpoints')
+                e_kind = item_names_of(kind.clouds.getByAlias(cloud),'endpoints')
+                if e_item<>e_kind:
+                    mismatch(classname,"clouds",cloud)
+                # else: check endpoint details against each other
+
         if supers<>[sk.itsName for sk in kind.superKinds]:
             diff_supers.append(classname)
 
@@ -131,6 +159,8 @@ for title,items in [
         diff_supers),
     ("Classes with different or missing attributes (compared to parcel.xml)",
         diff_attrs),
+    ("Classes with different or missing clouds (compared to parcel.xml)",
+        diff_clouds),
 ]:
     items = set(items)
     bad |= items
@@ -140,6 +170,31 @@ for modname,classnames in imports_needed.items():
     report("Classes that should be imported by "+modname+":", classnames)
     bad |= set(classnames)
 
+
+
+def report_details(clsname):
+    for name, items in details[clsname].items():
+        report(
+            "Inconsistent %s for %s:" % (name,clsname),
+            items
+        )
+    bad.add(clsname)
+    del details[clsname]
+        
+
+for name, ob in report_on:
+    if ob in classKinds:
+        report_details(name_of(ob))
+    else:
+        prefix = name+'.'
+        for clsname,info in details.items():
+            if clsname.startswith(prefix):
+                report_details(clsname)
+
+report(
+    "Classes w/other issues (use names as arguments, to get more details):",
+    details
+)
 
 good = all - bad
 report(
