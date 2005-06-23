eede650f36c0cb0ae7549d8b486bcfa3e4747cf4
donn <donn@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-06-23 00:51:35 +0000

    Fixed a problem with many commands in CPIA Script ------------------------------------------------- * Used to be calling PostEventByName, but that didn't really work * Now searching for all BlockEvents * Also made the commands case-insensitive * Changed the Launch Chandler unit test to do a ReloadParcels
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@5736 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/CPIAScript.py
+++ chandler/application/CPIAScript.py
@@ -4,9 +4,14 @@ __copyright__ = "Copyright (c) 2005 Open Source Applications Foundation"
 __license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
 import Globals, logging
+import repository.item.Query as ItemQuery
+import wx
 
 logger = logging.getLogger('CPIA Script')
 logger.setLevel(logging.INFO)
+
+# Flag that a ReloadParcels can reload this module
+AcceptsReload = True
 
 """
 Handle running a Script.
@@ -42,12 +47,17 @@ def RunScript(script=None):
             if scriptDone:
                 Globals.CPIAScript = None # done
 
+def getAllBlockEvents():
+    blocksPath = wx.GetApp().UIRepositoryView.findPath('//parcels/osaf/framework/blocks/BlockEvent')
+    return ItemQuery.KindQuery().run([blocksPath])
+
 class CPIAScript:
     """ Script to be executed at Startup """
     # for now, we simply execute CPIA events by name
     def __init__(self, scriptText):
         self.scriptLines = scriptText.split(',')
         self.line = 0
+        self.blockEvents = getAllBlockEvents()
 
     def execute(self):
         # execute a line from the script, returning True when done
@@ -57,7 +67,21 @@ class CPIAScript:
         # simply post the event named in the script
         mainViewRoot = Globals.mainViewRoot
         logger.info("Script execution of '%s'." % nextLine)
-        mainViewRoot.postEventByName (nextLine, {})
+        # try to find this command in the blockEvents list
+        command = nextLine.upper()
+        for anEvent in self.blockEvents:
+            try:
+                eventName = anEvent.blockName
+            except AttributeError:
+                try:
+                    eventName = anEvent.itsName
+                except AttributeError:
+                    pass
+            if eventName and eventName.upper() == command:
+                mainViewRoot.post (anEvent, {})
+                break
+        else:
+            logger.info("Command '%s' not found." % nextLine)
         done = self.line >= len(self.scriptLines)
         if done:
             logger.info("Script completed execution normally")
