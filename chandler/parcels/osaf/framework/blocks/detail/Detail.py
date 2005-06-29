fc1aa0a486b26a77022b7a89ee8771bd77fbbae2
brendano <brendano@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-06-29 17:13:28 +0000

    Fix critical bug 3363 by backing out 5803's changes to Detail.py.  I don't understand why the patch doesnt work, but we need this for now, at least
    the patch command was:  svn diff -r 5803:5802 | patch -p0                                                      
    
    
    
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@5810 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/blocks/detail/Detail.py
+++ chandler/parcels/osaf/framework/blocks/detail/Detail.py
@@ -11,7 +11,6 @@ from osaf.framework.blocks import Block
 from osaf.framework.blocks import DynamicContainerBlocks
 from osaf.framework.blocks import ControlBlocks
 from osaf.framework.blocks import ContainerBlocks
-from osaf.framework.blocks.detail import DetailTrunkSubtree
 import osaf.framework.sharing.Sharing as Sharing
 from osaf.framework.blocks import Trunk
 import osaf.contentmodel.mail.Mail as Mail
@@ -320,7 +319,8 @@ class DetailTrunkDelegate (Trunk.TrunkDelegate):
         if item is None:
             # We use the subtree kind itself as the key for displaying nothing
             # (Mimi wants a particular look when no item is selected)
-            return DetailTrunkSubtree, False
+            trunkSubtreeKind = self.findPath("//parcels/osaf/framework/blocks/detail/DetailTrunkSubtree")
+            return trunkSubtreeKind, False
         else:
             return item.itsKind, False
     
@@ -369,6 +369,7 @@ class DetailTrunkDelegate (Trunk.TrunkDelegate):
         try:
             subtrees = self.subtreeList
         except AttributeError:
+            from osaf.framework.blocks.detail import DetailTrunkSubtree
             subtrees = list(DetailTrunkSubtree.iterItems(self.itsView))
             self.subtreeList = subtrees
         return subtrees
