32c3f7bcae414266a3823606aed59cfe92952bae
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-06-29 01:39:54 +0000

    Enabling long running tests when the CHANDLER_PERFORMANCE_TEST environment variable is defined.  Also populated __init__.py so run_tests.py can be used to run the sharing tests.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@5806 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/sharing/tests/TestLargeImportICalendar.py
+++ chandler/parcels/osaf/framework/sharing/tests/TestLargeImportICalendar.py
@@ -6,20 +6,20 @@ import osaf.framework.sharing.ICalendar as ICalendar
 class TestLargeImport(RepositoryTestCase):
 
     def testImport(self):
-        self.loadParcel("http://osafoundation.org/parcels/osaf/contentmodel/calendar")
-        path = os.path.join(os.getenv('CHANDLERHOME') or '.',
-                            'parcels', 'osaf', 'framework', 'sharing', 'tests')
+        if os.environ.get('CHANDLER_PERFORMANCE_TEST'):
+            self.loadParcel("http://osafoundation.org/parcels/osaf/contentmodel/calendar")
+            path = os.path.join(os.getenv('CHANDLERHOME') or '.',
+                                'parcels', 'osaf', 'framework', 'sharing', 'tests')
 
-        conduit = Sharing.FileSystemConduit(name="conduit",
-                                            sharePath=path,
-                                            shareName="3kevents.ics",
-                                            view=self.rep.view)
-        format = ICalendar.ICalendarFormat(name="format", view=self.rep.view)
-        share = Sharing.Share(name="share", conduit=conduit, format=format,
-                              view=self.rep.view)
-        share.get()
+            conduit = Sharing.FileSystemConduit(name="conduit",
+                                                sharePath=path,
+                                                shareName="3kevents.ics",
+                                                view=self.rep.view)
+            format = ICalendar.ICalendarFormat(name="format", view=self.rep.view)
+            share = Sharing.Share(name="share", conduit=conduit, format=format,
+                                  view=self.rep.view)
+            share.get()
 
 
 if __name__ == "__main__":
-    # unittest.main()
-    pass
+    unittest.main()
---
+++ chandler/parcels/osaf/framework/sharing/tests/__init__.py
@@ -0,0 +1,21 @@
+"""Sharing tests package
+
+When adding sharing test modules to this package, please add their
+name to the __all__ list, below.  Do not add non-test modules, or module
+names outside this package.
+"""
+
+__all__ = [
+    'TestFileSystemSharing', 'TestICalendar', 'TestLargeImportICalendar', 
+]
+
+def suite():
+    """Unit test suite; run by testing 'parcels.osaf.framework.sharing.tests.suite'"""
+    from run_tests import ScanningLoader
+    from unittest import defaultTestLoader, TestSuite
+    loader = ScanningLoader()
+    return TestSuite(
+        [loader.loadTestsFromName(__name__+'.'+test_name)
+            for test_name in __all__]
+    )
+
