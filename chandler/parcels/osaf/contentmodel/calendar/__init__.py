d823609dd8f279058b1b1e388be818d9fe4d02cd
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-07-23 17:25:42 +0000

    Removed nine empty parcel.xml files.  A parcel.xml file is empty if it defines no items other than the parcel itself. (An empty parcel can usually be removed if it has subparcels with parcel.xml files, or if it or at least one of its subparcels is referenced via an XML namespace in another parcel.xml.)
    Also, removed some unnecessary inter-parcel references.
    This is all in preparation for a more detailed analysis
    of our current parcel interdependencies, to support plans
    for flattening and simplifying the parcel structure,
    simplifying and optimizing the parcel loader, and adding
    parcel "lifecycle events" (install, upgrade, uninstall)
    to support schema evolution.
    
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@6137 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/contentmodel/calendar/__init__.py
+++ chandler/parcels/osaf/contentmodel/calendar/__init__.py
@@ -1,5 +1,19 @@
-# calendar parcel module
+"""Calendar Content Model
 
+Kinds and Attributes related to Calendar functionality. This schema is still a
+strawman schema, a starting point.
+
+Issues:
+    * We have not yet fully addressed dates, times, timezones, etc.
+    * Recurrence is still a placeholder, and might be general enough to live
+      with PimSchema.
+    * reminderTime is also a trial balloon.
+    * The calendar schema depends heavily on people/contacts/users/groups/etc,
+      we have yet to adequately model them.
+    * Consider using the icalendar terminology, generally
+    * Consider using the common icalendar task attributes (i.e.
+      percentComplete)
+"""
 from Calendar import Calendar as __Calendar
 from Calendar import CalendarEvent, CalendarEventMixin, Location, RecurrencePattern
 
