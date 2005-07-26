9d4c70029d3af21e8766aaaf27abf4cd3c2bd3cf
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-07-26 19:29:17 +0000

    osaf.startup: Add code and docs for reactor management.  Note that this module is still experimental and is *not* called as part of Chandler startup yet.  It still needs wakeup caller and TCP server support, as well as progress callbacks before it can replace the various ad hoc startup facilities.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@6193 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/startup.py
+++ chandler/parcels/osaf/startup.py
@@ -2,9 +2,17 @@
 
 from application import schema
 from repository.persistence.Repository import RepositoryThread
+import threading
 
-__all__ = ['Startup', 'Thread', 'run_in_thread', 'run_startup']
+__all__ = [
+    'Startup', 'run_startup', 'Thread', 'run_in_thread',
+    'get_reactor_thread', 'run_reactor', 'stop_reactor',
+]
 
+
+# --------
+# Startups
+# --------
 
 def run_startup(repositoryView):
     """Run all active startup items in `repositoryView`"""
@@ -66,6 +74,10 @@ class Startup(schema.Item):
         return False
 
 
+# -------
+# Threads
+# -------
+
 class Thread(Startup):
     """A Startup that runs its `invoke` target in a new thread"""
 
@@ -91,5 +103,72 @@ class run_in_thread(RepositoryThread):
             self.view.setCurrentView()
         self.code(self.view.findUUID(self.uuid))
 
+
+
+# ------------------
+# Reactor Management
+# ------------------
+
+_reactor_thread = None
+
+
+def get_reactor_thread():
+    """Return the threading.thread running the Twisted reactor, or None"""
+    return _reactor_thread
+    
+
+def run_reactor(in_thread=True):
+    """Safely run the Twisted reactor"""
+
+    global _reactor_thread
+
+    from twisted.python import threadable
+    threadable.init()
+    from twisted.internet import reactor
+
+    if not in_thread:
+        if reactor.running:
+            raise AssertionError("Reactor is already running")
+        # enable repeated reactor runs
+        for evt in reactor.crash, reactor.disconnectAll:
+            reactor.addSystemEventTrigger('during', 'shutdown', evt)
+        reactor.run(0)
+        return
+
+    import sys
+
+    if _reactor_thread is None:
+        if threading.currentThread().getName() != "MainThread":
+            raise AssertionError(
+                "can't start reactor thread except from the main thread"
+            )
+        limbo = [1]
+        reactor.addSystemEventTrigger('before', 'startup', limbo.pop)
+        _reactor_thread = RepositoryThread(
+            name="reactor", target=run_reactor, args=(False,)
+        )
+        _reactor_thread.setDaemon(True) # let main thread exit even if running
+        _reactor_thread.start()
+
+        while limbo and _reactor_thread.isAlive():
+            pass   # wait for reactor to start or thread to die (e.g. w/error)
+
+        if not _reactor_thread.isAlive():
+            _reactor_thread = None
+
+    
+def stop_reactor():
+    """Stop the Twisted reactor and wait for its thread to exit"""
+    
+    global _reactor_thread
+    from twisted.internet import reactor
+
+    if reactor.running:
+        reactor.callFromThread(reactor.stop)
+
+    if _reactor_thread is not None:
+        if _reactor_thread.isAlive():
+            _reactor_thread.join()
+        _reactor_thread = None
 
 
