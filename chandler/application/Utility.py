139d8db2013c4e9ba5e5f847a5966c3f2504cee5
stearns <stearns@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-07-28 22:33:44 +0000

    Ooops, forgot to bump SCHEMA_VERSION in my last checkin (r6258).
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@6259 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -16,7 +16,8 @@ from repository.item.RefCollections import RefList
 # Increment this value whenever the schema changes and edit this comment (to
 # make sure Subversion knows you changed it, in case someone else changes it
 # at about the same time)...
-SCHEMA_VERSION = "35"
+# stearns: moved detail view blocks into their own parcel.
+SCHEMA_VERSION = "36"
 
 logger = None # initialized in initLogging()
 
