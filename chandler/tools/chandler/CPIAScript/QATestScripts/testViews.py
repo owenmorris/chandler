bfc79305a20a5041cac7e80b55d5ae65a02eb233
anthony <anthony@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-08-09 18:37:11 +0000

    Added script for testing the switching of calendar views
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7732 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/tools/chandler/CPIAScript/QATestScripts/testViews.py
@@ -0,0 +1,29 @@
+import osaf.framework.scripting.QATestAppLib as QATestAppLib
+import wx
+logger = QATestAppLib.TestLogger()
+ApplicationBarAll()
+
+logger.Start("All View to Mail View")
+ApplicationBarMail()
+wx.GetApp().Yield()
+logger.Stop()
+logger.Report()
+
+logger.Start("Mail View to Task View")
+ApplicationBarTask()
+wx.GetApp().Yield()
+logger.Stop()
+logger.Report()
+
+logger.Start("Task View to Calendar View")
+ApplicationBarEvent()
+wx.GetApp().Yield()
+logger.Stop()
+logger.Report()
+
+logger.Start("Calendar View to All View")
+ApplicationBarAll()
+wx.GetApp().Yield()
+logger.Stop()
+logger.Report()
+logger.Close()
