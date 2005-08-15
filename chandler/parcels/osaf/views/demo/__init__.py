0e0551a64205f44020b1e53339bd4462e032f95c
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-08-15 16:05:21 +0000

    convert the repository viewer over to using python for blocks, and put in a python stub for the block demo so that it can be accessed from other views
    in preparation for mainview conversion to python
    
    
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@6569 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/views/demo/__init__.py
+++ chandler/parcels/osaf/views/demo/__init__.py
@@ -1,1 +1,8 @@
 # Demo
+
+from osaf.framework.blocks import BoxContainer
+
+def installParcel(parcel, oldName=None):
+
+    # just a stub right now
+    BoxContainer.update(parcel, 'BlockDemoView')
---
+++ chandler/parcels/osaf/views/repositoryviewer/__init__.py
@@ -0,0 +1,51 @@
+from Repository import RepositoryItemDetail
+from osaf.framework.blocks import Tree, SplitterWindow, SizeType
+
+def installParcel(parcel, oldName=None):
+
+    SplitterWindow.template('RepositoryView',
+                            displayName=_(u'RepositoryViewer'),
+                            eventBoundary=True,
+                            splitPercentage=0.40,
+                            childrenBlocks=[
+        Tree.template('RepositoryTree',
+                  elementDelegate='osaf.views.repositoryviewer.Repository.RepositoryDelegate',
+                  hideRoot=False,
+                  noLines=False,
+                  columnHeadings=[
+                      _(u'ItemName'),
+                      _(u'Display Name'),
+                      _(u'Kind'),
+                      _(u'UUID'),
+                      _(u'Path'),
+                  ],
+                  columnWidths=[160, 110, 70, 245, 155],
+                  size=SizeType(600,200),
+                  minimumSize=SizeType(400,100)),
+        RepositoryItemDetail.template('ItemDetail',
+                                      size=SizeType(100,50),
+                                      minimumSize=SizeType(100,50))
+        ]).install(parcel)
+        
+    SplitterWindow.template('CPIAView',
+                            displayName=_(u'Repository Viewer'),
+                            eventBoundary=True,
+                            splitPercentage=0.4,
+                            childrenBlocks=[
+        Tree.template('CPIATree',
+                      elementDelegate='osaf.views.repositoryviewer.Repository.CPIADelegate',
+                      hideRoot=False,
+                      noLines=False,
+                      columnHeadings=[
+            _(u'Block Name'),
+            _(u'Kind'),
+            _(u'Display Name'),
+            _(u'UUID'),
+            _(u'Path')
+            ],
+                      columnWidths=[160,110,70, 245, 155]),
+        RepositoryItemDetail.template('CPIAItemDetail',
+                                      size=SizeType(100,50),
+                                      minimumSize=SizeType(100,50))
+        ]).install(parcel)
+            
