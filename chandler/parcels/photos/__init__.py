abcf15f3f796b346635093a992a1436423a435c6
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-08-17 01:23:02 +0000

    Migrated photos/parcel.xml to python
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@6636 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/photos/__init__.py
+++ chandler/parcels/photos/__init__.py
@@ -1,1 +1,24 @@
 from Photos import PhotoMixin, Photo
+from application import schema
+
+def installParcel(parcel, old_version=None):
+
+    blocks = schema.ns('osaf.framework.blocks', parcel)
+    detail = schema.ns('osaf.framework.blocks.detail', parcel)
+
+    detail.DetailTrunkSubtree.update(parcel, "photo_subtree",
+        key = PhotoMixin.getKind(parcel.itsView),
+        rootBlocks = [
+            detail.DetailSynchronizedAttributeEditorBlock.update(
+                parcel, "photo_image",
+                viewAttribute = "photoBody",
+                position = 0.86,
+                stretchFactor = 1.0,
+                border = blocks.RectType(2.0, 2.0, 2.0, 2.0),
+                presentationStyle = blocks.PresentationStyle.update(
+                    parcel, "photo_image_presentation",
+                    format = "Image"
+                )
+            )
+        ]
+    )
