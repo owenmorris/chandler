d44251701335e545630bfe2c71e997591f739f34
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-08-27 00:04:12 +0000

    Fix bug #3401
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@6841 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -19,7 +19,7 @@ from repository.item.RefCollections import RefList
 #    to let others know what changed.  
 # Your comment also helps Subversion detect a conflict, in case 
 #    someone else changes it at about the same time.
-SCHEMA_VERSION = "53" # stearns: added subscribee
+SCHEMA_VERSION = "54" # john: open in CalendarView
 
 logger = None # initialized in initLogging()
 
