a3b31a9d39eb4507e4ebe7d53f86174609f4c1d6
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-07 23:29:47 +0000

    convert the table summary and calendar views over to python.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7024 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/views/main/__init__.py
+++ chandler/parcels/osaf/views/main/__init__.py
@@ -2,7 +2,9 @@ from SideBar import SidebarBlock as __SidebarBlock
 from SideBar import CPIATestSidebarTrunkDelegate, SidebarTrunkDelegate
 
 from mainblocks import make_mainview
+from summaryblocks import make_summaryblocks
 
 def installParcel(parcel, oldVersion=None):
     make_mainview(parcel)
+    make_summaryblocks(parcel)
     
