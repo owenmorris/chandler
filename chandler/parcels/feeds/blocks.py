096364db252d42a329e09f2e78da67fbe6ffd274
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-10 01:25:40 +0000

    Port feeds parcel's xml to Python
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7083 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/feeds/blocks.py
+++ chandler/parcels/feeds/blocks.py
@@ -12,7 +12,7 @@ from PyICU import ICUtzinfo, DateFormat
 import datetime
 import channels
 from i18n import OSAFMessageFactory as _
-
+from application import schema
 
 logger = logging.getLogger(__name__)
 
@@ -101,3 +101,130 @@ class FeedController(Block.Block):
                     _("New Channel Error"),
                     _("Could not create channel for %s\nCheck the URL and try again.") % url)
                 raise
+
+
+
+def installParcel(parcel, oldVersion=None):
+
+    detail = schema.ns('osaf.framework.blocks.detail', parcel)
+    blocks = schema.ns('osaf.framework.blocks', parcel)
+    main   = schema.ns('osaf.views.main', parcel)
+    feeds  = schema.ns('feeds', parcel)
+
+    feed_controller = FeedController.update(parcel, "feed_controller")
+
+    NewFeedChannelEvent = blocks.BlockEvent.update(
+        parcel, "NewFeedChannelEvent",
+        blockName="NewFeedChannel",
+        dispatchEnum="SendToBlockByReference",
+        destinationBlockReference=feed_controller,
+        commitAfterDispatch=True,
+    )
+
+    blocks.MenuItem.update(parcel, "NewFeedChannel",
+        blockName = "NewFeedChannelItem",
+        title = "New Feed Channel",
+        event = NewFeedChannelEvent,
+        eventsForNamedLookup = [NewFeedChannelEvent],
+        parentBlock = main.NewItemMenu,
+    )
+
+    # detail view stuff
+
+    # Shortcut for creating a detail-view attribute label
+    def label(name, title):
+        return detail.StaticRedirectAttributeLabel.update(
+            parcel, name, title=title,
+            characterStyle = blocks.LabelStyle, stretchFactor = 0.0,
+            textAlignmentEnum = "Right", minimumSize = blocks.SizeType(60, 24),
+            border = blocks.RectType(0, 0, 0, 5),
+        )
+
+    # Shortcut for creating a detail-view attribute editor
+    def field(name, title, stretchFactor=0.0):
+        return detail.StaticRedirectAttribute.update(
+            parcel, name, title=title,
+            characterStyle=blocks.LabelStyle, stretchFactor=stretchFactor,
+            textAlignmentEnum = "Left",
+        )
+
+    # Shortcut for creating a label/field editing area
+    def pair(name, **kw):
+        return detail.DetailSynchronizedLabeledTextAttributeBlock.update(
+            parcel, name, stretchFactor = 0.0,
+            border = blocks.RectType(5, 0, 0, 5), **kw
+        )
+
+    detail.DetailTrunkSubtree.update(parcel, "ChannelSubtree",
+
+        # This ensures that this detail view gets attached to feed items
+        key = feeds.FeedItem.getKind(parcel.itsView),
+
+        # UI Elements for detail view
+        rootBlocks = [
+            detail.MarkupBar,
+
+            FeedItemDetail.update(parcel, "ItemBodyArea",
+                blockName = "articletext",
+                size = blocks.SizeType(100,50),
+                minimumSize = blocks.SizeType(100,50),
+            ),
+
+            # URL
+            pair("LinkArea", selectedItemsAttribute="link", position=0.3,
+                childrenBlocks = [
+                    label("LinkLabel", title="link"),
+                    LinkDetail.update(parcel, "LinkAttribute",
+                        characterStyle = blocks.CharacterStyle.update(
+                            parcel, "LinkStyle",
+                            fontFamily = "DefaultUIFont",
+                            fontSize = 10, fontStyle = "underline",
+                        ),
+                        # huh, I only seem to be able to apply this to whole
+                        # ContentItemDetail items:
+                        #
+                        # colorStyle = blocks.ColorStyle.update(
+                        #     parcel, "LinkColor",
+                        #     foregroundColor = blocks.ColorType(0,0,255,255),
+                        # ),
+                        stretchFactor = 0.0,
+                        textAlignmentEnum = "Left",
+                        title = "linkattribute",
+                    ),
+                ],
+            ),
+
+            # Author area
+            pair("AuthorArea", selectedItemsAttribute="author", position=0.19,
+                childrenBlocks = [
+                    label("AuthorLabel", title="author"),
+                    field("AuthorAttribute", title="author"),
+                ]
+            ),
+
+            # Date area
+            pair("DateArea", selectedItemsAttribute="date", position=0.4,
+                childrenBlocks = [
+                    label("DateLabel", title="date"),
+                    DateDetail.update(parcel, "DateAttribute",
+                        title="date",
+                        characterStyle=blocks.LabelStyle,
+                        stretchFactor=0.0,
+                        textAlignmentEnum="Left",
+                    ),
+                ],
+            ),
+
+            # Category
+            pair("CategoryArea", selectedItemsAttribute="category",
+                position=0.2,
+                childrenBlocks = [
+                    label("CategoryLabel", title="category"),
+                    field("CategoryAttribute",
+                        title="category", stretchFactor=1.0
+                    ),
+                ]
+            ),
+        ],
+    )
+
