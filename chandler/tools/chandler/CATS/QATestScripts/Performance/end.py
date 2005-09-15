f3c8a6f0ac7ca05984c468f41b11918ad25a91f9
aparna <aparna@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-15 21:15:13 +0000

    Run this file using the command line option of chandler to get performance for startup times
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7777 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/tools/chandler/CATS/QATestScripts/Performance/end.py
@@ -0,0 +1,8 @@
+# Create script file end.py: import sys; sys.exit(1)
+# Create a new repository by starting Chandler with --create
+# Start Chandler 3 times with: time chandler -f end.py
+# Pick the run whose time was in the middle (the "real" time row)
+
+
+import sys;
+sys.exit(1)
