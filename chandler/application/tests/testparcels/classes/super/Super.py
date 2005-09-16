ebd467b84354ee75f9faa2fdbe8eb377748391d1
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-16 12:46:02 +0000

    Die, parcel.xml, die!
    Be sure to "make purge", and port any personal parcel.xml 
    files to Python before using this update.
    
    
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7203 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/tests/testparcels/classes/super/Super.py
+++
@@ -1,6 +0,0 @@
-from repository.item.Item import Item
-
-class Super(Item):
-    def __init__(self, name, parent, kind):
-        Item.__init__(self, name, parent, kind)
-        self.initCalled = True
