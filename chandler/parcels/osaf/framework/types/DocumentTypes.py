0c2c1dfc5b23f35a030d8599ae0cb0469838c6b7
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-17 20:26:37 +0000

    Untangle the ColorTypes recursive import hairball
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7245 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/parcels/osaf/framework/types/DocumentTypes.py
@@ -0,0 +1,28 @@
+import repository.schema.Types as CoreTypes
+import wx
+from application import schema
+
+
+class SizeType(schema.Struct):
+    __slots__ = 'width', 'height'
+
+
+class PositionType(schema.Struct):
+    __slots__ = 'x', 'y'
+
+
+class RectType(schema.Struct):
+    __slots__ = 'top', 'left', 'bottom', 'right'
+
+
+class ColorType(schema.Struct):
+    __slots__ = 'red', 'green', 'blue', 'alpha'
+
+    def wxColor(self):
+        # Make a wx color
+        return wx.Color(self.red, self.green, self.blue)
+
+    def toTuple(self):
+        # is there a more pythonic way of doing this?
+        return (self.red, self.green, self.blue)
+
