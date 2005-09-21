589b54528276d0f1b5ea3c5c03df98d713ea411e
twl <twl@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-21 17:50:54 +0000

    Fix 4029 - Remove old (repository.query.Query) query now that we have collections
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7351 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/query/__init__.py
+++
@@ -1,6 +0,0 @@
-__revision__  = "$Revision$"
-__date__      = "$Date$"
-__copyright__ = "Copyright (c) 2004 Open Source Applications Founation"
-__license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
-
-# The query package
