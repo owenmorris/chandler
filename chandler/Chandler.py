4dffcb25a2b7e85b89501296c7e17d8cb18eb899
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-26 20:59:01 +0000

    Cleanup and minor performance improvements.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7480 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/Chandler.py
+++ chandler/Chandler.py
@@ -1,75 +1,76 @@
-__version__ = "$Revision$"
-__date__ = "$Date$"
-__copyright__ = "Copyright (c) 2003-2005 Open Source Applications Foundation"
-__license__ = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
+"""
+Chandler startup
 
-import sys, os
-import application.Globals
+@version:   $Revision$
+@date:      $Date$
+@copyright: Copyright (c) 2003-2005 Open Source Applications Foundation
+@license:   http://osafoundation.org/Chandler_0.1_license_terms.htm
+"""
+
+import os
+import application.Globals as Globals
 import application.Utility as Utility
-from repository.persistence.RepositoryError \
-    import RepositoryOpenDeniedError, ExclusiveOpenDeniedError
-import i18n
-
 
 def main():
-    message = "while trying to start."
-
     # Process any command line switches and any environment variable values
 
-    application.Globals.options = Utility.initOptions()
+    Globals.options = Utility.initOptions()
 
-    application.Globals.chandlerDirectory = Utility.locateChandlerDirectory()
-    os.chdir(application.Globals.chandlerDirectory)
-    Utility.initLogging(application.Globals.options)
-    Utility.initI18n(application.Globals.options)
-
+    Globals.chandlerDirectory = Utility.locateChandlerDirectory()
+    os.chdir(Globals.chandlerDirectory)
+    Utility.initLogging(Globals.options)
+    Utility.initI18n(Globals.options)
 
     def realMain():
-        if __debug__ and application.Globals.options.wing:
-            # Check for -wing command line argument; if specified, try to connect to
-            # an already-running WingIDE instance.  See
+        if __debug__ and Globals.options.wing:
+            # Check for -wing command line argument; if specified, try to 
+            # connect to an already-running WingIDE instance. See
             # http://wiki.osafoundation.org/bin/view/Chandler/DebuggingChandler#wingIDE
             # for details.
 
             import wingdbstub
-        if __debug__ and application.Globals.options.komodo:
-            # Check for -komodo command line argument; if specified, try to connect to
-            # an already-running Komodo instance.  See
+        if __debug__ and Globals.options.komodo:
+            # Check for -komodo command line argument; if specified, try to 
+            # connect to an already-running Komodo instance. See
             # http://wiki.osafoundation.org/bin/view/Chandler/DebuggingChandler#Komodo
             # for details.
 
             import dbgp.client
             dbgp.client.brk()
+
         from application.Application import wxApplication
 
-        # redirect stdio and stderr to a dialog if we're running the debug version.
-        # This is done to catch asserts, which otherwise will never get seen by
-        # people who run Chandler using the launchers, e.g. Aparna. If you're
-        # running release you can also set things up so that you can see
-        # stderr and stdout if you run in a shell or from wing with a console.
+        # Redirect stdio and stderr to a dialog if we're running the debug 
+        # version. This is done to catch asserts, which otherwise will never
+        # get seen by people who run Chandler using the launchers, e.g.
+        # Aparna. If you're running release you can also set things up so 
+        # that you can see stderr and stdout if you run in a shell or from
+        # wing with a console.
         #
-        # useBestVisual - uses best screen resolutions on some old computers. See
-        #                 wxApp.SetUseBestVisual
+        # useBestVisual - uses best screen resolutions on some old computers.
+        #                 See wxApp.SetUseBestVisual
 
         app = wxApplication(redirect=__debug__, useBestVisual=True)
         app.MainLoop()
 
-    if application.Globals.options.nocatch:
+    if Globals.options.nocatch:
         # When debugging, it's handy to run without the outer exception frame
-        import logging, traceback, wx
         realMain()
     else:
         # The normal way: wrap the app in an exception frame
+        from repository.persistence.RepositoryError \
+            import RepositoryOpenDeniedError, ExclusiveOpenDeniedError
 
         try:
-            import logging, traceback, wx
+            import logging, wx
             realMain()
 
         except (RepositoryOpenDeniedError, ExclusiveOpenDeniedError):
             message = "Another instance of Chandler currently has the " \
                       "repository open."
             logging.exception(message)
-            dialog = wx.MessageDialog(None, message, "Chandler", wx.OK | wx.ICON_INFORMATION)
+            dialog = wx.MessageDialog(None, message, "Chandler", 
+                                      wx.OK | wx.ICON_INFORMATION)
             dialog.ShowModal()
             dialog.Destroy()
 
@@ -77,14 +78,19 @@ def main():
             logging.info("User chose not to clear the repository.  Exiting.")
 
         except Exception:
+            import sys, traceback
             type, value, stack = sys.exc_info()
-            formattedBacktrace = "".join (traceback.format_exception (type, value, stack, 5))
+            formattedBacktrace = "".join(traceback.format_exception(type,
+                                                                    value,
+                                                                    stack, 5))
 
-            message = ("Chandler encountered an unexpected problem %s\n" + \
-                      "Here are the bottom 5 frames of the stack:\n%s") % (message, formattedBacktrace)
+            message = ("Chandler encountered an unexpected problem while trying to start.\n" + \
+                      "Here are the bottom 5 frames of the stack:\n%s") % (formattedBacktrace)
             logging.exception(message)
-            # @@@ 25Issue - Cannot create wxItems if the app failed to initialize
-            dialog = wx.MessageDialog(None, message, "Chandler", wx.OK | wx.ICON_INFORMATION)
+            # @@@ 25Issue - Cannot create wxItems if the app failed to 
+            # @@@           initialize
+            dialog = wx.MessageDialog(None, message, "Chandler", 
+                                      wx.OK | wx.ICON_INFORMATION)
             dialog.ShowModal()
             dialog.Destroy()
 
