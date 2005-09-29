abde2bb7b9c4cb90b028c3c4957052277c2345d1
aparna <aparna@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-09-29 21:04:31 +0000

    Checking in the perf tests that handle the import within
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7797 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/tools/chandler/CATS/QATestScripts/Performance/PerfLargeDataStampEvent.py
@@ -0,0 +1,61 @@
+import osaf.sharing.Sharing as Sharing
+import osaf.sharing.ICalendar as ICalendar
+import util.QAUITestAppLib as QAUITestAppLib
+import os, wx
+import osaf.pim as pim
+
+App_ns = QAUITestAppLib.App_ns
+
+filePath = os.getenv('CATSREPORTDIR')
+if not filePath:
+    filePath = os.getcwd()
+
+# initialization
+fileName = "PerfLargeDataStampEvent.log"
+logger = QAUITestAppLib.QALogger(os.path.join(filePath, fileName),"Stamping after large data import") 
+
+
+path = os.path.join(os.getenv('CHANDLERHOME'),"util/QATestScripts/DataFiles")
+print path
+share = Sharing.OneTimeFileSystemShare(path, 'Generated3000.ics', ICalendar.ICalendarFormat, view=App_ns.itsView)
+
+#logger.Start("Import a large calendar and create a new event")
+try:
+    collection = share.get()
+except:
+    logger.Stop()
+    logger.ReportFailure("Importing calendar: exception raised")
+else:
+    App_ns.root.AddToSidebarWithoutCopying({'items' : [collection]})	
+    wx.GetApp().Yield()
+    ev = wx.IdleEvent()
+    wx.GetApp().ProcessEvent(ev)
+    #logger.Stop()
+    logger.ReportPass("Importing calendar")
+
+#def TestEventCreation(title):
+#    global logger
+#    global App_ns
+#    global pim
+#    testEvent = App_ns.item_named(pim.CalendarEvent, title)
+#    if testEvent is not None:
+#        logger.ReportPass("Testing event creation: '%s'" % title)
+#    else:
+#        logger.ReportFailure("Testing event creation: '%s' not created" % title)
+
+#TestEventCreation("Go to the beach")
+#TestEventCreation("Basketball game")
+#TestEventCreation("Visit friend")
+#TestEventCreation("Library")
+
+#action
+note = QAUITestAppLib.UITestItem("Note", logger)
+note.StampAsCalendarEvent(True)
+
+#verification
+note.Check_DetailView({"stampEvent":True})
+
+
+#logger.SetChecked(True)
+#logger.Report("Import")
+logger.Close()
