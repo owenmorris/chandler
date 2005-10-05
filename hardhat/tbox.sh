f72274bd0f09fff5078835f95e28138743dc296e
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-10-05 05:05:06 +0000

    tbox.sh is a port of the python tinderbox code to bash script tbox_update.py is a helper program to send tinderbox2 status emails from within tbox.sh tbox.sh is commented to show what parameters and enviroment variables control it's behaviour
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7633 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ hardhat/tbox.sh
@@ -0,0 +1,341 @@
+#!/bin/bash
+
+#
+# script to run all Chandler unit tests
+#
+# All output is gathered into temporary log files and
+# then dumped to stdout for capture if wanted
+#
+# Scans the chandler/ tree for any sub-directory that is named 
+# "tests" and then within that directory calls RunPython for any
+# file named Test*.py
+#
+# if CHANDLER_PERFORMANCE_TEST=yes then CATS Performance Tests are run
+# if CHANDLER_FUNCTIONAL_TEST=no then CATS Functional Tests are skipped
+#
+# For tbox operation, define TBOX=yes and if you want it to cycle define TBOX_CYCLE=yes
+# tbox operation changes the meaning of the path parameter - under tbox it should point
+# to the working directory (default is ~/tinderbox) and from there the chandler directory
+# is calculated.
+#
+# In tbox mode, only the release mode is processed.
+#
+
+USAGE="Usage: `basename $0` chandler-base-path"
+
+if [ ! -n "$1" ]; then
+    if [ "$TBOX" = "yes" ]; then
+        T_DIR=~/tinderbuild
+        C_DIR=$T_DIR/chandler
+    else
+        echo $USAGE
+        echo if CHANDLER_FUNCTIONAL_TEST=no then CATS Functional Tests are skipped
+        echo if CHANDLER_PERFORMANCE_TEST=yes then CATS Performance Tests are run
+        echo if TBOX=yes then only release mode is tested and no distribution is built
+        exit 65
+    fi
+else
+    if [ "$TBOX" = "yes" ]; then
+        T_DIR="$1"
+        C_DIR=$T_DIR/chandler
+    else
+        C_DIR="$1"
+        T_DIR=$C_DIR
+
+        if [ ! -d "$C_DIR/i18n" ]; then
+            echo Error: The path [$C_DIR] given does not point to a chandler/ directory
+            echo $USAGE
+            exit 65
+        fi
+    fi
+fi
+
+if [ "$TBOX" = "yes" ]; then
+    if [ "$TBOX_BUILD" = "" ]; then
+        echo TBOX_BUILD environment variable must be set
+    fi
+    if [ "$TBOX_TREE" = "" ]; then
+        echo TBOX_TREE environment variable must be set
+    fi
+    if [ ! -d "$T_DIR" ]; then
+        mkdir "$T_DIR"
+    fi
+fi
+
+HH_DIR=`pwd`
+LOGFILES="svn.log distrib.log install.log tests.log"
+MODES="release debug"
+SLEEP_MINUTES=5
+ENDLOOP="no"
+
+while [ "$ENDLOOP" = "no" ]
+do
+    BUILDID=`date +%Y%m%d%H%M%S`
+    BUILDLOG=$T_DIR/tbox_$BUILDID.log
+
+    if [ -e $BUILDLOG ]; then
+        rm $BUILDLOG
+    fi
+
+    echo - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + | tee -a $BUILDLOG
+    echo Started `date`                                              | tee -a $BUILDLOG
+    echo Setting up script environment                               | tee -a $BUILDLOG
+
+    if [ "$OSTYPE" = "cygwin" ]; then
+        RUN_CHANDLER=RunChandler.bat
+        RUN_PYTHON=RunPython.bat
+    else
+        RUN_CHANDLER=RunChandler
+        RUN_PYTHON=RunPython
+    fi
+
+    for i in $LOGFILES ; do
+        if [ -e $T_DIR/$i ]; then
+            rm $T_DIR/$i
+        fi
+        touch $T_DIR/$i
+    done
+
+    if [ "$TBOX" = "yes" ]; then
+        $HH_DIR/tbox_update.py -t $TBOX_TREE -b $TBOX_BUILD -s building -T bear -D "@code-bear.com"
+
+        if [ "$CHANDLER_PERFORMANCE_TEST" = "yes" ]; then
+            MODES="release"
+        fi
+    else
+          # if the debug/ path is not found, then avoid debug tests
+        if [ ! -d $C_DIR/debug ]; then
+            MODES="release"
+            echo Skipping debug tests as $C_DIR/debug does not exist | tee -a $BUILDLOG
+        fi
+    fi
+
+      # each directory to exclude should be place in the EXCLUDES array
+      # and a 0 value should be place in the L_EXCLUDES array
+      # the EXCLUDES array is then walked and the length of the 
+      # directory is calculated - beats doing it by hand and making a mistake
+
+    EXCLUDES=("$C_DIR/release" "$C_DIR/debug" "$C_DIR/tools" "$C_DIR/util")
+    L_EXCLUDES=(0 0 0 0)
+    for item in 0 1 2 3 ; do
+        L_EXCLUDES[$item]=${#EXCLUDES[$item]}
+    done
+
+    if [ "$TBOX" = "yes" ]; then
+        echo Setting up Chandler environment | tee -a $BUILDLOG
+
+        INSTALL="no"
+
+        if [ -d $C_DIR ]; then
+            cd $C_DIR
+            svn up &> $T_DIR/svn.log
+
+            if [ `wc -l < $T_DIR/svn.log` -ne "1" ]; then
+                echo Source updates require install and distribution | tee -a $BUILDLOG
+                INSTALL="yes"
+            fi
+        else
+            echo Chandler install required | tee -a $BUILDLOG
+            INSTALL="yes"
+            cd $T_DIR
+            svn co http://svn.osafoundation.org/chandler/trunk/chandler &> $T_DIR/svn.log
+        fi
+
+        if [ "$INSTALL" = "yes" ]; then
+            for mode in $MODES ; do
+                if [ "$mode" = "debug" ]; then
+                    SNAP="DEBUG=1"
+                else
+                    SNAP=
+                fi
+
+                echo Running \"make -C $C_DIR $SNAP install strip\" | tee -a $BUILDLOG
+                make -C $C_DIR $SNAP install strip &> $T_DIR/install.log
+
+                if [ ! "$CHANDLER_PERFORMANCE_TEST" = "yes" ]; then
+                    echo do distribution step | tee -a $BUILDLOG
+                fi
+            done
+        else
+            echo Source has not changed - skipping install and distribution | tee -a $BUILDLOG
+        fi
+
+          # brute force remove all variations of the last line of the svn log
+          # until only the revision number remains
+        REVISION=`tail -n 1 $T_DIR/svn.log`
+        REVISION=${REVISION/At revision /}
+        REVISION=${REVISION/Updated to revision /}
+        REVISION=${REVISION/./}
+
+          # remove any reference to buildRevision (only if at start of line)
+        sed -i "/^buildRevision/d" $C_DIR/version.py
+        echo buildRevision = \"$REVISION\" >> $C_DIR/version.py
+    fi
+
+    echo Running \"make -C $C_DIR cats\" | tee -a $BUILDLOG
+
+    make -C $C_DIR cats
+
+    DIRS=`find $C_DIR -type d -name tests -print`
+
+      # this code walks thru all the dirs with "tests" in their name
+      # and then compares them to the exclude dir array by
+      # taking the substring of the L_EXCLUDE length value
+      # if there is a match, the loop is broken and the dir is skipped
+
+    for item in $DIRS ; do
+        FILEPATH=${item%/*}
+        EXCLUDED=no
+        for index in 0 1 2 3 ; do
+            exclude=${EXCLUDES[$index]}
+            len=${L_EXCLUDES[$index]}
+
+            if [ "${FILEPATH:0:$len}" = "$exclude" ]; then
+                EXCLUDED=yes
+                break;
+            fi
+        done         
+        if [ "$EXCLUDED" = "no" ]; then
+            TESTDIRS="$TESTDIRS $item"
+        fi
+    done
+
+      # Set TEST_RESULT to initial value of "ok"
+      # if any test fails it will be set to "failed" 
+
+    TEST_RESULT="ok"
+
+      # walk thru all of the test dirs and find the test files
+
+    for mode in $MODES ; do
+        echo Running $mode unit tests | tee -a $BUILDLOG
+
+        for testdir in $TESTDIRS ; do
+            TESTS=`find $testdir -name 'Test*.py' -print`
+
+            for test in $TESTS ; do
+                if [ "$OSTYPE" = "cygwin" ]; then
+                    TESTNAME=`cygpath -w $test`
+                else
+                    TESTNAME=$test
+                fi
+
+                echo Running $TESTNAME | tee -a $BUILDLOG
+
+                cd $C_DIR
+                echo ./$mode/$RUN_PYTHON $TESTNAME &> $T_DIR/test.log
+                cat $T_DIR/test.log $T_DIR/tests.log
+            
+                  # scan the test output for the success messge "OK"
+                RESULT=`grep '^OK$' $T_DIR/test.log`
+
+                echo $TESTNAME [$RESULT] >> $T_DIR/foo_test.log
+                if [ "$RESULT" != "OK" ]; then
+                    TEST_RESULT="failed"
+                fi
+            done
+        done
+
+          # if Functional Tests are needed - walk the CATS directory
+          # and create a list of all valid tests
+
+        echo Running $mode functional tests | tee -a $BUILDLOG
+
+        if [ ! "$CHANDLER_FUNCTIONAL_TEST" = "no" ]; then
+            TESTS=`find $C_DIR/util/QATestScripts/Functional -name 'Test*.py' -print`
+
+            for test in $TESTS ; do
+                if [ "$OSTYPE" = "cygwin" ]; then
+                    TESTNAME=`cygpath -w $test`
+                    P_DIR=`cygpath -w $C_DIR`
+                else
+                    TESTNAME=$test
+                    P_DIR=$C_DIR
+                fi
+
+                echo Running $TESTNAME | tee -a $BUILDLOG
+
+                cd $C_DIR
+                echo ./$mode/$RUN_CHANDLER --create --profileDir="$P_DIR" --scriptFile="$TESTNAME" &> $T_DIR/test.log
+                cat $T_DIR/test.log >> $T_DIR/tests.log
+            
+                  # functional tests output a #TINDERBOX# Status = SUCCESS that we can scan for
+                RESULT=`grep "#TINDERBOX# Status = SUCCESS" $T_DIR/test.log`
+
+                echo $TESTNAME [$RESULT] >> $T_DIR/foo_test.log
+                if [ ! "$RESULT" = "#TINDERBOX# Status = SUCCESS" ]; then
+                    TEST_RESULT="failed"
+                fi
+            done
+        fi
+    done
+
+      # if Performance Tests are needed - walk the CATS directory
+      # and create a list of all valid tests
+
+    if [ "$CHANDLER_PERFORMANCE_TEST" = "yes" ]; then
+        echo Running performance tests | tee -a $BUILDLOG
+
+        TESTS=`find $C_DIR/util/QATestScripts/Performance -name 'Perf*.py' -print`
+
+        for test in $TESTS ; do
+            if [ "$OSTYPE" = "cygwin" ]; then
+                TESTNAME=`cygpath -w $test`
+                P_DIR=`cygpath -w $C_DIR`
+            else
+                TESTNAME=$test
+                P_DIR=$C_DIR
+            fi
+
+            echo Running $TESTNAME | tee -a $BUILDLOG
+
+            cd $C_DIR
+            echo ./release/$RUN_CHANDLER --create --profileDir="$P_DIR" --scriptFile="$TESTNAME" &> $T_DIR/test.log
+            cat $T_DIR/test.log >> $T_DIR/tests.log
+
+              # performance tests output a #TINDERBOX# Status = SUCCESS that we can scan for
+            RESULT=`grep "#TINDERBOX# Status = SUCCESS" $T_DIR/test.log`
+
+            echo $TESTNAME [$RESULT] >> $T_DIR/foo_test.log
+            if [ "$RESULT" = "#TINDERBOX# Status = SUCCESS" ]; then
+                TEST_RESULT="failed"
+            fi
+        done
+    fi
+
+    echo - - - - svn.log - - - - - - - - - - - - - - - - - - - - - - | tee -a $BUILDLOG
+    cat $T_DIR/svn.log                                               | tee -a $BUILDLOG
+    echo - - - - install.log - - - - - - - - - - - - - - - - - - - - | tee -a $BUILDLOG
+    cat $T_DIR/install.log                                           | tee -a $BUILDLOG
+    echo - - - - distrib.log - - - - - - - - - - - - - - - - - - - - | tee -a $BUILDLOG
+    cat $T_DIR/distrib.log                                           | tee -a $BUILDLOG
+    echo - - - - tests.log - - - - - - - - - - - - - - - - - - - - - | tee -a $BUILDLOG
+    cat $T_DIR/tests.log                                             | tee -a $BUILDLOG
+    echo - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + | tee -a $BUILDLOG
+
+    if [ "$TBOX" = "yes" ]; then
+          # need to add code to figure out if what the final status is
+        if [ "$TEST_RESULT" = "failed" ]; then
+            TBOX_STATUS=test_failed
+        else
+            TBOX_STATUS=success
+        fi
+
+        $HH_DIR/tbox_update.py -t $TBOX_TREE -b $TBOX_BUILD -s $TBOX_STATUS -f tbox_$BUILDID.log -p $T_DIR -T bear -D "@code-bear.com"
+    fi
+
+    if [ "$TBOX" = "yes" ]; then
+        if [ "$TBOX_CYCLE" != "yes" ]; then
+            ENDLOOP="yes"
+        fi
+    else
+        ENDLOOP="yes"
+    fi
+    
+    if [ "$ENDLOOP" = "no" ]; then
+        echo Sleeping for $SLEEP_MINUTES minutes | tee -a $BUILDLOG
+
+        SLEEP_INTERVAL=`expr $SLEEP_MINUTES \* 60`
+        sleep $SLEEP_INTERVAL | tee -a $BUILDLOG
+    fi
+done
---
+++ hardhat/tbox_update.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+# Helper program to send tinderbox2 status emails
+
+__author__    = "Mike Taylor <bear@osafoundation.org>"
+__copyright__ = "Copyright (c) 2005 Open Source Applications Foundation"
+__version__   = "1.0"
+__svn__       = "$Id: $"
+
+import os, time, smtplib
+import ConfigParser, optparse
+
+def initOptions():
+    """
+    Load and parse the command line options, with overrides in **kwds.
+    Returns options
+    """
+    #XXX i18n parcelPath, profileDir could have non-ascii paths
+    # option name, (value, short cmd, long cmd, type flag, default, help text)
+    _configItems = {'tree':     ('-t', '--tree',     's', 'Chandler', 'Tree Name'),
+                    'build':    ('-b', '--build',    's', None, 'Build Name'),
+                    'logfile':  ('-f', '--logfile',  's', None, 'Build Log filename'),
+                    'logpath':  ('-p', '--logpath',  's', None, 'Build Log path'),
+                    'status':   ('-s', '--status',   's', None, 'Build Status'),
+                    'toAddr':   ('-T', '--toaddr',   's', 'buildreport',        'Address (name) to send email'),
+                    'toDomain': ('-D', '--todomain', 's', '@osafoundation.org', 'Address (domain) to send email'),
+                   }
+
+    # %prog expands to os.path.basename(sys.argv[0])
+    usage  = "usage: %prog [options]"
+    parser = optparse.OptionParser(usage=usage, version="%prog " + __version__)
+
+    for key in _configItems:
+        (shortCmd, longCmd, optionType, defaultValue, helpText) = _configItems[key]
+
+        if optionType == 'b':
+            parser.add_option(shortCmd,
+                              longCmd,
+                              dest=key,
+                              action='store_true',
+                              default=defaultValue,
+                              help=helpText)
+        else:
+            parser.add_option(shortCmd,
+                              longCmd,
+                              dest=key,
+                              default=defaultValue,
+                              help=helpText)
+
+    (options, args) = parser.parse_args()
+    print options
+    return options
+
+
+def run(options):
+    treeName     = options.tree
+    buildName    = options.build
+    fromAddr     = "builds@osafoundation.org"
+    toAddr       = options.toAddr
+    toDomain     = options.toDomain
+    smtpOutbound = "smtp.osafoundation.org"
+    logFile      = options.logfile
+    logPath      = options.logpath
+    status       = options.status
+    logData      = None
+
+    if treeName is None:
+        print "Tree name must be specified"
+        return
+    if buildName is None:
+        print "Build name must be specified"
+        return
+    if logPath is None:
+        logPath = os.getcwd()
+
+    if logFile:
+        filename = os.path.join(logPath, logFile)
+    
+        if os.path.isfile(filename):
+            try:
+                hFile   = file(os.path.join(logPath, logFile), 'r')
+                logData = hFile.readlines() 
+
+                hFile.close()
+
+            except Exception, e:
+                logData = None
+                print "[silk_tbox] - Unable to import %s%s [%s]\n" % (logPath, logFile, str(e))
+        else:
+            print "Log file %s not found" % filename
+
+    nowTime  = str(int(time.time()))
+    subject  = "[tinderbox] %s from %s" % (status, buildName)
+
+    msg  = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (fromAddr, toAddr, subject))
+    msg += "tinderbox: tree: %s\n" % treeName
+    msg += "tinderbox: buildname: %s\n" % buildName
+    msg += "tinderbox: starttime: %s\n" % nowTime
+    msg += "tinderbox: timenow: %s\n" % nowTime
+    msg += "tinderbox: errorparser: linux\n"
+    msg += "tinderbox: status: %s\n" % status
+    msg += "tinderbox: END\n\n"
+
+    if logData:
+        msg += "".join(logData)
+
+    print "Sending email to tinderbox server at %s" % toAddr
+
+    try:
+        server = smtplib.SMTP(smtpOutbound)
+        server.sendmail(fromAddr, toAddr, msg)
+        server.quit()
+
+    except Exception, e:
+        print "SendMail error", e
+
+if __name__ == "__main__":
+    options = initOptions()
+    run(options)
