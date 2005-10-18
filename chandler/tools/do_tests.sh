1cd90a4c7bf64c233abdd6e8accbdd2106c599db
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-10-18 21:35:20 +0000

    Added command line parameters to the script First pass at the changes needed for bug 4331 New command line parameters:     -f run the functional test suite     -u run the unit tests     -p run the performance tests     -t <testname> run only the given test (wildcards can be used) And at the end of the run the script summarizes any failed tests.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@7948 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/do_tests.sh
+++ chandler/tools/do_tests.sh
@@ -11,22 +11,57 @@
 # if CHANDLER_FUNCTIONAL_TEST=no then CATS Functional Tests are skipped
 #
 
-USAGE="Usage: `basename $0` chandler-base-path"
+NO_ARGS=0 
+E_OPTERROR=65
+
+USAGE="Usage: `basename $0` [-fpu] [-t test_name] chandler-base-path"
 
 if [ ! -n "$1" ]; then
     echo $USAGE
-    echo if CHANDLER_FUNCTIONAL_TEST=no then CATS Functional Tests are skipped
-    echo if CHANDLER_PERFORMANCE_TEST=yes then CATS Performance Tests are run
-    exit 65
-else
-    C_DIR="$1"
-    T_DIR=$C_DIR
+    echo if CHANDLER_FUNCTIONAL_TEST=yes or -f then CATS Functional Tests are run
+    echo if CHANDLER_PERFORMANCE_TEST=yes or -p then CATS Performance Tests are run
+    echo if CHANDLER_UNIT_TEST=yes or -u then Chandler Unit Tests are run
+    echo "if a specific test name or (pattern) is given using -t then only that test name will be run"
+    exit E_OPTERROR
+fi
 
-    if [ ! -d "$C_DIR/i18n" ]; then
-        echo Error: The path [$C_DIR] given does not point to a chandler/ directory
-        echo $USAGE
-        exit 65
-    fi
+if [ "$CHANDLER_FUNCTIONAL_TEST" = "yes" ]; then
+    RUN_FUNCTIONAL=yes
+else
+    RUN_FUNCTIONAL=no
+fi
+if [ "$CHANDLER_PERFORMANCE_TEST" = "yes" ]; then
+    RUN_PERFORMANCE=yes
+else
+    RUN_PERFORMANCE=no
+fi
+if [ "$CHANDLER_UNIT_TEST" = "yes" ]; then
+    RUN_UNIT=yes
+else
+    RUN_UNIT=no
+fi
+
+while getopts ":fput:" Option
+do
+  case $Option in
+    f ) RUN_FUNCTIONAL=yes;;
+    p ) RUN_PERFORMANCE=yes;;
+    u ) RUN_UNIT=yes;;
+    t ) TEST_TO_RUN=$OPTARG;;
+    * ) echo "Unimplemented option chosen.";;   # DEFAULT
+  esac
+done
+
+  # leave any remaining command line parameters on the command line
+shift $(($OPTIND - 1))
+
+C_DIR="$1"
+T_DIR=$C_DIR
+
+if [ ! -d "$C_DIR/i18n" ]; then
+    echo Error: The path [$C_DIR] given does not point to a chandler/ directory
+    echo $USAGE
+    exit 65
 fi
 
 if [ "$CHANDLERBIN" = "" ]
@@ -35,11 +70,12 @@ then
 fi
 
 HH_DIR=`pwd`
-MODES="release debug"
+TESTLOG="$C_DIR/do_tests.log"
+FAILED_TESTS=""
 
-echo - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + 
-echo Started `date`                                              
-echo Setting up script environment                               
+echo - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + | tee -a $TESTLOG
+echo Started `date`                                              | tee -a $TESTLOG
+echo Setting up script environment                               | tee -a $TESTLOG
 
 if [ "$OSTYPE" = "cygwin" ]; then
     RUN_CHANDLER=RunChandler.bat
@@ -52,7 +88,9 @@ fi
   # if the debug/ path is not found, then avoid debug tests
 if [ ! -d $C_DIR/debug ]; then
     MODES="release"
-    echo Skipping debug tests as $C_DIR/debug does not exist 
+    echo Skipping debug tests as $C_DIR/debug does not exist | tee -a $TESTLOG
+else
+    MODES="release debug"
 fi
 
   # each directory to exclude should be place in the EXCLUDES array
@@ -66,94 +104,181 @@ for item in 0 1 2 3 ; do
     L_EXCLUDES[$item]=${#EXCLUDES[$item]}
 done
 
-DIRS=`find $C_DIR -type d -name tests -print`
+  # if a specific test name has been given then
+  # find that test and run it
 
-  # this code walks thru all the dirs with "tests" in their name
-  # and then compares them to the exclude dir array by
-  # taking the substring of the L_EXCLUDE length value
-  # if there is a match, the loop is broken and the dir is skipped
+if [ -n "$TEST_TO_RUN" ]; then
+    DIRS=`find $C_DIR -name $TEST_TO_RUN -print`
 
-for item in $DIRS ; do
-    FILEPATH=${item%/*}
-    EXCLUDED=no
-    for index in 0 1 2 3 ; do
-        exclude=${EXCLUDES[$index]}
-        len=${L_EXCLUDES[$index]}
+    for mode in $MODES ; do
+        echo Running $mode | tee -a $TESTLOG
 
-        if [ "${FILEPATH:0:$len}" = "$exclude" ]; then
-            EXCLUDED=yes
-            break;
-        fi
-    done         
-    if [ "$EXCLUDED" = "no" ]; then
-        TESTDIRS="$TESTDIRS $item"
-    fi
-done
-
-  # walk thru all of the test dirs and find the test files
-
-for mode in $MODES ; do
-    echo Running $mode unit tests 
-
-    for testdir in $TESTDIRS ; do
-        TESTS=`find $testdir -name 'Test*.py' -print`
-
-        for test in $TESTS ; do
+        for test in $DIRS ; do
             if [ "$OSTYPE" = "cygwin" ]; then
                 TESTNAME=`cygpath -w $test`
             else
                 TESTNAME=$test
             fi
 
-            echo Running $TESTNAME
+            echo Running $TESTNAME | tee -a $TESTLOG
 
             cd $C_DIR
-            $CHANDLERBIN/$mode/$RUN_PYTHON $TESTNAME
+
+            if echo "$TESTNAME" | grep -q "QATestScripts/" ; then
+                $CHANDLERBIN/release/$RUN_CHANDLER --create --profileDir="$P_DIR" --scriptFile="$TESTNAME" &> $C_DIR/test.log
+                SUCCESS="#TINDERBOX# Status = PASSED"
+            else
+                $CHANDLERBIN/$mode/$RUN_PYTHON $TESTNAME &> $C_DIR/test.log
+                SUCCESS="^OK"
+            fi
+
+            echo - - - - - - - - - - - - - - - - - - - - - - - - - - | tee -a $TESTLOG
+            cat $C_DIR/test.log | tee -a $TESTLOG
+
+            if echo "$SUCCESS" | grep $C_DIR/test.log ; then
+                FAILED_TESTS="$FAILED_TESTS ($mode)$TESTNAME"
+            fi
         done
     done
+else
+    if [ "$RUN_UNIT" = "yes" ]; then
+        DIRS=`find $C_DIR -type d -name tests -print`
+
+          # this code walks thru all the dirs with "tests" in their name
+          # and then compares them to the exclude dir array by
+          # taking the substring of the L_EXCLUDE length value
+          # if there is a match, the loop is broken and the dir is skipped
+
+        for item in $DIRS ; do
+            FILEPATH=${item%/*}
+            EXCLUDED=no
+            for index in 0 1 2 3 ; do
+                exclude=${EXCLUDES[$index]}
+                len=${L_EXCLUDES[$index]}
+
+                if [ "${FILEPATH:0:$len}" = "$exclude" ]; then
+                    EXCLUDED=yes
+                    break;
+                fi
+            done         
+            if [ "$EXCLUDED" = "no" ]; then
+                TESTDIRS="$TESTDIRS $item"
+            fi
+        done
+
+          # walk thru all of the test dirs and find the test files
+
+        for mode in $MODES ; do
+            echo Running $mode unit tests | tee -a $TESTLOG
+
+            for testdir in $TESTDIRS ; do
+                TESTS=`find $testdir -name 'Test*.py' -print`
+
+                for test in $TESTS ; do
+                    if [ "$OSTYPE" = "cygwin" ]; then
+                        TESTNAME=`cygpath -w $test`
+                    else
+                        TESTNAME=$test
+                    fi
+
+                    echo Running $TESTNAME | tee -a $TESTLOG
+
+                    cd $C_DIR
+                    $CHANDLERBIN/$mode/$RUN_PYTHON $TESTNAME &> $C_DIR/test.log
+
+                      # scan the test output for the success messge "OK"
+                    RESULT=`grep '^OK' $C_DIR/test.log`
+
+                    echo - - - - - - - - - - - - - - - - - - - - - - - - - - | tee -a $TESTLOG
+                    echo $TESTNAME [$RESULT] | tee -a $TESTLOG
+                    cat $C_DIR/test.log      | tee -a $TESTLOG
+
+                    if [ "$RESULT" != "OK" ]; then
+                        FAILED_TESTS="$FAILED_TESTS ($mode)$TESTNAME"
+                    fi
+                done
+            done
+        done
+    fi
 
       # if Functional Tests are needed - find the FunctionalTestSuite and run it
 
-    echo Running $mode functional tests 
+    if [ "$RUN_FUNCTIONAL" = "yes" ]; then
+        echo Running $mode functional tests | tee -a $TESTLOG
 
-    if [ ! "$CHANDLER_FUNCTIONAL_TEST" = "no" ]; then
-        test="$C_DIR/tools/QATestScripts/Functional/FunctionalTestSuite.py"
-        
-        if [ "$OSTYPE" = "cygwin" ]; then
-            TESTNAME=`cygpath -w $test`
-            P_DIR=`cygpath -w $C_DIR`
-        else
-            TESTNAME=$test
-            P_DIR=$C_DIR
-        fi
+        for mode in $MODES ; do
+            test="$C_DIR/tools/QATestScripts/Functional/FunctionalTestSuite.py"
 
-        echo Running $TESTNAME
+            if [ "$OSTYPE" = "cygwin" ]; then
+                TESTNAME=`cygpath -w $test`
+                P_DIR=`cygpath -w $C_DIR`
+            else
+                TESTNAME=$test
+                P_DIR=$C_DIR
+            fi
 
-        cd $C_DIR
-        $CHANDLERBIN/$mode/$RUN_CHANDLER --create --profileDir="$P_DIR" --scriptFile="$TESTNAME"
+            echo Running $TESTNAME | tee -a $TESTLOG
+
+            cd $C_DIR
+            $CHANDLERBIN/$mode/$RUN_CHANDLER --create --profileDir="$P_DIR" --scriptFile="$TESTNAME" &> $C_DIR/test.log
+
+              # scan the test output for the success messge "OK"
+            RESULT=`grep '#TINDERBOX# Status = PASSED' $C_DIR/test.log`
+
+            echo - - - - - - - - - - - - - - - - - - - - - - - - - - | tee -a $TESTLOG
+            echo $TESTNAME [$RESULT] | tee -a $TESTLOG
+            cat $C_DIR/test.log      | tee -a $TESTLOG
+
+            if [ "$RESULT" != "#TINDERBOX# Status = PASSED" ]; then
+                FAILED_TESTS="$FAILED_TESTS ($mode)$TESTNAME"
+            fi
+        done
     fi
-done
 
-  # if Performance Tests are needed - walk the CATS directory
-  # and create a list of all valid tests
+      # if Performance Tests are needed - walk the CATS directory
+      # and create a list of all valid tests
 
-if [ "$CHANDLER_PERFORMANCE_TEST" = "yes" ]; then
-    echo Running performance tests 
+    if [ "$RUN_PERFORMANCE" = "yes" ]; then
+        echo Running performance tests | tee -a $TESTLOG
 
-    TESTS=`find $C_DIR/tools/QATestScripts/Performance -name 'Perf*.py' -print`
+        TESTS=`find $C_DIR/tools/QATestScripts/Performance -name 'Perf*.py' -print`
 
-    for test in $TESTS ; do
-        if [ "$OSTYPE" = "cygwin" ]; then
-            TESTNAME=`cygpath -w $test`
-            P_DIR=`cygpath -w $C_DIR`
-        else
-            TESTNAME=$test
-            P_DIR=$C_DIR
-        fi
+        for test in $TESTS ; do
+            if [ "$OSTYPE" = "cygwin" ]; then
+                TESTNAME=`cygpath -w $test`
+                P_DIR=`cygpath -w $C_DIR`
+            else
+                TESTNAME=$test
+                P_DIR=$C_DIR
+            fi
 
-        echo Running $TESTNAME
+            echo Running $TESTNAME | tee -a $TESTLOG
 
-        cd $C_DIR
-        $CHANDLERBIN/release/$RUN_CHANDLER --create --profileDir="$P_DIR" --scriptFile="$TESTNAME"
+            cd $C_DIR
+            $CHANDLERBIN/release/$RUN_CHANDLER --create --profileDir="$P_DIR" --scriptFile="$TESTNAME" &> $C_DIR/test.log
+
+              # scan the test output for the success messge "OK"
+            RESULT=`grep '#TINDERBOX# Status = PASSED' $C_DIR/test.log`
+
+            echo - - - - - - - - - - - - - - - - - - - - - - - - - - | tee -a $TESTLOG
+            echo $TESTNAME [$RESULT] | tee -a $TESTLOG
+            cat $C_DIR/test.log      | tee -a $TESTLOG
+
+            if [ "$RESULT" != "#TINDERBOX# Status = PASSED" ]; then
+                FAILED_TESTS="$FAILED_TESTS $TESTNAME"
+            fi
+        done
+    fi
+fi
+
+echo - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + | tee -a $TESTLOG
+
+if [ -n "$FAILED_TESTS" ]; then
+    echo The following tests failed | tee -a $TESTLOG
+
+    for item in $FAILED_TESTS ; do
+        echo "    $item" | tee -a $TESTLOG
     done
+else
+    echo All tests passed | tee -a $TESTLOG
 fi
