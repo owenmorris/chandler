43ec45bd1519334fa54f35ea0dc700ba1d97d890
vajda <vajda@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-10-31 23:45:47 +0000

    - fixed bug http://bugzilla.osafoundation.org/show_bug.cgi?id=4430
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8138 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/persistence/RepositoryError.py
+++ chandler/repository/persistence/RepositoryError.py
@@ -4,12 +4,15 @@ __date__      = "$Date$"
 __copyright__ = "Copyright (c) 2004 Open Source Applications Foundation"
 __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
-
 import threading
+
+#
+# __doc__ strings need to be explicitely set in order not to be removed by -OO
+#
 
 
 class RepositoryError(ValueError):
-    "All repository related exceptions go here"
+    __doc__ = "All repository related exceptions go here"
 
 
 class RepositoryLockNotGrantedError:
@@ -26,7 +29,7 @@ class RepositoryPasswordError(RepositoryError):
 
 
 class VersionConflictError(RepositoryError):
-    "Another view changed %s and saved those changes before this view - %s - got a chance to do so. These changes conflict with this thread's changes, the item cannot be saved (0x%0.4x/0x%0.4x)."
+    __doc__ = "Another view changed %s and saved those changes before this view - %s - got a chance to do so. These changes conflict with this thread's changes, the item cannot be saved (0x%0.4x/0x%0.4x)."
 
     def __str__(self):
         return self.__doc__ %(self.args[0].itsPath, self.args[0].itsView,
@@ -42,21 +45,21 @@ class RepositoryVersionError(RepositoryError):
         return self.__doc__ %(self.args[0], self.args[1])
 
 class RepositoryFormatVersionError(RepositoryVersionError):
-    "Repository format version mismatch, expected version 0x%08x, but got 0x%08x"
+    __doc__ = "Repository format version mismatch, expected version 0x%08x, but got 0x%08x"
 
 class RepositorySchemaVersionError(RepositoryVersionError):
-    "Repository core schema version mismatch, expected version 0x%08x, but got 0x%08x"
+    __doc__ = "Repository core schema version mismatch, expected version 0x%08x, but got 0x%08x"
 
 
 class NoSuchItemError(RepositoryError):
-    "No such item %s, version %d"
+    __doc__ = "No such item %s, version %d"
 
     def __str__(self):
         return self.__doc__ % (self.args[0], self.args[1])
 
 
 class MergeError(VersionConflictError):
-    "(%s) merging %s failed because %s, reason code: %s"
+    __doc__ = "(%s) merging %s failed because %s, reason code: %s"
 
     def __str__(self):
         return self.__doc__ %(self.args[0], self.args[1].itsPath, self.args[2],
@@ -87,55 +90,55 @@ class MergeError(VersionConflictError):
 
 
 class LoadError(RepositoryError):
-    "While loading %s, %s"
+    __doc__ = "While loading %s, %s"
 
     def __str__(self):
         return self.__doc__ %(self.args[0], self.args[1])
 
 
 class RecursiveLoadItemError(LoadError):
-    "Item %s is already being loaded"
+    __doc__ = "Item %s is already being loaded"
 
     def __str__(self):
         return self.__doc__ %(self.args[0])
 
 
 class LoadValueError(LoadError):
-    "While loading %s.%s, %s"
+    __doc__ = "While loading %s.%s, %s"
 
     def __str__(self):
         return self.__doc__ %(self.args[0], self.args[1], self.args[2])
 
 
 class SaveError(RepositoryError):
-    "While saving %s, %s"
+    __doc__ = "While saving %s, %s"
 
     def __str__(self):
         return self.__doc__ %(self.args[0], self.args[1])
 
 
 class SaveValueError(RepositoryError):
-    "While saving value for '%s' on %s: %s"
+    __doc__ = "While saving value for '%s' on %s: %s"
 
     def __str__(self):
         return self.__doc__ %(self.args[1], self.args[0]._repr_(), self.args[2])
 
 
 class ItemImportError(RepositoryError):
-    "While importing %s into %s, %s"
+    __doc__ = "While importing %s into %s, %s"
 
     def __str__(self):
         return self.__doc__ %(self.args[0], self.args[1], self.args[2])
 
 
 class ImportParentError(ItemImportError):
-    "No matching import parent %s for %s found"
+    __doc__ = "No matching import parent %s for %s found"
 
     def __str__(self):
         return self.__doc__ %(self.args[0].itsPath, self.args[1]._repr_())
 
 class ImportKindError(ItemImportError):
-    "No matching import kind %s for %s found"
+    __doc__ = "No matching import kind %s for %s found"
 
     def __str__(self):
         return self.__doc__ %(self.args[0].itsPath, self.args[1]._repr_())
