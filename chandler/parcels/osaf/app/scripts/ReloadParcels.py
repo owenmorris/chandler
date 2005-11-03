e564b6746cc229acbed6097a41982df218b1ee5f
donn <donn@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-11-03 01:19:10 +0000

    Fix bug 4530 - the launch chandler unit test hangs * This bug was caused by bug 4554 - ReloadParcels on the Calendar view destroys the calendar view * Fixed by making a change to the ReloadParcels test script to workaround 4554
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8164 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/app/scripts/ReloadParcels.py
+++ chandler/parcels/osaf/app/scripts/ReloadParcels.py
@@ -1,3 +1,6 @@
 # This is the StartupTest script, used to test Chandler at start up time.
 App_ns = app_ns()
+
+# workaround bug 4554 - do the reload in the All view, not the Calendar view
+App_ns.root.ApplicationBarAll()
 App_ns.root.ReloadParcels()
