82f85cdfaf0f90c9ab07987b267193552466a27d
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-11-10 01:16:56 +0000

    Scripts for Bug 4631 and Bug 4619. Add perf scripts for resizing app and moving calendar by a week.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8228 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/tools/QATestScripts/Performance/PerfLargeDataJumpWeek.py
@@ -0,0 +1,40 @@
+import tools.QAUITestAppLib as QAUITestAppLib
+import osaf.framework.scripting as scripting
+
+from datetime import datetime
+
+fileName = "PerfLargeDataJumpWeek.log"
+logger = QAUITestAppLib.QALogger(fileName, "Jump from one week to another")
+
+App_ns = app_ns()
+
+try:
+    # Test Phase: Initialization
+
+    # Start at the same date every time
+    testdate = datetime(2005, 11, 27)
+    App_ns.root.SelectedDateChanged(start=testdate)
+
+    # Load a large calendar
+    testView = QAUITestAppLib.UITestView(logger, u'Generated3000.ics')
+    scripting.User.idle()
+
+    # Test Phase: Action
+
+    logger.Start("Jump calendar by one week")
+    testdate = datetime(2005, 12, 4)
+    App_ns.root.SelectedDateChanged(start=testdate)
+    scripting.User.idle()
+    logger.Stop()
+
+    # Test Phase: Verification
+
+    logger.SetChecked(True)
+    if App_ns.calendar.calendarControl.rangeStart == testdate:
+        logger.ReportPass("Jump calendar by one week")
+    else:
+        logger.ReportFailure("Jump calendar by one week")
+    logger.Report("Jump calendar by one week")
+        
+finally:
+    logger.Close()
---
+++ chandler/tools/QATestScripts/Performance/PerfLargeDataResizeCalendar.py
@@ -0,0 +1,45 @@
+import tools.QAUITestAppLib as QAUITestAppLib
+import osaf.framework.scripting as scripting
+
+from datetime import datetime
+
+fileName = "PerfLargeDataResizeCalendar.log"
+logger = QAUITestAppLib.QALogger(fileName, "Resize app in calendar mode")
+
+App_ns = app_ns()
+
+try:
+    # Test Phase: Initialization
+
+    # Start at the same date every time
+    testdate = datetime(2005, 11, 27)
+    App_ns.root.SelectedDateChanged(start=testdate)
+
+    frame = App_ns.root.widget.GetParent()
+    (x, y) = frame.GetSize()
+    x += 20
+    y += 20
+
+    # Load a large calendar
+    testView = QAUITestAppLib.UITestView(logger, u'Generated3000.ics')
+    scripting.User.idle()
+
+    # Test Phase: Action
+
+    logger.Start("Resize app in calendar mode")
+    frame.SetSize((x, y))
+    scripting.User.idle()
+    logger.Stop()
+
+    # Test Phase: Verification
+
+    logger.SetChecked(True)
+    (bigx, bigy) = frame.GetSize()
+    if (bigx == x and bigy == y):
+        logger.ReportPass("Resize app in calendar mode")
+    else:
+        logger.ReportFailure("Resize app in calendar mode")
+    logger.Report("Resize app in calendar mode")
+        
+finally:
+    logger.Close()
