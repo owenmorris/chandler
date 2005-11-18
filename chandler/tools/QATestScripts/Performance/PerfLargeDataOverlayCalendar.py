07681e0796145bd067282413ce1bb150dab37982
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-11-18 18:58:21 +0000

    Add perf script to measure overlaying a big calendar. Bug 4692. r=donn
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8350 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/tools/QATestScripts/Performance/PerfLargeDataOverlayCalendar.py
@@ -0,0 +1,38 @@
+import tools.QAUITestAppLib as QAUITestAppLib
+
+from datetime import datetime
+
+fileName = "PerfLargeDataOverlayCalendar.log"
+logger = QAUITestAppLib.QALogger(fileName, "Overlay calendar")
+
+App_ns = app_ns()
+
+try:
+    # Test Phase: Initialization
+
+    # Start at the same date every time
+    testdate = datetime(2005, 11, 27)
+    App_ns.root.SelectedDateChanged(start=testdate)
+
+    # Load a large calendar
+    testView = QAUITestAppLib.UITestView(logger, u'Generated3000.ics')
+    User.idle()
+
+    # Test Phase: Action
+
+    logger.Start("Overlay calendar")
+    clickSucceeded = User.emulate_sidebarClick(App_ns.sidebar, "Generated3000")
+    User.idle()
+    logger.Stop()
+
+    # Test Phase: Verification
+
+    logger.SetChecked(True)
+    if clickSucceeded:
+        logger.ReportPass("Overlay calendar")
+    else:
+        logger.ReportFailure("Overlay calendar")
+    logger.Report("OverlayCalendar")
+        
+finally:
+    logger.Close()
