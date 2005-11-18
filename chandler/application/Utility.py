b03bbe65e65b423ab686d93b4ad29abb9ff8f5e2
stearns <stearns@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-11-18 22:15:43 +0000

    Oops - my attempt to avoid a schema bump in rev 8356 backfired, so here it is (it caused bug 4727, which this fixes).
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8359 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -24,7 +24,7 @@ import i18n
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "117" # jeffrey: add import_as_new preference
+SCHEMA_VERSION = "118" # stearns: changed SpacerBlock classname
 
 logger = None # initialized in initLogging()
 
