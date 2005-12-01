8f87af775b5c1218c5a0ec63dd255ffffece591c
vajda <vajda@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-12-01 23:02:09 +0000

    minor rename
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8480 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/repository/__init__.py
+++ chandler/repository/__init__.py
@@ -4,5 +4,5 @@ __date__      = "$Date$"
 __copyright__ = "Copyright (c) 2003-2004 Open Source Applications Foundation"
 __license__   = "http://osafoundation.org/Chandler_0.1_license_terms.htm"
 
-# The model package
+# The repository package
 
