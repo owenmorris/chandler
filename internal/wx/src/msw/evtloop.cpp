97027e357b1f641cded50ea301316cdcb95b7e93
davids <davids@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2005-12-19 02:27:27 +0000

    wx v2.7.0 incremental update
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8749 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/wx/src/msw/evtloop.cpp
+++ internal/wx/src/msw/evtloop.cpp
@@ -4,7 +4,7 @@
 // Author:      Vadim Zeitlin
 // Modified by:
 // Created:     01.06.01
-// RCS-ID:      $Id: evtloop.cpp,v 1.34 2005/09/25 11:14:10 VZ Exp $
+// RCS-ID:      $Id: evtloop.cpp,v 1.35 2005/12/19 01:39:54 KO Exp $
 // Copyright:   (c) 2001 Vadim Zeitlin <zeitlin@dptmaths.ens-cachan.fr>
 // License:     wxWindows licence
 ///////////////////////////////////////////////////////////////////////////////
@@ -124,23 +124,34 @@ bool wxEventLoop::IsChildOfCriticalWindow(wxWindowMSW *win)
 bool wxEventLoop::PreProcessMessage(WXMSG *msg)
 {
     HWND hwnd = msg->hwnd;
-    wxWindow * const wndThis = wxGetWindowFromHWND((WXHWND)hwnd);
+    wxWindow *wndThis = wxGetWindowFromHWND((WXHWND)hwnd);
     wxWindow *wnd;
 
-    // this may happen if the event occurred in a standard modeless dialog (the
-    // only example of which I know of is the find/replace dialog) - then call
-    // IsDialogMessage() to make TAB navigation in it work
+    // this might happen if we're in a modeless dialog, or if a wx control has
+    // children which themselves were not created by wx (i.e. wxActiveX control children)
     if ( !wndThis )
     {
-        // we need to find the dialog containing this control as
-        // IsDialogMessage() just eats all the messages (i.e. returns true for
-        // them) if we call it for the control itself
-        while ( hwnd && ::GetWindowLong(hwnd, GWL_STYLE) & WS_CHILD )
+        while ( hwnd && (::GetWindowLong(hwnd, GWL_STYLE) & WS_CHILD ))
         {
             hwnd = ::GetParent(hwnd);
+
+            // If the control has a wx parent, break and give the parent a chance
+            // to process the window message
+            wndThis = wxGetWindowFromHWND((WXHWND)hwnd);
+            if (wndThis != NULL)
+                break;
         }
 
-        return hwnd && ::IsDialogMessage(hwnd, msg) != 0;
+        if ( !wndThis )
+        {
+            // this may happen if the event occurred in a standard modeless dialog (the
+            // only example of which I know of is the find/replace dialog) - then call
+            // IsDialogMessage() to make TAB navigation in it work
+
+            // NOTE: IsDialogMessage() just eats all the messages (i.e. returns true for
+            // them) if we call it for the control itself
+            return hwnd && ::IsDialogMessage(hwnd, msg) != 0;
+        }
     }
 
     if ( !AllowProcessing(wndThis) )
