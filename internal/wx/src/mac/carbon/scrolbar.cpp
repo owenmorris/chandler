602aff57341314c9d7d444d79df3146c44646139
davids <davids@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-01-04 18:17:01 +0000

    wx v2.7.0 incremental update
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8882 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/wx/src/mac/carbon/scrolbar.cpp
+++ internal/wx/src/mac/carbon/scrolbar.cpp
@@ -4,7 +4,7 @@
 // Author:      Stefan Csomor
 // Modified by:
 // Created:     1998-01-01
-// RCS-ID:      $Id: scrolbar.cpp,v 1.38 2005/09/23 12:54:10 MR Exp $
+// RCS-ID:      $Id: scrolbar.cpp,v 1.39 2006/01/04 05:21:30 vell Exp $
 // Copyright:   (c) Stefan Csomor
 // Licence:       wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
