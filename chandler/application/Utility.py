ccd7d05f1fc02445ef1f7fe6aa7e97cc0644974f
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-01-05 22:39:14 +0000

    bump schema for preview pane change
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@8917 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -24,7 +24,7 @@ from repository.persistence.RepositoryError \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "127" # alecf: move calendar to use new selection mechanism
+SCHEMA_VERSION = "128" # alecf: add some calendar prefs
 
 logger = None # initialized in initLogging()
 
