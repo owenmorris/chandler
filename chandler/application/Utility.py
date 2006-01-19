a0bf32633cb78d7817adc84309b01d40f34ab618
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-01-19 00:34:39 +0000

    Forgot to update schema version when I modernized flickr parcel
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9090 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -24,7 +24,7 @@ from repository.persistence.RepositoryError \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "132" # john: Move some application specific events out of global events
+SCHEMA_VERSION = "133" # john: Modernize Flickr
 
 logger = None # initialized in initLogging()
 
