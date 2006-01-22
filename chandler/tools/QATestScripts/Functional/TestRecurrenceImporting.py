69dbbe2b04f5fda72cbe44304b0c370740de4d4f
aparna <aparna@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-01-22 02:39:43 +0000

    Added functional test for importing calendar with recurring events
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9130 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/tools/QATestScripts/Functional/TestRecurrenceImporting.py
@@ -0,0 +1,49 @@
+import osaf.sharing.Sharing as Sharing
+import osaf.sharing.ICalendar as ICalendar
+import tools.QAUITestAppLib as QAUITestAppLib
+import os, wx, sys
+import osaf.pim as pim
+
+App_ns = app_ns()
+
+# initialization
+fileName = "TestRecurrenceImporting.log"
+logger = QAUITestAppLib.QALogger(fileName, "TestRecurrenceImporting")
+
+try:
+    path = os.path.join(os.getenv('CHANDLERHOME'),"tools/QATestScripts/DataFiles")
+    # Upcast path to unicode since Sharing requires a unicode path
+    path = unicode(path, sys.getfilesystemencoding())
+    share = Sharing.OneTimeFileSystemShare(path, u'TestRecurrence.ics', ICalendar.ICalendarFormat, itsView=App_ns.itsView)
+    
+    logger.Start("Import calendar with recurring events")
+    try:
+        collection = share.get()
+    except:
+        logger.Stop()
+        logger.ReportFailure("Importing calendar: exception raised")
+    else:
+        App_ns.sidebarCollection.add(collection)
+        User.idle()
+        logger.Stop()
+        logger.ReportPass("Importing calendar")
+    
+    def VerifyEventCreation(title):
+        testEvent = App_ns.item_named(pim.CalendarEvent, title)
+        if testEvent is not None:
+            logger.ReportPass("Testing event creation: '%s'" % title)
+        else:
+            logger.ReportFailure("Testing event creation: '%s' not created" % title)
+    
+    User.idle()
+    VerifyEventCreation("Yearly Never End")
+    VerifyEventCreation("Monthly Meeting")
+    VerifyEventCreation("Multi-All Day")
+    VerifyEventCreation("All-day never end")
+    
+    logger.SetChecked(True)
+    logger.Report()
+finally:
+    # cleanup
+    logger.Close()
+    
