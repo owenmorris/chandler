da4891be076f4f93500f13aea335c12f0269310f
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-01-26 17:09:05 +0000

    Forgot to bump schema version after my checking last evening.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9183 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -24,7 +24,7 @@ from repository.persistence.RepositoryError \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "134" # john: More Flickr modernization
+SCHEMA_VERSION = "135" # john: mainblocks refactoring
 
 logger = None # initialized in initLogging()
 
