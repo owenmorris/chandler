e693eafe1e46d4f3995fb5642bfc55eb80e55e73
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-01-27 19:11:29 +0000

    - optimize get_divisions so that we only ever call key() once per key
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9204 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/util/divisions.py
+++ chandler/util/divisions.py
@@ -15,33 +15,40 @@ def get_divisions(l, key=None):
     """
 
     division_stack = [(0,len(l)-1)]
-
     divisions = [0]
 
-    # one version when there is a key, one without
+    # optimize for when you have/don't have a key() method
     if key is None:
-        def try_division(i,j):
-            if l[i] != l[j]:
-                if j-i == 1:
-                    divisions.append(j)
-                else:
-                    division_stack.append((i,j))
+        def get_value(k):
+            return k
     else:
-        def try_division(i,j):
-            assert i < j
+        value_cache = {}
+        def get_value(k):
+            """
+            memoize the results of calling key() because it is
+            potentially VERY expensive
+            """
+            # can't use get() because we're trying to avoid calls to key()
+            if value_cache.has_key(k):
+                result = value_cache[k]
+            else:
+                value_cache[k] = result = key(k)
+            return result
+                
+    def try_division(i,j):
+        assert i < j
 
-            if key(l[i]) != key(l[j]):
-                if j-i == 1:
-                    divisions.append(j)
-                else:
-                    division_stack.append((i,j))
-
+        if get_value(l[i]) != get_value(l[j]):
+            if j-i == 1:
+                divisions.append(j)
+            else:
+                division_stack.append((i,j))
+                
     while len(division_stack) != 0:
         index1, index2 = division_stack.pop()
 
         middle = index1 + (index2-index1)/2
 
-        counter += 1                    # for middle
         try_division(index1, middle)
         try_division(middle, index2)
 
