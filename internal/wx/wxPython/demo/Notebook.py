3e0578045154e614ad50ce21e8f40e055c8666e1
davids <davids@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-02-02 03:35:50 +0000

    wx v2.7.0 incremental update - wxPython updates from Robin
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9301 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/wx/wxPython/demo/Notebook.py
+++ internal/wx/wxPython/demo/Notebook.py
@@ -62,17 +62,11 @@ class TestNB(wx.Notebook):
         win = self.makeColorPanel(wx.CYAN)
         self.AddPage(win, "Cyan")
 
-#         win = self.makeColorPanel(wxWHITE)
-#         self.AddPage(win, "White")
+        win = self.makeColorPanel(wx.NamedColour('Midnight Blue'))
+        self.AddPage(win, "Midnight Blue")
 
-#         win = self.makeColorPanel(wxBLACK)
-#         self.AddPage(win, "Black")
-
-        win = self.makeColorPanel(wx.NamedColour('MIDNIGHT BLUE'))
-        self.AddPage(win, "MIDNIGHT BLUE")
-
-        win = self.makeColorPanel(wx.NamedColour('INDIAN RED'))
-        self.AddPage(win, "INDIAN RED")
+        win = self.makeColorPanel(wx.NamedColour('Indian Red'))
+        self.AddPage(win, "Indian Red")
 
         self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
         self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
