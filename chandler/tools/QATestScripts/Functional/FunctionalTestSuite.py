e4391a0aeaa6387b066daa93e84d2a91e3d66984
dan <dan@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-02-07 21:01:52 +0000

    added platform specific test exclusion
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9402 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/QATestScripts/Functional/FunctionalTestSuite.py
+++ chandler/tools/QATestScripts/Functional/FunctionalTestSuite.py
@@ -3,7 +3,7 @@
 ## Description: This test suite runs the 4 basic testcases of generating event, mail, task and note items in chandler
  
 import tools.QAUITestAppLib as QAUITestAppLib
-import os
+import os, sys
 
 functional_dir = os.path.join(os.getenv('CHANDLERHOME'),"tools/QATestScripts/Functional")
 
@@ -11,33 +11,60 @@ functional_dir = os.path.join(os.getenv('CHANDLERHOME'),"tools/QATestScripts/Fun
 fileName = "FunctionalTestSuite.log"
 logger = QAUITestAppLib.QALogger(fileName,"FunctionalTestSuite")
 
-def run_tests(*tests):
+def exclude(fullList, excludeList):
+    for item in excludeList:
+        if item in fullList:
+            fullList.remove(item)
+    return fullList
+
+def run_tests(tests):
     for filename in tests:
         execfile(os.path.join(functional_dir, filename))
+        
+allTests = ["TestCreateAccounts.py",
+                        "TestAllDayEvent.py", 
+                        "TestNewCollection.py",
+                        "TestDates.py", 
+                        "TestNewEvent.py",
+                        "TestNewMail.py",
+                        "TestNewTask.py",
+                        "TestNewNote.py",
+                        "TestStamping.py", 
+                        "TestMoveToTrash.py", 
+                        "TestDeleteCollection.py",
+                        "TestNewCollNoteStampMulti.py", 
+                        "TestCalView.py",
+                        "TestRecurrenceImporting.py", 
+                        "TestRecurringEvent.py",  
+                        "TestSwitchingViews.py",
+                        "TestExporting.py",
+                        "TestFlickr.py",
+                        "TestImporting.py",
+                        "TestImportOverwrite.py",
+                        "TestSharing.py"]
+
+exclude_on_linux = [                                          
+                                        ]
+exclude_on_mac = [ 
+                                        ]
+exclude_on_windows = [ 
+                                        ]
+exclude_on_all = [
+                                        "TestMoveToTrash.py", #bug # 5150  
+                                        "TestStamping.py", #Chandler bug#5097
+                                        "TestNewCollNoteStampMulti.py", #Chandler bug #5097
+                                        "TestAllDayEvent.py", #test not functioning bug#5110
+                                        "TestDates.py", #Chandler not handling daylightsavings bug#5038
+                                        "TestRecurrenceImporting.py", #Chandler bug #5116
+                                    ]
+
+tests_to_run = exclude(allTests, exclude_on_all)
+if sys.platform == "linux2" : tests_to_run = exclude(tests_to_run, exclude_on_linux)
+if sys.platform == "darwin" : tests_to_run = exclude(tests_to_run, exclude_on_mac)
+if sys.platform == "win32" : tests_to_run = exclude(tests_to_run, exclude_on_windows)
 
 try:
-    run_tests("TestCreateAccounts.py",
-              #"TestAllDayEvent.py", test not functioning bug#5110
-              "TestNewCollection.py",
-              #"TestDates.py", Chandler not handling daylightsavings bug#5038
-              "TestNewEvent.py",
-              "TestNewMail.py",
-              "TestNewTask.py",
-              "TestNewNote.py",
-              #"TestStamping.py", Chandler bug#5097
-              #"TestMoveToTrash.py", bug # 5150
-              "TestDeleteCollection.py",
-              #"TestNewCollNoteStampMulti.py", Chandler bug #5097
-              "TestCalView.py",
-              #"TestRecurrenceImporting.py", Chandler bug #5116
-              "TestRecurringEvent.py",  
-              "TestSwitchingViews.py",
-              "TestExporting.py",
-              "TestFlickr.py",
-              "TestImporting.py",
-              "TestImportOverwrite.py",
-              "TestSharing.py")
-    
+    run_tests(tests_to_run)
 finally:    
     #cleaning
     logger.Close()
