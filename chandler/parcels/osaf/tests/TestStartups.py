3d120a5a1a8d8e63a7e60d5b4e00db0fc42dd365
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-02-07 00:32:01 +0000

    Add a 90 second alarm timeout for Linux and OS X, in an attempt to find out more about the hang(s) occurring there.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9383 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/tests/TestStartups.py
+++ chandler/parcels/osaf/tests/TestStartups.py
@@ -14,6 +14,17 @@ def additional_tests():
 
 
 if __name__=='__main__':
+    try:
+        from signal import signal, alarm, SIGALRM
+    except ImportError:
+        pass    # no alarm on Windows  :(
+    else:
+        # Set up a 90 second maximum timeout
+        def timeout(*args):
+            raise KeyboardInterrupt("Timeout occurred")
+        signal(SIGALRM, timeout)
+        alarm(90)
+
     from run_tests import ScanningLoader
     unittest.main(testLoader = ScanningLoader())
 
