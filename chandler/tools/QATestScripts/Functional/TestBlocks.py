c690bac092cde2b7f5f4ba5b3ec1705d683fff6b
dan <dan@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-02-11 00:12:53 +0000

    added more logging
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9476 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/QATestScripts/Functional/TestBlocks.py
+++ chandler/tools/QATestScripts/Functional/TestBlocks.py
@@ -6,9 +6,24 @@ logger = QAUITestAppLib.QALogger("TestBlocks.log","TestBlocks")
 # that occurs when the user interface is exercised. If a problem occurs
 # you should see a traceback.
 try:
-    app_ns().root.ChooseCPIATestMainView()
-    app_ns().root.ChooseChandlerMainView()
-    app_ns().root.ReloadParcels()
-
+    logger.Start('TestBlocks')
+    logger.SetChecked(True)
+    try:
+        app_ns().root.ChooseCPIATestMainView()
+        logger.ReportPass('CPIATestMainView')
+    except:
+        logger.ReportFailure('CPIATestMainView')
+    try:
+        app_ns().root.ChooseChandlerMainView()
+        logger.ReportPass('ChandlerMainView')
+    except:
+        logger.ReportFailure('ChandlerMainView')
+    try:
+        app_ns().root.ReloadParcels()
+        logger.ReportPass('ReloadParcels')
+    except:
+        logger.ReportFailure('ReloadParcels')
+    logger.Stop()
 finally:
+    logger.Report('TestBlocks')
     logger.Close()
