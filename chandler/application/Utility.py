54614a06a27be9903c0821e4c583e95a907a3d54
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-03-20 22:18:33 +0000

    bump the schema to pick up the new test menu
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9870 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -22,7 +22,7 @@ from repository.persistence.RepositoryError import \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "157" # alecf: added per-kind columns in the table summary
+SCHEMA_VERSION = "158" # alecf: clean the test menu
 
 logger = None # initialized in initLogging()
 
