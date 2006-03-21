ee1b35c2e249f379dc9656278b19437f6d3f441c
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-03-21 23:03:06 +0000

    add more documentation for using preferences, including item watching
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@9896 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/framework/prefs.py
+++ chandler/parcels/osaf/framework/prefs.py
@@ -4,6 +4,9 @@ from application import schema
 """
 Here's how you should use the Preferences class:
 
+Declare a new pref class
+------------------------
+
 from osaf.app import Preferences
 class MyPrefs(Preferences):
     textPref = schema.One(schema.Text, defaultValue='somevalue')
@@ -13,7 +16,8 @@ then install it in your module:
 def installParcel(parcel, ...):
     MyPrefs.update(parcel, 'MyPrefs')
 
-now to access your preferences you'd say
+Accessing a preference
+----------------------
 
 myPrefs = schema.ns("my.module.namespace", view).MyPrefs
 prefValue = myPrefs.textPref
@@ -24,6 +28,20 @@ for parcelPref in Preferences.iterItems(view):
     print "In pref object %s:" % parcelPref.__class__.__name__
     for prefname, prefvalue, preftype in prefObj.iterAttributes():
         print "    %s=%s" % (prefname, prefvalue)
+
+Watching for changes
+--------------------
+
+class SomeObject(schema.Item):
+    def __init__(self, *args, **kwds):
+        # persistent prefs ok in __init__ because __init__ is only
+        # called the first time the object is created, not when it
+        # gets restored from the repository
+        self.watchItem(myPrefs, 'onMyPrefChanged')
+
+    def onMyPrefChanged(self, op, pref, names):
+        if 'textPref' in names:
+            print "textPref just changed to %s" % pref.textPref
 
 """
 
