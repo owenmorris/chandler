0ba0f1935bf3644e05ae2fe101b00cd937a5f7dd
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-03-30 20:00:14 +0000

    bump the schema version to account for SmartCollection rename
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10051 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -22,7 +22,7 @@ from repository.persistence.RepositoryError import \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "166" # alecf: get rid of InExColl.setup()
+SCHEMA_VERSION = "167" # alecf: get rid of InExColl.setup()
 
 logger = None # initialized in initLogging()
 
