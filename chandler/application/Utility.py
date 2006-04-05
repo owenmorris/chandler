1940070bbbfb4c2b70a1fd22ee07284d8764d90f
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-04-05 20:24:29 +0000

    bump the schema version to account for schema changes in calendar
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10159 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -22,7 +22,7 @@ from repository.persistence.RepositoryError import \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "171" # alecf: add new calendar events
+SCHEMA_VERSION = "172" # alecf: remove selectedDate from CalendarBlock
 
 logger = None # initialized in initLogging()
 
