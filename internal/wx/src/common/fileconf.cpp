5bdf767007dafc8ed190a5e80499c8c87f64f741
davids <davids@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-04-08 09:17:30 +0000

    wx v2.7.0 incremental update - fixed small memory leak in file config handling
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10204 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/wx/src/common/fileconf.cpp
+++ internal/wx/src/common/fileconf.cpp
@@ -4,7 +4,7 @@
 // Author:      Vadim Zeitlin
 // Modified by:
 // Created:     07.04.98 (adapted from appconf.cpp)
-// RCS-ID:      $Id: fileconf.cpp,v 1.147 2006/04/05 16:54:02 VZ Exp $
+// RCS-ID:      $Id: fileconf.cpp,v 1.148 2006/04/07 22:41:11 VZ Exp $
 // Copyright:   (c) 1997 Karsten Ballüder   &  Vadim Zeitlin
 //                       Ballueder@usa.net     <zeitlin@dptmaths.ens-cachan.fr>
 // Licence:     wxWindows licence
@@ -568,6 +568,8 @@ void wxFileConfig::CleanUp()
 
 wxFileConfig::~wxFileConfig()
 {
+    delete m_conv;
+
     Flush();
 
     CleanUp();
