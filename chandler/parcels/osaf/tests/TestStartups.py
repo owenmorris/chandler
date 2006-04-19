f57aa2b84bad8a382d19af12ddf904d4719b0ebd
pje <pje@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-04-19 17:04:02 +0000

    Add test wrapper to TestStartups to stop it as soon as the even one example fails, since some things can lock up if invariants are broken.  This should hopefully stop the hangs and show what the real problem(s) are -- or else prove that they're not being caused by broken invariants.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10346 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/tests/TestStartups.py
+++ chandler/parcels/osaf/tests/TestStartups.py
@@ -1,10 +1,52 @@
-import unittest
+import unittest, doctest, sys
+from StringIO import StringIO
+
+class StopTests(Exception):
+    """Stop doctests in progress"""
+    
+class StoppingRunner(doctest.DocTestRunner):
+    """Stop running test cases as soon as a failure or error occurs"""
+
+    def report_failure(*args, **kw):
+        doctest.DocTestRunner.report_failure(*args, **kw)
+        raise StopTests
+
+    def report_unexpected_exception(*args, **kw):
+        doctest.DocTestRunner.report_unexpected_exception(*args, **kw)
+        raise StopTests
 
 def test_startups():
-    import doctest
-    return doctest.DocFileSuite(
+
+    class StoppingCase(doctest.DocFileCase):
+    
+        def runTest(self):
+            test = self._dt_test
+            old = sys.stdout
+            new = StringIO()
+            optionflags = self._dt_optionflags
+            if not (optionflags & doctest.REPORTING_FLAGS):
+                optionflags |= doctest._unittest_reportflags    
+            runner = StoppingRunner(optionflags=optionflags,
+                                   checker=self._dt_checker, verbose=False)
+            try:
+                runner.DIVIDER = "-"*70
+                try:
+                    failures, tries = runner.run(
+                        test, out=new.write, clear_globs=False)
+                except StopTests:
+                    failures = 1
+            finally:
+                sys.stdout = old
+    
+            if failures:
+                raise self.failureException(self.format_failure(new.getvalue()))
+
+    test = doctest.DocFileTest(
         'startups.txt', optionflags=doctest.ELLIPSIS, package='osaf',
     )
+
+    test.__class__ = StoppingCase   # ugh
+    return test
 
 
 def additional_tests():
