76a1c1fc28f439c7749dc24b0cbf5aa3205cacda
twl <twl@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-04-20 05:19:38 +0000

    Bump schema version for bug 3531
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10361 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -22,7 +22,7 @@ from repository.persistence.RepositoryError import \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "183" # john: update Table and Column schema
+SCHEMA_VERSION = "184" # twl: change body type to schema.Text, bug 3531
 
 logger = None # initialized in initLogging()
 
