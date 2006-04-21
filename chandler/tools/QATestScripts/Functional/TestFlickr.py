03dadbed5f63cbcb38707064afc84f2cb120d9c5
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-04-21 19:29:47 +0000

    Catch socket errors, and don't consider them to be test failures
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10384 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/QATestScripts/Functional/TestFlickr.py
+++ chandler/tools/QATestScripts/Functional/TestFlickr.py
@@ -1,6 +1,7 @@
 import tools.QAUITestAppLib as QAUITestAppLib
 import flickr
 import application.Globals as Globals
+import socket
 
 # initialization
 fileName = "TestFlickr.log"
@@ -27,32 +28,37 @@ try:
     fc = flickr.PhotoCollection(itsView = repView)
     fc.tag = flickr.Tag.getTag(repView, "oscon2005")
     fc.displayName = "oscon2005"
-    fc.fillCollectionFromFlickr(repView)
 
-    # Add the channel to the sidebar
-    app_ns().sidebarCollection.add(fc)
+    try:
+        fc.fillCollectionFromFlickr(repView)
+    except socket.timeout:
+        logger.ReportPass("Flickr timed out; skipping test")
+    else:
 
-    def sidebarCollectionNamed(name):
-        """
-        Look for a sidebar collection with name, otherwise return False
-        """
-        sidebarWidget = app_ns().sidebar.widget
-        for i in range(sidebarWidget.GetNumberRows()):
-            collection = sidebarWidget.GetTable().GetValue(i,0)[0]
-            if collection.displayName == name:
-                return collection
-        return False
+        # Add the channel to the sidebar
+        app_ns().sidebarCollection.add(fc)
 
-    # force sidebar to update
-    User.idle()
+        def sidebarCollectionNamed(name):
+            """
+            Look for a sidebar collection with name, otherwise return False
+            """
+            sidebarWidget = app_ns().sidebar.widget
+            for i in range(sidebarWidget.GetNumberRows()):
+                collection = sidebarWidget.GetTable().GetValue(i,0)[0]
+                if collection.displayName == name:
+                    return collection
+            return False
 
-    # check results
-    col = sidebarCollectionNamed("oscon2005")
-    if not col:
-        logger.ReportFailure("Flickr Collection wasn't created")
-    if col and len(col) != 10:
-        logger.ReportFailure("Flickr Collection had the wrong number of elements: %d, expected %d" % (len(col.item), 10))
-    logger.ReportPass("Flickr collection of correct size created")
+        # force sidebar to update
+        User.idle()
+
+        # check results
+        col = sidebarCollectionNamed("oscon2005")
+        if not col:
+            logger.ReportFailure("Flickr Collection wasn't created")
+        if col and len(col) != 10:
+            logger.ReportFailure("Flickr Collection had the wrong number of elements: %d, expected %d" % (len(col.item), 10))
+        logger.ReportPass("Flickr collection of correct size created")
 
 finally:
     logger.Close()
