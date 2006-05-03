7ba49b391c45f5486127091909d45642565ac2e5
alecf <alecf@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-05-03 16:55:26 +0000

    bump schema version to account for in/out/mine change
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10563 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -22,7 +22,7 @@ from repository.persistence.RepositoryError import \
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "190" # John: Update CPIA viewer
+SCHEMA_VERSION = "191" # alecf: make In and Out be 'mine'
 
 logger = None # initialized in initLogging()
 
