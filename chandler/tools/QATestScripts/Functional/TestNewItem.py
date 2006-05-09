3e4703220a3231d05793f45e5f58f23d0e7eed23
grant <grant@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-05-09 00:35:50 +0000

    Add tests that "New Item" respects the selection in the app bar.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10623 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

---
+++ chandler/tools/QATestScripts/Functional/TestNewItem.py
@@ -0,0 +1,61 @@
+import tools.QAUITestAppLib as QAUITestAppLib
+    
+# initialization
+fileName = "TestNewItem.log"
+logger = QAUITestAppLib.QALogger(fileName, "TestNewItem")
+
+START_ITEM = QAUITestAppLib.App_ns.DetailRoot.contents
+    
+def switchAndCheck(logger, buttonName, expectedClass):
+
+    name = "TestNewItem(%s)" % (buttonName,)
+    logger.Start(name)
+
+    # Switch to the requested view...
+    QAUITestAppLib.App_ns.appbar.press(name=buttonName)
+    
+    # ... idle() so the app can handle changes
+    QAUITestAppLib.scripting.User.idle()
+
+    # ... Create a new item (For some reason, calling
+    # scripting.User.emulate_typing() didn't correctly
+    # simulate a control/cmd-n here)
+    QAUITestAppLib.App_ns.MainView.onNewItemEvent(
+       QAUITestAppLib.App_ns.NewItemItem.event)
+    
+    # ... wait again so the app can refresh
+    QAUITestAppLib.scripting.User.idle()
+
+    # See what's in the detail view
+    newItem = QAUITestAppLib.App_ns.DetailRoot.contents
+
+    # Verify we got what we expected
+    if newItem is START_ITEM:
+        logger.ReportFailure("Selection in detail view didn't change!")
+    elif newItem.__class__ != expectedClass:
+        logger.ReportFailure("Expected a %s, got %s" % (expectedClass, newItem))
+    else:
+        logger.ReportPass("Created a %s" % (expectedClass))
+
+    # Random QAUITestAppLib nonsense
+    logger.SetChecked(True)
+    logger.Report(name)
+    logger.Stop()
+
+try:
+
+    switchAndCheck(logger, "ApplicationBarAllButton",
+                   QAUITestAppLib.pim.notes.Note)
+    
+    switchAndCheck(logger, "ApplicationBarEventButton",
+                   QAUITestAppLib.pim.calendar.CalendarEvent)
+    
+    switchAndCheck(logger, "ApplicationBarMailButton",
+                   QAUITestAppLib.Mail.MailMessage)
+    
+    switchAndCheck(logger, "ApplicationBarTaskButton",
+                   QAUITestAppLib.pim.Task)
+
+finally:
+    # cleanup
+    logger.Close()
