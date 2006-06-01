87ae7faacc68973fe326532067385659b9183ea2
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-06-01 22:25:27 +0000

    Add a little explanation to the assert failures. Also add a comment explaining the most likely cause of failure.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10816 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/tests/TestCrypto.py
+++ chandler/application/tests/TestCrypto.py
@@ -33,9 +33,9 @@ class CryptoTestCase(unittest.TestCase):
     def testCrypto(self):
         # First time there should not be anything, so load and save 0
         r = Utility.initCrypto(self.path)
-        self.assert_(r == 0, r)
+        self.assert_(r == 0, 'First time crypto init should return 0, got %d' % r)
         r = Utility.stopCrypto(self.path)
-        self.assert_(r == 0, r)
+        self.assert_(r == 0, 'First time crypto stop should return 0, got %d' % r)
 
         # Trick us into thinking we have initialized entropy
         from osaf.framework.certstore import utils
@@ -43,13 +43,17 @@ class CryptoTestCase(unittest.TestCase):
 
         # Now we should save 1k on stop, after that loading and saving 1k
         r = Utility.initCrypto(self.path)
-        self.assert_(r == 0, r)
+        self.assert_(r == 0, 'Entropy initialized, crypto init should return 0 first time, got %d' % r)
+        
+        # Note: the most likely reason why these would fail is if the entropy
+        # file was not created, for example if we failed to create the
+        # directory in setUp()
         r = Utility.stopCrypto(self.path)
-        self.assert_(r == 1024, r)
+        self.assert_(r == 1024, 'Entropy initialized, should save 1024 entropy first time, got %d' % r)
         r = Utility.initCrypto(self.path)
-        self.assert_(r == 1024, r)
+        self.assert_(r == 1024, 'Entropy initialized, should load 1024 entropy, got %d' % r)
         r = Utility.stopCrypto(self.path)
-        self.assert_(r == 1024, r)
+        self.assert_(r == 1024, 'Entropy initialized, should save 1024 entropy, got %d' % r)
 
 
 if __name__=='__main__':
