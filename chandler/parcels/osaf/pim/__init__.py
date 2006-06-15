owenmorris/chandler

commit 6628e246000a20fc96c5bc832d76daf1eaecc06f

- This should take a significant bite out of bug 4637, switching   weeks with a 3000 event calendar is slow.  The trick is to not   look at all events, but just assume there are no "really long"   events (longer than a week).
  This is obviously not a reasonable assumption, this checkin is
  introducing a bug that events longer than a week may not be
  displayed in the calendar.  Tomorrow I'll commit code to create
  a filter collection containing all really long events, which
  we'll special case when determining what events to draw, but
  that depends on some work Andi's doing.

  This change improved rendering of a 500 event calendar by about 10%
  on my Windows machine.  Once the special casing is in place I
  expect this will still be a big win for people who don't schedule
  many really long events.


git-svn-id: http://svn.osafoundation.org/chandler/trunk@10946 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

chandler/parcels/osaf/pim/__init__.py
@@ -133,6 +133,12 @@ def installParcel(parcel, oldVersion=None):
         trash=None,
     )
 
+    #longFilter = "view.findValue(uuid, 'duration', timedelta(0) > seven_days"
+    #reallyLongEvents = FilteredCollection.update(parcel, 'reallyLongEvents',
+        #source = events,
+        #filterExpression = longFilter,
+        #filterAttributes = ['duration'])
+
     # the monitor list assumes all reminders will be relativeTo
     # effectiveStartTime, which is true in 0.6, but may not be in the future
     eventsWithReminders.addIndex('reminderTime', 'compare',

chandler/parcels/osaf/pim/calendar/Calendar.py
@@ -76,9 +76,13 @@ def ensureIndexed(coll):
     if not coll.hasIndex('__adhoc__'):
         coll.addIndex('__adhoc__', 'numeric')
 
+zero_delta = timedelta(0)
+REALLY_LONG = timedelta(7)
+
 def getKeysInRange(view, startVal, startAttrName, startIndex, startColl,
                          endVal,   endAttrName,   endIndex,   endColl,
-                         filterColl = None, filterIndex = None, useTZ=True):
+                         filterColl = None, filterIndex = None, useTZ=True,
+                         longDelta=None):
     """
     Yield keys for events occurring between startVal and endVal.  Don't load
     items, just yield relevant keys, sorted according to startIndex.
@@ -91,48 +95,67 @@ def getKeysInRange(view, startVal, startAttrName, startIndex, startColl,
     """
     
     # callbacks to use for searching the indexes
-    def mStart(key):
-        # gets the last item starting before endVal
+    def mStart(key, delta=None):
+        # gets the last item starting before endVal, or before startVal - delta
+        if delta is None:
+            delta = zero_delta
+        else:
+            delta = delta + endVal - startVal
         testVal = getattr(view[key], startAttrName)
         if testVal is None:
             return -1 # interpret None as negative infinity
         # note that we're NOT using >=, if we did, we'd include all day
         # events starting at the beginning of the next week
         if useTZ:
-            if endVal > testVal:
+            if endVal - delta > testVal:
                 return 0
         else:
-            if endVal.replace(tzinfo=None) > testVal.replace(tzinfo=None):
+            if endVal.replace(tzinfo=None) - delta > testVal.replace(tzinfo=None):
                 return 0
         return -1
 
-    def mEnd(key):
-        # gets the first item starting after startVal
+    def mEnd(key, delta=None):
+        # gets the first item ending after startVal, or after endVal + delta
+        if delta is None:
+            delta = zero_delta
+        else:
+            delta = delta + endVal - startVal
         testVal = getattr(view[key], endAttrName)
         if testVal is None:
             return 0 # interpret None as positive infinity, thus, a match
         if useTZ:
-            if startVal < testVal:
+            if startVal + delta < testVal:
                 return 0
         else:
-            if startVal.replace(tzinfo=None) < testVal.replace(tzinfo=None):
+            if startVal.replace(tzinfo=None) + delta < testVal.replace(tzinfo=None):
                 return 0
         return 1
     
     lastStartKey = startColl.findInIndex(startIndex, 'last', mStart)
     if lastStartKey is None:
-        return #there were no keys ending after start
+        return #there were no keys starting after end
+    if longDelta is not None:
+        firstStartKey = startColl.findInIndex(startIndex, 'last',
+                                    lambda key: mStart(key, longDelta))
+    else:
+        firstStartKey = None
+    
     firstEndKey = endColl.findInIndex(endIndex, 'first', mEnd)
     if firstEndKey is None:
-        return #there were no keys ending before end
-
+        return #there were no keys ending before start
+    if longDelta is not None:
+        lastEndKey = endColl.findInIndex(endIndex, 'first',
+                                         lambda key: mEnd(key, longDelta))
+    else:
+        lastEndKey = None
+        
     if filterColl is not None:
         _filterIndex = filterColl.getIndex(filterIndex)
 
-    keys = set(endColl.iterindexkeys(endIndex, firstEndKey, None))
+    keys = set(endColl.iterindexkeys(endIndex, firstEndKey, lastEndKey))
 
     # generate keys, starting from the earliest according to startIndex
-    for key in startColl.iterindexkeys(startIndex, None, lastStartKey):
+    for key in startColl.iterindexkeys(startIndex, firstStartKey, lastStartKey):
         if key in keys and (filterColl is None or key in _filterIndex):
             yield key
 
@@ -183,7 +206,8 @@ def eventsInRange(view, start, end, filterColl = None, dayItems=True,
     allEvents = schema.ns("osaf.pim", view).events
     keys = getKeysInRange(view, start, 'effectiveStartTime', startIndex,
                           allEvents, end,'effectiveEndTime', endIndex,
-                          allEvents, filterColl, '__adhoc__', tzprefs.showUI)
+                          allEvents, filterColl, '__adhoc__', tzprefs.showUI,
+                          longDelta = REALLY_LONG)
     for key in keys:
         if (view[key].rruleset is None and 
             ((dayItems and timedItems) or isDayItem(view[key]) == dayItems)):

commit 8c488431ed5152885c17de1b995f174b4c773fef

- Add support for displaying week long events, which I deliberately   broke in r10946
git-svn-id: http://svn.osafoundation.org/chandler/trunk@10965 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

chandler/parcels/osaf/pim/__init__.py
@@ -7,7 +7,8 @@ from items import (
 )
 from notes import Note
 from contacts import Contact, ContactName
-from calendar.Calendar import CalendarEvent, CalendarEventMixin
+from calendar.Calendar import CalendarEvent, CalendarEventMixin, LONG_TIME, \
+                              zero_delta
 from calendar.Calendar import Calendar, Location, RecurrencePattern
 from calendar.TimeZone import installParcel as tzInstallParcel
 from calendar.DateTimeUtil import (ampmNames, durationFormat, mediumDateFormat, 
@@ -73,6 +74,10 @@ class NonRecurringFilter(Item):
 
         return not (isGenerated or modificationsFor)
 
+class LongEventFilter(Item):
+
+    def isLongEvent(self, view, uuid):
+        return view.findValue(uuid, 'duration', zero_delta) > LONG_TIME
 
 def installParcel(parcel, oldVersion=None):
     view = parcel.itsView
@@ -142,12 +147,23 @@ def installParcel(parcel, oldVersion=None):
         trash=None,
     )
 
-    #longFilter = "view.findValue(uuid, 'duration', timedelta(0) > seven_days"
-    #reallyLongEvents = FilteredCollection.update(parcel, 'reallyLongEvents',
-        #source = events,
-        #filterExpression = longFilter,
-        #filterAttributes = ['duration'])
-
+    longEvents = FilteredCollection.update(parcel, 'longEvents',
+        source = events,
+        filterMethod= (LongEventFilter(None, parcel), 'isLongEvent'),
+        filterAttributes = ['duration'])
+    longEvents.addIndex('effectiveStart', 'subindex',
+                        superindex=(events, events.__collection__,
+                                    'effectiveStart'))
+    longEvents.addIndex('effectiveStartNoTZ', 'subindex',
+                        superindex=(events, events.__collection__,
+                                    'effectiveStartNoTZ'))
+    longEvents.addIndex('effectiveEnd', 'subindex',
+                        superindex=(events, events.__collection__,
+                                    'effectiveEnd'))
+    longEvents.addIndex('effectiveEndNoTZ', 'subindex',
+                        superindex=(events, events.__collection__,
+                                    'effectiveEndNoTZ'))
+    
     # the monitor list assumes all reminders will be relativeTo
     # effectiveStartTime, which is true in 0.6, but may not be in the future
     eventsWithReminders.addIndex('reminderTime', 'compare',

chandler/parcels/osaf/pim/calendar/Calendar.py
@@ -16,7 +16,6 @@ from osaf.pim.items import Calculated, ContentItem
 from osaf.pim.notes import Note
 from osaf.pim.calendar import Recurrence
 from application.dialogs import RecurrenceDialog
-
 import wx
 
 from TimeZone import formatTime
@@ -77,12 +76,12 @@ def ensureIndexed(coll):
         coll.addIndex('__adhoc__', 'numeric')
 
 zero_delta = timedelta(0)
-REALLY_LONG = timedelta(7)
+LONG_TIME  = timedelta(7)
 
 def getKeysInRange(view, startVal, startAttrName, startIndex, startColl,
                          endVal,   endAttrName,   endIndex,   endColl,
                          filterColl = None, filterIndex = None, useTZ=True,
-                         longDelta=None):
+                         longDelta=None, longCollection=None):
     """
     Yield keys for events occurring between startVal and endVal.  Don't load
     items, just yield relevant keys, sorted according to startIndex.
@@ -90,10 +89,15 @@ def getKeysInRange(view, startVal, startAttrName, startIndex, startColl,
     endIndex is needed to determine whether or not events starting before
     startVal also end before startVal.
     
-    startIndex, endIndex, and filterIndex are the names (strings) of indexes
-    on the relevant collections.
-    """
+    startIndex, endIndex, and filterIndex are the names (strings) of
+    indexes on the relevant collections.
     
+    If longDelta is present, the range checked will be constrained to look for
+    events within longDelta of (startVal, endVal), supplemented as appropriate
+    with any events in longCollection.  startIndex and endIndex must exist
+    on longCollection.
+    
+    """
     # callbacks to use for searching the indexes
     def mStart(key, delta=None):
         # gets the last item starting before endVal, or before startVal - delta
@@ -153,10 +157,19 @@ def getKeysInRange(view, startVal, startAttrName, startIndex, startColl,
         _filterIndex = filterColl.getIndex(filterIndex)
 
     keys = set(endColl.iterindexkeys(endIndex, firstEndKey, lastEndKey))
+    ignores = []
+    # first, yield long events, calculated by recursing, look at longCollection
+    if longCollection is not None:
+        for key in getKeysInRange(view, startVal, startAttrName, startIndex,
+                         longCollection, endVal, endAttrName, endIndex,
+                         longCollection, filterColl, filterIndex, useTZ):
+            ignores.append(key)
+            yield key
 
-    # generate keys, starting from the earliest according to startIndex
+    # next, generate normal keys
     for key in startColl.iterindexkeys(startIndex, firstStartKey, lastStartKey):
-        if key in keys and (filterColl is None or key in _filterIndex):
+        if key in keys and (filterColl is None or key in _filterIndex) \
+           and key not in ignores:
             yield key
 
 def isDayItem(item):
@@ -203,11 +216,12 @@ def eventsInRange(view, start, end, filterColl = None, dayItems=True,
         startIndex = 'effectiveStartNoTZ'
         endIndex   = 'effectiveEndNoTZ'
     
-    allEvents = schema.ns("osaf.pim", view).events
+    allEvents  = schema.ns("osaf.pim", view).events
+    longEvents = schema.ns("osaf.pim", view).longEvents
     keys = getKeysInRange(view, start, 'effectiveStartTime', startIndex,
                           allEvents, end,'effectiveEndTime', endIndex,
                           allEvents, filterColl, '__adhoc__', tzprefs.showUI,
-                          longDelta = REALLY_LONG)
+                          longDelta = LONG_TIME, longCollection=longEvents)
     for key in keys:
         if (view[key].rruleset is None and 
             ((dayItems and timedItems) or isDayItem(view[key]) == dayItems)):