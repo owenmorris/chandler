77523975cd29404a27e1af2fbd83144c31fcb511
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-06-20 18:10:24 +0000

    Inserting the new Apache 2.0 license
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10998 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/Chandler.py
+++ chandler/Chandler.py
@@ -1,11 +1,22 @@
+#   Copyright (c) 2003-2006 Open Source Applications Foundation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
 """
 Chandler startup
-
-@version:   $Revision$
-@date:      $Date$
-@copyright: Copyright (c) 2003-2005 Open Source Applications Foundation
-@license:   http://osafoundation.org/Chandler_0.1_license_terms.htm
 """
+
 
 import os, PyLucene
 import application.Globals as Globals
