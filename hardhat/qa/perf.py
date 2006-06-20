d80bb6dcd75ba8065e2e2b5c45079cf8a4a36a78
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-06-20 18:36:25 +0000

    Inserting Apache v2.0 license information This is a relicensing of hardhat from GPL to Apache v2.0
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@10999 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- hardhat/qa/perf.py
+++ hardhat/qa/perf.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 # vi:ts=2 sw=2 nofen
 
-__author__       = "Mike Taylor <bear@code-bear.com>"
-__contributors__ = []
-__copyright__    = "Copyright (c) 2004,2005 Mike Taylor"
-__license__      = "BSD"
-__version__      = "1.0"
-__svn__          = "$Id$"
+
+#
+#   Copyright (c) 2006 Open Source Applications Foundation
+#   Copyright (c) 2004-2005 Mike Taylor
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#
+# Author:           Mike Taylor (bear@code-bear.com)
+# Contributors:     Heikki
+#
 
 
 import sys, os, string, datetime, time, math
--- hardhat/tbox_update.py
+++ hardhat/tbox_update.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 
-# Helper program to send tinderbox2 status emails
+#   Copyright (c) 2003-2006 Open Source Applications Foundation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
-__author__    = "Mike Taylor <bear@osafoundation.org>"
-__copyright__ = "Copyright (c) 2005 Open Source Applications Foundation"
-__version__   = "1.0"
-__svn__       = "$Id: $"
+#
+# Helper program to send tinderbox2 status emails
+#
+# Author:   Mike Taylor (bear@osafoundation.org)
+# Version:  1.0
+#
 
 import os, time, smtplib
 import ConfigParser, optparse
