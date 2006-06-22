d3576f9757c8d87d09d9b8cb019f51558fc3e8fe
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-06-22 23:15:16 +0000

    Removing wxPython from internal/ as it now lives in external/
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@11037 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/wx/wxPython/wx/py/version.py
+++
@@ -1,9 +0,0 @@
-"""Provides an object representing the current 'version' or 'release'
-of Py as a whole.  Individual classes, such as the shell, filling and
-interpreter, each have a revision property based on the CVS Revision."""
-
-__author__ = "Patrick K. O'Brien <pobrien@orbtech.com>"
-__cvsid__ = "$Id: version.py,v 1.6 2005/12/30 23:00:53 RD Exp $"
-__revision__ = "$Revision: 1.6 $"[11:-2]
-
-VERSION = '0.9.5'
