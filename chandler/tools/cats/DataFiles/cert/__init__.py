d06b93e8d6a45ab71aa2c73fb348bc9f351039da
dan <dan@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-07-05 17:11:06 +0000

    Remove cert directory from new test framework. cert directory has already been removed from old test framework (r11002).
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@11081 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/cats/DataFiles/cert/__init__.py
+++
@@ -1,7 +0,0 @@
-import application.schema as schema
-
-def installParcel(parcel, oldVersion=None):
-    app = schema.ns('osaf.app', parcel)
-    
-    from osaf.framework.certstore.data import loadCerts
-    loadCerts(parcel,__name__, "certs.pem") 
