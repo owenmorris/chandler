4cbc4ab4689b1be6f06a65891280038e3b9443f0
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-08-10 19:40:41 +0000

    Udating EggTranslations setup.py and adding svn:ignore property
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@11380 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/projects/EggTranslations-Plugin/setup.py
+++ chandler/projects/EggTranslations-Plugin/setup.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-#   Copyright (c) 2003-2006 Open Source Applications Foundation
+#   Copyright (c) 2006 Open Source Applications Foundation
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -14,27 +14,18 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-"""
-@author:    Brian Kirsch - bkirsch@osafoundation.org
-@copyright: Copyright (c) 2003-2006 Open Source Applications Foundation
-@license:   Apache License, Version 2.0
-"""
 
 from setuptools import setup
 
-# the setup block
 setup(
-    # package description
-    name = "EggTranslations-Plugin",
-    version = "0.1",
-    author = "Brian Kirsch",
+    name         = "EggTranslations-Plugin",
+    version      = "0.1",
+    author       = "Brian Kirsch",
     author_email = "bkirsch@osafoundation.org",
-    description = "Provides an API for accessing localizations and resources packaged in eggs",
-    license = "Apache License, Version 2.0",
-    test_suite = "tests",
+    description  = "Provides an API for accessing localizations and resources packaged in eggs",
+    license      = "Apache License, Version 2.0",
+    test_suite   = "tests",
+    py_modules   = ["egg_translations"],
     include_package_data = True,
-    zip_safe = True,
-
-    # package contents
-    py_modules = ["egg_translations"],
+    zip_safe             = True,
 )
