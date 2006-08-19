5f500109d58e1d0ba758b60c9905781f1580654d
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-08-19 23:39:42 +0000

    back out revision #11460 until I can understand why it's failing tinderbox
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@11462 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/projects/Chandler-EventLoggerPlugin/eventLogger/__init__.py
+++ chandler/projects/Chandler-EventLoggerPlugin/eventLogger/__init__.py
@@ -16,45 +16,15 @@
 __parcel__ = "eventLogger"
 
 from application import schema
-from application import Globals
 from osaf.framework.blocks import DispatchHook, BlockEvent
 from osaf.framework.blocks.MenusAndToolbars import MenuItem
-from osaf.startup import PeriodicTask
-from osaf.sharing.WebDAV import ChandlerServerHandle
-from zanshin.http import Request
-from string import joinfields
-from datetime import timedelta
-from datetime import datetime
-import socket
-import re
-import logging
-import logging.handlers
-import os
-
-
-logger = logging.getLogger(__name__)
-
-logDir = os.path.join(Globals.options.profileDir, 'eventLogger')
-logFile = os.path.join(logDir, 'event.log')
-logFileMaxSize = 250000 #Bytes
-logFileMaxCount = 10    #Max logfiles to keep
+from i18n import ChandlerMessageFactory as _
 
 class EventLoggingDispatchHook (DispatchHook):
-    """
-    Class to handle event dispatches
-    """
     logging = schema.One(schema.Boolean, initialValue=False)
 
     def dispatchEvent (self, event, depth):
-        """
-        Actual handler which instantiates the logger and 'logs' the event
-        """
-        if not event.arguments.has_key ('UpdateUI') and depth == 1:
-            logger = getattr (self, "logger", None)
-            if logger is None:
-                ## cache the logger for future use
-                self.logger = self.createLogger()
-            self.logEvent(event)
+        print event, depth
 
     def onToggleLoggingEvent (self, event):
         self.logging = not self.logging
@@ -66,103 +36,10 @@ class EventLoggingDispatchHook (DispatchHook):
         else:
             hooksListItem.hooks.remove (dispatchHook)
 
+
     def onToggleLoggingEventUpdateUI (self, event):
         event.arguments['Check'] = self.logging
 
-    def createLogger(self):   
-        if not os.path.isdir(logDir):
-            os.mkdir(logDir)
-
-        handler = logging.handlers.RotatingFileHandler(logFile,'a', \
-                                                       logFileMaxSize, \
-                                                       logFileMaxCount)
-        handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
-        logger = logging.getLogger('eventLogger')
-        logger.addHandler(handler)
-        logger.setLevel(logging.DEBUG)
-        return logger
-
-
-    def logEvent(self, event):
-        items = event.arguments.get('items', None)
-        if items is not None and len(items) > 0:
-            item_name = items[0].getItemDisplayName()
-        else:
-            item_name = ""
-
-        collection = event.arguments.get('collection', None)
-        if collection is not None and len(collection) > 0:
-            collection_name = collection.getItemDisplayName()
-        else:
-            collection_name = ""
-
-        self.logger.debug("%s - %s - %s - %s" % (event.blockName, event.arguments, item_name, collection_name))
-
-
-class UploadTask(object):
-    """
-    PeriodicTasks require that we use a non-persistent Python object
-    """
-    def __init__(self, item):
-        self.rv = item.itsView
-        logger.info("eventLogger - Installing Parcel")
-
-    def run(self):
-        logger.info("eventLogger - receiveWakeupCall()")
-
-        dirList = os.listdir(logDir)
-        for file in dirList:
-            if re.search("event\.log\.\d+", file):
-
-                fname = os.path.join(logDir, file)
-
-                logger.info("eventLogger.Uploader: Trying to upload %s", fname)
-                self.uploadFile(fname)
-        return True
-
-
-    def uploadFile(self, filename):
-        """
-        You can view via https://feedback.osafoundation.org/manage/usage/
-        """
-        
-        path = '/desktop/usage/post/submit'
-        host = 'feedback.osafoundation.org'
-        port = 443
-        useSSL = True
-
-        timestamp = datetime.now()
-
-        f = open(filename, 'rb')
-        fdata = f.read()
-        f.close()
-
-        data = []
-        data.append("username=%s" % os.getlogin())
-        data.append("hostname=%s" % socket.gethostname())
-        data.append("uploaded=%s" % timestamp)
-        data.append("\n%s" % fdata)
-
-        datastring = joinfields(data,"\n")
-
-        request = Request('POST', path, None, datastring)
-        handle = ChandlerServerHandle(host=host, 
-                                      port=port,
-                                      useSSL=useSSL, 
-                                      repositoryView=self.rv)
-        defferedObject = handle.addRequest(request)
-        defferedObject.addCallback(self.uploadSuccess, filename).addErrback(self.uploadFailure, filename) 
-       
-
-    def uploadSuccess(self, response, fname):
-        if response.status == 200:
-            os.remove(fname)          
-            logger.info("Successfully uploaded %s", fname)
-        else:
-            logger.info("Failed up upload %s - %s:%s", fname, response.status, response.message)
-
-    def uploadFailure(self, fobject, fname):
-        logger.info("Failed to upload %s\n%s", fname, fobject)
 
 def installParcel(parcel, old_version=None):
     mainView = schema.ns('osaf.views.main', parcel.itsView)
@@ -183,20 +60,13 @@ def installParcel(parcel, old_version=None):
         parcel, 'StartLogging',
         blockName = 'StartLoggingMenuItem',
         menuItemKind = 'Check',
-        title = u'Start Logging',
-        helpString = u'Turn on logging and send result to OSAF',
+        title = _(u'Start Logging'),
+        helpString = _(u'Turn on logging and send result to OSAF'),
         event = ToggleLogging,
         eventsForNamedLookup = [ToggleLogging],
         location = "TestMenu",
         operation = 'InsertBefore',
         itemLocation = "SearchWindowItem",
         parentBlock = mainView.MainView)
-
-    # The periodic task that uploads logfiles in the background  
-    PeriodicTask.update(
-        parcel, 'eventLoggerUploadTask',
-        invoke = 'eventLogger.UploadTask',
-        run_at_startup = True,
-        active = True,
-        interval = timedelta(minutes=1))
+ 
 
