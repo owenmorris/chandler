968a0f290140c7d9e159c3dcabb968296d42f6b1
vajda <vajda@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-08-28 22:07:37 +0000

    - made Nil an empty iterable    - version 0.6-38
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@11545 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/chandlerdb/chandlerdb/item/item.c
+++ internal/chandlerdb/chandlerdb/item/item.c
@@ -1394,10 +1394,21 @@ typedef struct {
     PyObject_HEAD
 } t_nil;
 
-static int t_nil_length(t_nil *self)
-{
-    return 0;
-}
+
+static PyObject *t_nil_get(t_nil *self, PyObject *args);
+static PyObject *t_nil_iternext(t_nil *self);
+static int t_nil_length(t_nil *self);
+static PyObject *t_nil_dict_get(t_nil *self, PyObject *key);
+
+
+static PyMethodDef t_nil_methods[] = {
+    { "get", (PyCFunction) t_nil_get, METH_VARARGS, NULL },
+    { "iteritems", (PyCFunction) PyObject_SelfIter, METH_NOARGS, NULL },
+    { "iterkeys", (PyCFunction) PyObject_SelfIter, METH_NOARGS, NULL },
+    { "itervalues", (PyCFunction) PyObject_SelfIter, METH_NOARGS, NULL },
+    { NULL, NULL, 0, NULL }
+};
+
 
 static PySequenceMethods nil_as_sequence = {
     (inquiry) t_nil_length,             /* sq_length */
@@ -1409,6 +1420,13 @@ static PySequenceMethods nil_as_sequence = {
     0,                                  /* sq_ass_slice */
     0,                                  /* sq_contains */
 };
+
+static PyMappingMethods nil_as_mapping = {
+    (inquiry) t_nil_length,             /* mp_length */
+    (binaryfunc) t_nil_dict_get,        /* mp_subscript */
+    (objobjargproc) 0,                  /* mp_ass_subscript */
+};
+
 
 static PyTypeObject NilType = {
     PyObject_HEAD_INIT(NULL)
@@ -1424,7 +1442,7 @@ static PyTypeObject NilType = {
     0,                                         /* tp_repr */
     0,                                         /* tp_as_number */
     &nil_as_sequence,                          /* tp_as_sequence */
-    0,                                         /* tp_as_mapping */
+    &nil_as_mapping,                           /* tp_as_mapping */
     0,                                         /* tp_hash  */
     0,                                         /* tp_call */
     0,                                         /* tp_str */
@@ -1437,9 +1455,9 @@ static PyTypeObject NilType = {
     0,                                         /* tp_clear */
     0,                                         /* tp_richcompare */
     0,                                         /* tp_weaklistoffset */
-    0,                                         /* tp_iter */
-    0,                                         /* tp_iternext */
-    0,                                         /* tp_methods */
+    PyObject_SelfIter,                         /* tp_iter */
+    (iternextfunc)t_nil_iternext,              /* tp_iternext */
+    t_nil_methods,                             /* tp_methods */
     0,                                         /* tp_members */
     0,                                         /* tp_getset */
     0,                                         /* tp_base */
@@ -1451,6 +1469,35 @@ static PyTypeObject NilType = {
     0,                                         /* tp_alloc */
     0,                                         /* tp_new */
 };
+
+
+static int t_nil_length(t_nil *self)
+{
+    return 0;
+}
+
+static PyObject *t_nil_dict_get(t_nil *self, PyObject *key)
+{
+    PyErr_SetObject(PyExc_KeyError, key);
+    return NULL;
+}
+
+static PyObject *t_nil_get(t_nil *self, PyObject *args)
+{
+    PyObject *key, *value = Py_None;
+
+    if (!PyArg_ParseTuple(args, "O|O", &key, &value))
+        return NULL;
+
+    Py_INCREF(value);
+    return value;
+}
+
+static PyObject *t_nil_iternext(t_nil *self)
+{
+    PyErr_SetNone(PyExc_StopIteration);
+    return NULL;
+}
 
 
 void _init_item(PyObject *m)
