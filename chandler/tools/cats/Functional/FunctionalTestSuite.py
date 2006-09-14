b1f89f1bf1e90c1f74a1113398a6078908086892
dan <dan@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-09-14 22:03:07 +0000

    Re-enabling tests excluded in commit 11777
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@11780 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/cats/Functional/FunctionalTestSuite.py
+++ chandler/tools/cats/Functional/FunctionalTestSuite.py
@@ -68,11 +68,9 @@ else:
     
 exclusions = {#tests to exclude on specific platfoms only
     'other':[
-        ("TestTableSelection","TestTableSelection"),#6726
     ],
     
     'mac':[
-        ("TestRemoveFromTrashOnImport","TestRemoveFromTrashOnImport"),#bug 6727
      
     ],
     
