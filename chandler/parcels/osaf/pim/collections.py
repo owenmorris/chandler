owenmorris/chandler

commit 7ebd433d0631992e89f4e4211fcc5435c24b15f5

- upgraded build to chandlerdb 0.6-51    - fixed bug 7018      (http://bugzilla.osafoundation.org/show_bug.cgi?id=7018)      (some monitors need to be fired after item init)
git-svn-id: http://svn.osafoundation.org/chandler/trunk@12016 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

chandler/repository/item/Item.py
@@ -37,7 +37,7 @@ class Item(CItem):
     """
 
     def __init__(self, itsName=None, itsParent=None, itsKind=None,
-                 _uuid=None, _noMonitors=False, fireAfterChange=True,
+                 _uuid=None, _noMonitors=False, fireChanges=True,
                  **values):
         """
         Construct an Item.
@@ -94,8 +94,7 @@ class Item(CItem):
             else:
                 self.setDirty(Item.NDIRTY | Item.NODIRTY)
 
-            if values:
-                self._setInitialValues(values, fireAfterChange)
+            self._setInitialValues(values, fireChanges)
         finally:
             self._status &= ~Item.NODIRTY
 
@@ -104,18 +103,23 @@ class Item(CItem):
                                        'add', 'collection', 'extent',
                                        self.itsUUID)
 
-    def _setInitialValues(self, values, fireAfterChange):
+    # fire afterChange methods on initial keyword values
+    # fire system monitors on all values, initial or set during afterChange
+    def _setInitialValues(self, values, fireChanges):
 
         for name, value in values.iteritems():
             setattr(self, name, value)
 
-        if fireAfterChange:
-            kind = self.itsKind
-            if kind is not None:
-                for name in values.iterkeys():
-                    attr = kind.getAttribute(name, True, self)
-                    if attr is not None:
-                        attr.c.invokeAfterChange(self, 'set', name)
+        if fireChanges:
+            self._status &= ~Item.NODIRTY
+            try:
+                self._status |= Item.SYSMONONLY
+                for name in self._values.keys():
+                    self._fireChanges('set', name, name in values)
+                for name in self._references.keys():
+                    self._fireChanges('set', name, name in values)
+            finally:
+                self._status &= ~Item.SYSMONONLY
 
     def __iter__(self):
         """
@@ -1339,6 +1343,7 @@ class Item(CItem):
             item._status |= Item.NODIRTY
             item._values._copy(self._values, copyPolicy, copyFn)
             item._references._copy(self._references, copyPolicy, copyFn)
+            item._setInitialValues(Nil, True)
         finally:
             item._status &= ~Item.NODIRTY
             
@@ -1359,7 +1364,7 @@ class Item(CItem):
         return item
 
     def clone(self, name=None, parent=None,
-              exclude=(), fireAfterChange=True, **values):
+              exclude=(), fireChanges=True, **values):
 
         cls = type(self)
         item = cls.__new__(cls)
@@ -1378,9 +1383,7 @@ class Item(CItem):
             item._status |= Item.NODIRTY
             item._values._clone(self._values, exclude)
             item._references._clone(self._references, exclude)
-
-            if values:
-                item._setInitialValues(values, fireAfterChange)
+            item._setInitialValues(values, fireChanges)
         finally:
             item._status &= ~Item.NODIRTY
         

chandler/repository/item/Monitors.py
@@ -97,6 +97,8 @@ class Monitors(Item):
         else:
             dispatcher._cacheMonitor(monitor)
 
+        return monitor
+
     @classmethod
     def detach(cls, item, method, op, attribute, *args, **kwds):
 

chandler/repository/item/Sets.py
@@ -16,6 +16,7 @@
 from itertools import izip
 
 from chandlerdb.util.c import UUID, isuuid, Nil
+from chandlerdb.item.c import CItem
 from chandlerdb.persistence.c import CView
 from chandlerdb.item.ItemValue import ItemValue
 from repository.item.Monitors import Monitors
@@ -1341,8 +1342,9 @@ class FilteredSet(Set):
                 if item is not None:
                     if attrs:
                         for name, op in attrs:
-                            Monitors.attach(item, '_filteredItemChanged',
-                                            op, name, attribute)
+                            mon = Monitors.attach(item, '_filteredItemChanged',
+                                                  op, name, attribute)
+                            mon._status |= CItem.SYSMONITOR
 
         return oldItem, oldAttribute
 

chandler/Makefile
@@ -19,7 +19,7 @@ BUILD_PLUGINS = Chandler-HelloWorldPlugin \
 # When a version changes, the ARCHIVES lists below needs to be updated.
 # these get installed into release or debug
 ARCHIVES = $(CHANDLERARCHIVES)/Launchers-$(SNAP)-0.8-$(BP)G.tar.gz \
-           $(CHANDLERARCHIVES)/chandlerdb-$(SNAP)-0.6-$(BP)50.tar.gz \
+           $(CHANDLERARCHIVES)/chandlerdb-$(SNAP)-0.6-$(BP)51.tar.gz \
            $(CHANDLERARCHIVES)/db-$(SNAP)-4.4.20-$(BP)5.tar.gz \
            $(CHANDLERARCHIVES)/python-$(SNAP)-2.4.3-$(BP)3.tar.gz \
            $(CHANDLERARCHIVES)/epydoc-$(SNAP)-2.1-$(BP)9.tar.gz \

chandler/repository/item/Indexed.py
@@ -13,9 +13,10 @@
 #   limitations under the License.
 
 
+from chandlerdb.item.c import CItem
+from chandlerdb.item.ItemError import *
 from repository.item.Indexes import \
     AttributeIndex, ValueIndex, StringIndex, CompareIndex, SubIndex
-from chandlerdb.item.ItemError import *
 
 
 # A mixin class used by ref collections and abstract sets that provides
@@ -112,6 +113,7 @@ class Indexed(object):
         """
 
         item, name = self._getOwner()
+        view = self._getView()
 
         if self._indexes is not None:
             if indexName in self._indexes:
@@ -121,44 +123,38 @@ class Indexed(object):
 
         index = self._createIndex(indexType, **kwds)
 
-        if not (self._getView().isLoading() or kwds.get('loading', False)):
+        if not (view.isLoading() or kwds.get('loading', False)):
             self.fillIndex(index)
             self._setDirty(True) # noMonitors=True
             monitor = kwds.get('monitor')
 
-            if monitor is not None:
+            def _attach(attrName):
                 from repository.item.Monitors import Monitors
+                mon = Monitors.attach(item, '_reIndex', 'set', attrName,
+                                      name, indexName)
+                mon._status |= CItem.SYSMONITOR
+                mon = Monitors.attach(item, '_reIndex', 'remove', attrName,
+                                      name, indexName)
+                mon._status |= CItem.SYSMONITOR
+
+            if monitor is not None:
                 if isinstance(monitor, (str, unicode)):
-                    Monitors.attach(item, '_reIndex',
-                                    'set', monitor, name, indexName)
-                    Monitors.attach(item, '_reIndex',
-                                    'remove', monitor, name, indexName)
+                    _attach(monitor)
                 else:
                     for m in monitor:
-                        Monitors.attach(item, '_reIndex',
-                                        'set', m, name, indexName)
-                        Monitors.attach(item, '_reIndex',
-                                        'remove', m, name, indexName)
+                        _attach(m)
                     
             elif indexType in ('attribute', 'value', 'string'):
-                from repository.item.Monitors import Monitors
                 attributes = kwds.get('attributes', None)
                 if attributes is not None:
                     for attribute in attributes:
-                        Monitors.attach(item, '_reIndex',
-                                        'set', attribute, name, indexName)
-                        Monitors.attach(item, '_reIndex',
-                                        'remove', attribute, name, indexName)
+                        _attach(attribute)
                 else:
-                    attribute = kwds['attribute']
-                    Monitors.attach(item, '_reIndex',
-                                    'set', attribute, name, indexName)
-                    Monitors.attach(item, '_reIndex',
-                                    'remove', attribute, name, indexName)
+                    _attach(kwds['attribute'])
 
             if indexType == 'subindex':
                 uuid, superName, superIndexName = index._super
-                superIndex = getattr(self._getView()[uuid],
+                superIndex = getattr(view[uuid],
                                      superName).getIndex(superIndexName)
                 superIndex.addSubIndex(item.itsUUID, name, indexName)
 

chandler/repository/schema/Kind.py
@@ -240,6 +240,7 @@ class Kind(Item):
             item.onItemLoad(self.itsView)
 
         if not _noMonitors:
+            item._setInitialValues(Nil, True)
             self.itsView._notifyChange(self.extent._collectionChanged,
                                        'add', 'collection', 'extent',
                                        item.itsUUID)

commit 1d99bb6ec8e513f839cae06632dde9ee93a646eb

- fixed bug 7112      (http://bugzilla.osafoundation.org/show_bug.cgi?id=7112)      (Import calendar performance regression with r12016)    - introduced 'init' monitor op code used during initial value setting
git-svn-id: http://svn.osafoundation.org/chandler/trunk@12111 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

chandler/parcels/osaf/pim/collections.py
@@ -475,11 +475,7 @@ class FilteredCollection(SingleSourceWrapperCollection):
         if source is None:
             s = EmptySet()
         else:
-            attrTuples = set()
-            for name in self.filterAttributes:
-                attrTuples.add((name, "set"))
-                attrTuples.add((name, "remove"))
-            attrs = tuple(attrTuples)
+            attrs = tuple(self.filterAttributes)
 
             if hasattr(self, 'filterExpression'):
                 s = ExpressionFilteredSet(source, self.filterExpression, attrs)

chandler/parcels/osaf/pim/mail.py
@@ -819,7 +819,7 @@ class MailStamp(stamping.Stamp, MIMEContainer):
     # on with schema.observer here.
     @schema.observer(toAddress, stamping.Stamp.stamp_types)
     def updateWho(self, op, name):
-        if op == 'set' and stamping.has_stamp(self, MailStamp):
+        if op in ('init', 'set') and stamping.has_stamp(self, MailStamp):
             self.itsItem.who = u", ".join(unicode(x) for x in self.toAddress)
         else:
             self.itsItem.who = u""

chandler/repository/item/Item.py
@@ -115,9 +115,9 @@ class Item(CItem):
             try:
                 self._status |= Item.SYSMONONLY
                 for name in self._values.keys():
-                    self._fireChanges('set', name, name in values)
+                    self._fireChanges('init', name, name in values)
                 for name in self._references.keys():
-                    self._fireChanges('set', name, name in values)
+                    self._fireChanges('init', name, name in values)
             finally:
                 self._status &= ~Item.SYSMONONLY
 

chandler/repository/item/Monitors.py
@@ -57,7 +57,7 @@ class Monitors(Item):
     def cacheMonitors(self):
 
         view = self.itsView
-        view._monitors = { 'set': {}, 'remove': {} }
+        view._monitors = { 'init': {}, 'set': {}, 'remove': {} }
         self.itsView.MONITORING = True
 
         for monitor in getattr(self, 'monitors', []):

chandler/repository/item/Sets.py
@@ -1336,15 +1336,23 @@ class FilteredSet(Set):
                 attrs = self.attributes
                 if oldItem is not None:
                     if attrs:
-                        for name, op in attrs:
+                        def detach(op, name):
                             Monitors.detach(oldItem, '_filteredItemChanged',
                                             op, name, oldAttribute)
+                        for name in attrs:
+                            detach('init', name)
+                            detach('set', name)
+                            detach('remove', name)
                 if item is not None:
                     if attrs:
-                        for name, op in attrs:
+                        def attach(op, name):
                             mon = Monitors.attach(item, '_filteredItemChanged',
                                                   op, name, attribute)
                             mon._status |= CItem.SYSMONITOR
+                        for name in attrs:
+                            attach('init', name)
+                            attach('set', name)
+                            attach('remove', name)
 
         return oldItem, oldAttribute
 

chandler/repository/persistence/RepositoryView.py
@@ -65,8 +65,9 @@ class RepositoryView(CView):
     # 0.6.11: removed Kind inheritedSuperKinds transient cache
     # 0.6.12: removed 'persisted' aspect
     # 0.6.13: added IndexMonitor class
+    # 0.6.14: added support for 'init' monitor op
     
-    CORE_SCHEMA_VERSION = 0x00060d00
+    CORE_SCHEMA_VERSION = 0x00060e00
 
     def __init__(self, repository, name, version):
         """

chandler/repository/schema/Types.py
@@ -960,7 +960,7 @@ class ConstantEnumeration(Enumeration):
     def _fillItem(self, *args):
         super(ConstantEnumeration, self)._fillItem(*args)
         if 'values' in self._values:
-            self._afterValuesChange('set', 'values')
+            self._afterValuesChange('init', 'values')
 
     def getImplementationType(self):
         return EnumValue
@@ -1000,7 +1000,7 @@ class ConstantEnumeration(Enumeration):
 
     def _afterValuesChange(self, op, name):
 
-        if op == 'set':
+        if op in ('init', 'set'):
             constants = [SchemaEnumValue(self, name, value)
                          for name, value in self._values['values']]
             constants.sort(None, lambda x: x.name)