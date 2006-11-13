338354da409c7398741f53a9dc7dd971917e8093
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2006-11-13 17:08:36 +0000

    Per pje, no need to expose sharing.UUIDType; record types can use schema.UUID because of a typedef in eim.py
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@12312 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/sharing/recordtypes.py
+++ chandler/parcels/osaf/sharing/recordtypes.py
@@ -1,10 +1,11 @@
 from osaf import sharing
+from application import schema
 
 # These may eventually live elsewhere, but checking them in here so we can
 # collaborate on them
 
 class ItemRecord(sharing.Record):
-    uuid = sharing.key(sharing.UUIDType)
+    uuid = sharing.key(schema.UUID)
     title = sharing.field(sharing.TextType(size=256))
     triage_status = sharing.field(sharing.TextType(size=256))
     # triage_status_changed = sharing.field(sharing.FloatType) # needs to be addedto eim.py
@@ -12,15 +13,15 @@ class ItemRecord(sharing.Record):
     created_on = sharing.field(sharing.DateType)
 
 class NoteRecord(sharing.Record):
-    uuid = sharing.key(sharing.UUIDType)
+    uuid = sharing.key(schema.UUID)
     body = sharing.field(sharing.LobType())
     icaluid = sharing.field(sharing.TextType(size=256))
 
 class TaskRecord(sharing.Record):
-    uuid = sharing.key(sharing.UUIDType)
+    uuid = sharing.key(schema.UUID)
 
 class EventRecord(sharing.Record):
-    uuid = sharing.key(sharing.UUIDType)
+    uuid = sharing.key(schema.UUID)
     dtstart = sharing.field(sharing.DateType)
     dtend = sharing.field(sharing.DateType)
     location = sharing.field(sharing.TextType(size=256))
@@ -35,7 +36,7 @@ class EventRecord(sharing.Record):
 
 
 class MailMessageRecord(sharing.Record):
-    uuid = sharing.key(sharing.UUIDType)
+    uuid = sharing.key(schema.UUID)
     subject = sharing.field(sharing.TextType(size=256))
     to = sharing.field(sharing.TextType(size=256))
     cc = sharing.field(sharing.TextType(size=256))
