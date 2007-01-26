3b0defd6d350d476f2ed73d2c8b753f184e4fb3c
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-01-26 07:40:36 +0000

    Ah - figured out why deb files where being created but not gathered. The variable substitution needed to be less ambiguous: ${FOO} vs $FOO
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@12947 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/installers/deb/makeinstaller.sh
+++ internal/installers/deb/makeinstaller.sh
@@ -21,26 +21,26 @@ fi
 
 if [ -d "$DEB_PATH" ]; then
     echo "Clearing debian chandler working image"
-    if [ -d "$DEB_PATH/chandler" ]; then
-        rm -rf $DEB_PATH/chandler
+    if [ -d "{$DEB_PATH}/chandler" ]; then
+        rm -rf ${DEB_PATH}/chandler
     fi
-    rm -f $DEB_PATH/chandler_*.deb
+    rm -f ${DEB_PATH}/chandler_*.deb
     echo "Preparing build tree"
-    cd $DEB_PATH
+    cd ${DEB_PATH}
     mkdir -p chandler/usr/local
     mkdir chandler/DEBIAN/
     echo "Creating build tree from distribution tarball"
-    tar xzf $DISTRIB_PATH/$DISTRIB_FILE.tar.gz
-    mv $DISTRIB_FILE chandler/usr/local/chandler
+    tar xzf ${DISTRIB_PATH}/${DISTRIB_FILE}.tar.gz
+    mv ${DISTRIB_FILE} chandler/usr/local/chandler
     echo "Ensuring all files have a+r set"
     chmod -R a+r chandler/usr/local/chandler/
-    CHANDLER_SIZE=`du -c -b $DEB_PATH/chandler/usr/local/chandler | tail -n1 | awk '{ print $1 }'`
-    sed -e "s/CHANDLER_VERSION/$DISTRIB_VERSION.$DISTRIB_RELEASE/" -e "s/CHANDLER_SIZE/$CHANDLER_SIZE/" < $DEB_PATH/control.in > $DEB_PATH/chandler/DEBIAN/control
+    CHANDLER_SIZE=`du -c -b ${DEB_PATH}/chandler/usr/local/chandler | tail -n1 | awk '{ print $1 }'`
+    sed -e "s/CHANDLER_VERSION/${DISTRIB_VERSION}.${DISTRIB_RELEASE}/" -e "s/CHANDLER_SIZE/${CHANDLER_SIZE}/" < ${DEB_PATH}/control.in > ${DEB_PATH}/chandler/DEBIAN/control
     echo `pwd`
-    echo "Calling dpkg-deb -b chandler chandler_$DISTRIB_VERSION.$DISTRIB_RELEASE-1_i386.deb"
-    dpkg-deb -b chandler chandler_$DISTRIB_VERSION.$DISTRIB_RELEASE-1_i386.deb
-    echo "Moving $DEB_PATH/chandler_$DISTRIB_VERSION.$DISTRIB_RELEASE-1_i386.deb to $DISTRIB_PATH/$DISTRIB_FILE_i386.deb"
-    mv $DEB_PATH/chandler_$DISTRIB_VERSION.$DISTRIB_RELEASE-1_i386.deb $DISTRIB_PATH/$DISTRIB_FILE_i386.deb
+    echo "Calling dpkg-deb -b chandler chandler_${DISTRIB_VERSION}.${DISTRIB_RELEASE}-1_i386.deb"
+    dpkg-deb -b chandler chandler_${DISTRIB_VERSION}.${DISTRIB_RELEASE}-1_i386.deb
+    echo "Moving ${DEB_PATH}/chandler_${DISTRIB_VERSION}.${DISTRIB_RELEASE}-1_i386.deb to ${DISTRIB_PATH}/${DISTRIB_FILE}_i386.deb"
+    mv ${DEB_PATH}/chandler_${DISTRIB_VERSION}.${DISTRIB_RELEASE}-1_i386.deb ${DISTRIB_PATH}/${DISTRIB_FILE}_i386.deb
     echo "deb generation done"
 else
     echo "ERROR: debian local environment does not seem to be setup"
