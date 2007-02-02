dad56ceef84eaecee838598567fccba539fa43a4
jeffrey <jeffrey@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-02-02 02:27:34 +0000

    - First pass at EIM Event import code, no recurrence manipulation yet. - Remove anyTime as part of the EventRecord model, keep it as part of   DTSTART like it is in iCalendar sharing
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@13018 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/sharing/translator.py
+++ chandler/parcels/osaf/sharing/translator.py
@@ -3,14 +3,85 @@ from osaf import pim
 from osaf.sharing import eim, model
 from PyICU import ICUtzinfo
 import time
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from decimal import Decimal
+
+from vobject.base import textLineToContentLine
+from vobject.icalendar import DateOrDateTimeBehavior
+import osaf.pim.calendar.TimeZone as TimeZone
 
 __all__ = [
     'PIMTranslator',
 ]
 
 COSMO_MODE = False
+
+utc = ICUtzinfo.getInstance('UTC')
+oneDay = timedelta(1)
+
+def with_nochange(value, converter, view=None):
+    if value is eim.NoChange:
+	return value
+    if view is None:
+	return converter(value)
+    else:
+	return converter(value, view)
+
+
+### Event field conversion functions
+# incomplete
+
+def transparency(val):
+    out = val.lower()
+    if out == 'cancelled':
+        out = 'fyi'
+    elif out not in ('confirmed', 'tentative'):
+        out = 'confirmed'
+    return out
+
+def location(val, view):
+    return pim.Location.getLocation(view, val)
+
+def readICalendarDateTime(text):
+    line = textLineToContentLine('dtstart' + text)
+    native_line = DateOrDateTimeBehavior.transformToNative(line)
+    anyTime = getattr(native_line, 'x_osaf_anytime_param', "").upper() == 'TRUE'
+    allDay = False
+    start = native_line.value
+    if type(start) == date:
+	allDay = not anyTime
+	start = TimeZone.forceToDateTime(start)
+    else:
+	# this parameter is broken, this should be fixed in vobject, at which
+	# point this will break
+	tzid = native_line.params.get('X-VOBJ-ORIGINAL-TZID')
+	if tzid is None:
+	    tzinfo = ICUtzinfo.floating
+	else:
+	    tzinfo = ICUtzinfo.getInstance(tzid)
+	start = start.replace(tzinfo=tzinfo)
+    return (start, allDay, anyTime)
+
+def getTimeValues(record):
+    """
+    Extract start time, end time, and allDay/anyTime from a record.
+    """
+    dtstart = record.dtstart
+    dtend   = record.dtend
+    if dtstart is not eim.NoChange:
+	start, allDay, anyTime = readICalendarDateTime(dtstart)
+    else:
+	allDay = anyTime = start = eim.NoChange
+	
+    if dtend is not eim.NoChange:
+	end, end_allDay, end_anyTime = readICalendarDateTime(dtend)
+	if end_allDay or end_anyTime:
+	    # iCalendar syntax for serializing all day dtends is off by one day;
+	    end -= oneDay 
+    else:
+	end = eim.NoChange
+    
+    return (start, end, allDay, anyTime)
 
 class PIMTranslator(eim.Translator):
 
@@ -33,8 +104,6 @@ class PIMTranslator(eim.Translator):
                 displayName=record.title,
             ) # incomplete
             return
-
-        utc = ICUtzinfo.getInstance('UTC')
 
         if record.createdOn is not eim.NoChange:
             # createdOn is a Decimal we need to change to datetime
@@ -63,12 +132,11 @@ class PIMTranslator(eim.Translator):
             triageStatus=triageStatus,
             triageStatusChanged=tsc,
             createdOn=createdOn
-        ) # incomplete
+        ) # incomplete, missing lastModifiedBy
 
 
     @eim.exporter(pim.ContentItem)
     def export_item(self, item):
-        utc = ICUtzinfo.getInstance('UTC')
         yield model.ItemRecord(
             item.itsUUID,                               # uuid
             item.displayName,                           # title
@@ -140,11 +208,29 @@ class PIMTranslator(eim.Translator):
                 pim.EventStamp,
             ) # incomplete
             return
-
+	
+	start, end, allDay, anyTime = getTimeValues(record)
+	if end is eim.NoChange and start is not eim.NoChange:
+	    # odd case, Chandler's object model doesn't allow start to change
+	    # without changing end, so explicitly set both start and end time
+	    # appropriately
+	    item = self.rv.findUUID(record.uuid)
+	    if item is not None:
+		event = pim.EventStamp(item)
+		end = event.endTime
+		event.startTime = start
+		event.endTime = end
+		end = start = eim.NoChange
+	
         self.loadItemByUUID(
             record.uuid,
             pim.EventStamp,
-            transparency=record.status
+	    startTime=start,
+	    endTime=end,
+	    allDay=allDay,
+	    anyTime=anyTime,
+            transparency = with_nochange(record.status, transparency),
+	    location     = with_nochange(record.location, location, self.rv),
         ) # incomplete
 
 
