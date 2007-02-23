4a96aba057a4e4bfc16e240dd9cf63747c80b066
jeffrey <jeffrey@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-02-23 04:43:18 +0000

    - Fix for bug 8205, Apple Mail qutoed-printable encoding of ics not handled
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@13267 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/mail/message.py
+++ chandler/parcels/osaf/mail/message.py
@@ -176,6 +176,8 @@ def messageTextToKind(view, messageText, indexText=False, compression='bz2'):
                                messageText, compression)
 
 
+quoted_printable_header = ('Content-Transfer-Encoding', 'quoted-printable')
+
 def messageObjectToKind(view, messageObject, messageText=None,
                         indexText=False, compression='bz2'):
     """
@@ -204,8 +206,11 @@ def messageObjectToKind(view, messageObject, messageText=None,
     def importIcalendarPayload(messageObject):
         if messageObject.get_content_type() == "text/calendar":
             import osaf.sharing.ICalendar as ICalendar
+            ics = messageObject.get_payload()
+            if quoted_printable_header in messageObject._headers:
+                ics = email.quoprimime.decodestring(ics)
             try:
-                items = ICalendar.itemsFromVObject(view, messageObject.get_payload(),
+                items = ICalendar.itemsFromVObject(view, ics,
                                                    filters=(Remindable.reminders.name,))[0]
             except:
                 # ignore parts we can't parse [log something?]
