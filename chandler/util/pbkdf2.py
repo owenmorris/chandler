233e286a99a30c054db7e5a10ec5f649c0d8d573
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-03-08 02:54:53 +0000

    I noticed a bug (hardcoded digest size); here is the fixed version from Matt.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@13422 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/util/pbkdf2.py
+++ chandler/util/pbkdf2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # A simple implementation of pbkdf2 using stock python modules. See RFC2898
 # for details. Basically, it derives a key from a password and salt.
@@ -6,21 +6,35 @@
 # (c) 2004 Matt Johnston <matt @ ucc asn au>
 # This code may be freely used and modified for any purpose.
 
-import sha
-import hmac
+# Revision history
+# v0.1  October 2004    - Initial release
+# v0.2  8 March 2007    - Make usable with hashlib in Python 2.5 and use
+#                         the correct digest_size rather than always 20
 
+import sys
+import hmac
 from binascii import hexlify, unhexlify
 from struct import pack
-
-BLOCKLEN = 20
+try:
+	# only in python 2.5
+	import hashlib
+	sha = hashlib.sha1
+	md5 = hashlib.md5
+	sha256 = hashlib.sha256
+except ImportError:
+	# fallback
+	import sha
+	import md5
 
 # this is what you want to call.
 def pbkdf2( password, salt, itercount, keylen, hashfn = sha ):
-
-	
+	if 'hashlib' in sys.modules:
+		digest_size = hashfn().digest_size
+	else:
+		digest_size = hashfn.digest_size		
 	# l - number of output blocks to produce
-	l = keylen / BLOCKLEN
-	if keylen % BLOCKLEN != 0:
+	l = keylen / digest_size
+	if keylen % digest_size != 0:
 		l += 1
 
 	h = hmac.new( password, None, hashfn )
@@ -29,10 +43,9 @@ def pbkdf2( password, salt, itercount, keylen, hashfn = sha ):
 	for i in range(1, l+1):
 		T += pbkdf2_F( h, salt, itercount, i )
 
-	return T[: -( BLOCKLEN - keylen%BLOCKLEN) ]
+	return T[: -( digest_size - keylen % digest_size) ]
 
 def xorstr( a, b ):
-	
 	if len(a) != len(b):
 		raise "xorstr(): lengths differ"
 
@@ -50,7 +63,6 @@ def prf( h, data ):
 # Helper as per the spec. h is a hmac which has been created seeded with the
 # password, it will be copy()ed and not modified.
 def pbkdf2_F( h, salt, itercount, blocknum ):
-
 	U = prf( h, salt + pack('>i',blocknum ) )
 	T = U
 
@@ -69,7 +81,8 @@ def test():
 	itercount = 500
 	keylen = 16
 	ret = pbkdf2( password, salt, itercount, keylen )
-	print "key:      %s" % hexlify( ret )
+	hexret = ' '.join(map(lambda c: '%02x' % ord(c), ret)).upper()
+	print "key:      %s" % hexret
 	print "expected: 6A 89 70 BF 68 C9 2C AE A8 4A 8D F2 85 10 85 86"
 
 if __name__ == '__main__':
