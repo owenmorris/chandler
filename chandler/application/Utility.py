f87831cfe25b3d7f66d6bcbc55036599dd8c657d
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-04-04 20:12:33 +0000

    Bug 8662, Ctrl+Alt+Left/Right for prev/next week conflicted with workspace switching shortcuts on Ubuntu, so changing them to Ctrl+Shift+Left/Right.
    Bug 8655, Ctrl+S in Run CPIA script conflicted with File > Save Changes, so changing the CPIA
    script shortcut to Ctrl+R.
    
    This patch also includes mnemonic fixes for new or moved menu items since the last round of
    mnemonic fixes.
    
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@13801 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -35,7 +35,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "361" # john: More menu changes and fixes to calendar event dispatching
+SCHEMA_VERSION = "362" # heikki: more mnemonics fixes
 
 logger = None # initialized in initLogging()
 
