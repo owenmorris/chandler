deafe5b57c4a1ec6135044b83986435f9ab08618
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-04-05 05:52:14 +0000

    Found yet more shortcuts and mnemonics that needed fixin'...
    Main menu and context menu disagreed what Ctrl+Del and Del were supposed to do; now Del
    is Remove in both and Ctrl+Del is Delete in both.
    
    This because Ctrl+Shift+T was reserved:
    
    Jump to date... Ctrl+G
    
    New specific items per the spec:
    
    New note Ctrl+Shift+N
    New task Ctrl+Shift+T
    New message Ctrl+Shift+M
    New event Ctrl+Shift+E
    
    Removed the always disabled New contact menu item.
    
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@13822 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -35,7 +35,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "364" # capps: menus (Remove/Delete moved, MoveTo/AddTo gone)
+SCHEMA_VERSION = "365" # heikki: Shortcut and mnemonic fixes
 
 logger = None # initialized in initLogging()
 
