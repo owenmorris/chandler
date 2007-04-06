e2b557a86ef20d17f571568981bc93f7a1022b6a
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-04-06 20:10:33 +0000

    bump schema  version
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@13854 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -35,7 +35,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "366" # heikki: Password records
+SCHEMA_VERSION = "367" # heikki: eventsForNamedLookup no longer copied 
 
 logger = None # initialized in initLogging()
 
