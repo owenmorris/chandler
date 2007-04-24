3854d78ef1436a5b2fba38b9aa3e3cc999f3e586
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-04-24 23:54:45 +0000

    Missed import.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14074 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/servlets/prefs.py
+++ chandler/parcels/osaf/servlets/prefs.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-
+import os
 from application import schema
 
 from osaf import webserver, Preferences
