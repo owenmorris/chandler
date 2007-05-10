b9b9dc41528f5f54a80ec72059a36c212dd67bc9
rae <rae@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-05-10 21:35:13 +0000

    Forgot to bump the schema version (oops!)
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14254 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -34,7 +34,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "379" # morgen: Force repo rebuild because of new pseudo-uuids
+SCHEMA_VERSION = "380" # rae: added ConflictConditionalBehaviour
 
 logger = None # initialized in initLogging()
 
