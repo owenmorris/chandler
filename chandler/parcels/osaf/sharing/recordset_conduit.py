6a02ba6399b94731174ae14dc1b812848832a89f
jeffrey <jeffrey@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-05-15 21:38:54 +0000

    - Fix TestICalendar failure
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14309 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/sharing/recordset_conduit.py
+++ chandler/parcels/osaf/sharing/recordset_conduit.py
@@ -562,7 +562,7 @@ class RecordSetConduit(conduits.BaseConduit):
 
         # Send if there is something to send or even if this is just an
         # initial publish of an empty collection:
-        if send and toSend or not self.share.established:
+        if send and (toSend or not self.share.established):
             sendCount = len(toSend)
             _callback(msg="Sending %d outbound change(s)" % sendCount,
                 totalWork=None)
