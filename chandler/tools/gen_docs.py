36dda5451baf4ab81fda420ada5f6d7236c353bf
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-05-22 07:31:22 +0000

    Update to the current versions of:     epydoc - 3.0beta1     pylint - 0.13.1     logilab-common - 0.21.2     logilab-astng - 0.17.0 Also updated gen_docs.py to use the new epydoc interface. bug 8938
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14372 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/gen_docs.py
+++ chandler/tools/gen_docs.py
@@ -587,171 +587,33 @@ def find_packages(where='.', exclude=()):
     return out
 
 def generateDocs(options, outputDir):
-    from epydoc.html import HTMLFormatter
-    from epydoc.objdoc import DocMap, report_param_mismatches
-    from epydoc.imports import import_module, find_modules
-    from epydoc.objdoc import set_default_docformat
+    import epydoc.cli
+
+    sys.argv += [ 'Chandler.py',
+                  'PyICU', 'PyLucene',
+                  'chandlerdb', 'chandlerdb.item',
+                  'chandlerdb.persistence',
+                  'chandlerdb.schema', 'chandlerdb.util',
+                  'application', 'i18n', 'repository' ]
+
+    e_options, names = epydoc.cli.parse_arguments()
+
+    e_options.prj_name    = 'Chandler'
+    e_options.exclude     = ['tests']
+    e_options.simple_term = True
+    e_options.target      = os.path.join(outputDir, 'api')
 
     if options.verbose:
-        verbosity = 4
+        e_options.verbosity = 4
     else:
-        verbosity = 1
+        e_options.verbosity = 1
 
-    chandlerBin = os.getenv('CHANDLERBIN')
-
-    targetDir = os.path.join(outputDir, 'api')
-
-    if not os.path.isdir(targetDir):
-        _mkdirs(targetDir)
-
-      # This is the options dictionary
-      # It is used by most of the epydoc routines and
-      # the contents were determined by examining epydoc/gui.py
-      # and epydoc/cli.py
-
-    source_modules = ['samples/skeleton', 'Chandler.py', 'version.py', 
-                      'PyICU', 
-                      'chandlerdb', 'chandlerdb.item', 'chandlerdb.persistence', 
-                      'chandlerdb.schema', 'chandlerdb.util']
-
-    source_modules += find_modules('application')
-    source_modules += find_modules('i18n')
-    source_modules += find_modules('repository')
-    source_modules += find_modules('tools')
-    
-    source_modules += find_packages('application', exclude=['*.tests'])
-    source_modules += find_packages('i18n',        exclude=['*.tests'])
-    source_modules += find_packages('repository',  exclude=['*.tests'])
-    source_modules += find_packages('tools',       exclude=['*.tests'])
-
-    parcels = find_packages('parcels', exclude=['*.tests'])
-
-    map(schema.importString, parcels)
-    for name,module in sys.modules.items():
-        if module is not None and name.rsplit('.', 1)[0] in parcels:
-            source_modules += [name]
-
-    e_options = { 'target':       targetDir,
-                  'verbosity':    verbosity,
-                  'prj_name':     'Chandler',
-                  'action':       'html',
-                  'tests':        { 'basic': 1 },
-                  'show_imports': 0,
-                  'frames':       1,
-                  'private':      0,
-                  'debug':        0,
-                  'docformat':    None,
-                  'top':          None,
-                  'inheritance':  "listed",
-                  'alphabetical': 1,
-                  'ignore_param_mismatch':   1,
-                  'list_classes_separately': 0,
-                  'modules': source_modules,
-                 }
-
-      # based on the code in epydoc's gui.py
-      # with subtle changes made to make it work :)
-
-    set_default_docformat('epytext')
-
-    try:
-        modules      = []
-        module_names = e_options['modules']
-
-          # walk thru list of modules and expand
-          # any packages found
-        for name in module_names[:]:
-            if os.path.isdir(name):
-                index       = module_names.index(name)
-                new_modules = find_modules(name)
-
-                if new_modules:
-                    module_names[index:index+1] = new_modules
-                elif options.verbose:
-                    print 'Error: %s is not a package' % name
-
-          # basic regex to exclude directories from consideration
-        exc = re.compile(".*tests.*|.*scripts.*")
-
-        for name in module_names:
-            if exc.match(name):
-                continue
-
-            if options.verbose:
-                print 'IMPORT: %s' % name
-
-              # try importing the module and
-              # add it to the list if successful
-            try:
-                module = import_module(name)
-
-                if module not in modules:
-                    modules.append(module)
-                elif options.verbose:
-                    print '  (duplicate)'
-            except ImportError, e:
-                if options.verbose:
-                    print e
-
-        if len(modules) == 0:
-            print 'Error: no modules successfully loaded!'
-            sys.exit(1)
-
-        document_bases        = 1
-        document_autogen_vars = 1
-        inheritance_groups    = (e_options['inheritance'] == 'grouped')
-        inherit_groups        = (e_options['inheritance'] != 'grouped')
-
-          # let epydoc create an empty document map
-        d = DocMap(verbosity, document_bases, document_autogen_vars,
-                   inheritance_groups, inherit_groups)
-
-          # walk the module list and let epydoc build the documentation
-        for (module, num) in zip(modules, range(len(modules))):
-            if options.verbose:
-                print '\n***Building docs for %s***' % module.__name__
-
-            try:
-                d.add(module)
-            except Exception, e:
-                print "Internal Error: %s" % e
-            except:
-                print "Internal Error"
-
-        if not e_options['ignore_param_mismatch']:
-            if not report_param_mismatches(d):
-                print '    (To supress these warnings, use --ignore-param-mismatch)'
-
-        htmldoc  = HTMLFormatter(d, **e_options)
-        numfiles = htmldoc.num_files()
-
-        def progress_callback(path):
-            (dir, file) = os.path.split(path)
-            (root, d)   = os.path.split(dir)
-
-            if d in ('public', 'private'):
-                fname = os.path.join(d, file)
-            else:
-                fname = file
-
-            if options.verbose:
-                print '\n***Writing %s***' % fname
-
-          # Write the documentation.
-        print "\n***Saving to %s" % targetDir
-
-        htmldoc.write(targetDir, progress_callback)
-
-    except Exception, e:
-        print 'Internal error: ', e
-        raise
-    except:
-        raise
+    epydoc.cli.main(e_options, names)
 
 
 if __name__ == '__main__':
     options = Utility.initOptions()
-    
+
     Utility.initLogging(options)
 
     if options.args:
