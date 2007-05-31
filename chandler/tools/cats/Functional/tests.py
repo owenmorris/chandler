02edb2a8d8897a0fad28fea77b5df2175d645273
dan <dan@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-05-31 22:23:40 +0000

    re-enable functional test TestFlickr as it seems to be passing now
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14503 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/cats/Functional/tests.py
+++ chandler/tools/cats/Functional/tests.py
@@ -72,7 +72,6 @@ exclusions = {#tests to exclude on specific platfoms only
         ("TestDates","TestDates"), #Chandler not handling daylightsavings bug#5038
         ("TestEditModeOnEnter","TestEditModeOnEnter"), #Chandler bug 5744
         ("TestVisibleHours","TestVisibleHours"), # bug 8969
-        ("TestFlickr", "TestFlickr"), # bug 9347
         ]
 }
 
