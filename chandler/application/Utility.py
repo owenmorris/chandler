65993dc38635eb8856fde33a2d7b934c15abd4e2
grant <grant@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-06-05 00:51:48 +0000

    Forgot to bump the schema version with my last checkin...
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14555 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -34,7 +34,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "403" # vajda: Demo menu moved back to menubar
+SCHEMA_VERSION = "404" # grant: Recurrence fixes (bug 7446 et al)
 
 logger = None # initialized in initLogging()
 
