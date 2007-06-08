70068f581408c0f6f3719b6f5c0309b6101a756e
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-06-08 17:30:58 +0000

    Tweaking how version.py works to fix a singlebuild problem
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14633 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/version.py
+++ chandler/version.py
@@ -4,54 +4,63 @@ _version = { 'release':    '0.7',
              'revision':   None,
            }
 
-_template = '%(release)s'
+#!#!#!#!# <-- used by build_lib.py to mark end of _version def
+# Note: do not edit or change anything above this comment block
+#       as it can/will be replaced during the build process
+#
+#     continuous build        build = '.dev'
+#                             checkpoint = None
+#                             revision set by caller
+#
+#     checkpoint build        build = '.dev'
+#                             checkpoint value set to YYYYMMDD
+#                             revision set by caller
+#
+#     Milestone or Release    build = ''
+#                             checkpoint = None
+#                             revision = None
 
-  # the checkpoint and release information is added
-  # to the version string *only* if we are doing one of:
-  #     continuous build        build = '.dev'
-  #                             checkpoint = None
-  #     checkpoint build        build = '.dev'
-  #                             checkpoint value set to YYYYMMDD
-  #     Milestone or Release    build = ''
-  #                             checkpoint = None
-  #                             revision = None
+_template = '%(release)s'
 
 if len(_version['build']) > 0:
     _template += '%(build)s'
 
-    import os
+    # dig into the file system to figure out what the revision number is
+    # but only if the build system hasn't provided one
+    if _version['revision'] is None:
+        import os
 
-    try:
-        chandlerDir = os.path.dirname(__file__)
-    except:
-        chandlerDir = '.'
+        try:
+            chandlerDir = os.path.dirname(__file__)
+        except:
+            chandlerDir = '.'
 
-        # pull the .svn/entries file from the directory where version.py resides
-        # and read the value for the revision property
+            # pull the .svn/entries file from the directory where version.py resides
+            # and read the value for the revision property
 
-    svnfile = os.path.join(chandlerDir, '.svn', 'entries')
+        svnfile = os.path.join(chandlerDir, '.svn', 'entries')
 
-    if os.path.isfile(svnfile):
-        # svn 1.3
-        for line in file(svnfile):
-            items = line.split('=')
-
-            if len(items) == 2:
-                item, value = items
-
-                if item.strip().lower() == 'revision':
-                    _version['revision'] = value[:-1].strip('">/')
-        
-        # svn 1.4   
-        if _version['revision'] == None:
-            revisions = []
+        if os.path.isfile(svnfile):
+            # svn 1.3
             for line in file(svnfile):
-                try:
-                    revisions.append(long(line))
-                except ValueError:
-                    pass
-            revisions.sort()
-            _version['revision'] = str(revisions[-1])
+                items = line.split('=')
+
+                if len(items) == 2:
+                    item, value = items
+
+                    if item.strip().lower() == 'revision':
+                        _version['revision'] = value[:-1].strip('">/')
+
+            # svn 1.4
+            if _version['revision'] == None:
+                revisions = []
+                for line in file(svnfile):
+                    try:
+                        revisions.append(long(line))
+                    except ValueError:
+                        pass
+                revisions.sort()
+                _version['revision'] = str(revisions[-1])
 
     if _version['revision'] is not None:
         _template += '-r%(revision)s'
@@ -64,5 +73,4 @@ build      = '%s' % _version['build']
 checkpoint = '%s' % _version['checkpoint']
 revision   = '%s' % _version['revision']
 version    = _template % _version
-
 
