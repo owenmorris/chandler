6f77494e6c776eb25cafcee01b60463de279410d
morgen <morgen@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-06-11 18:18:51 +0000

    My last commit needs a schema bump
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14648 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -34,7 +34,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "411" # vajda: changed ImportICalendar event to not commit.
+SCHEMA_VERSION = "412" # morgen: translator change
 
 logger = None # initialized in initLogging()
 
