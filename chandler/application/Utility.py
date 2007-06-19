4a7f2c589005f27b463298b43dcb87e0f39840ca
john <john@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-06-19 23:31:42 +0000

    show search results in dashboard view
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14757 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -35,7 +35,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "419" # morgen: added removal conflict
+SCHEMA_VERSION = "420" # john: show search results in dashboard
 
 logger = None # initialized in initLogging()
 
