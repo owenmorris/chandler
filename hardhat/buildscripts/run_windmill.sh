da2d22382265e821adacad2a6f3c6bb2e3662f82
bear <bear@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-07-05 18:50:36 +0000

    oops - point to the real config file location
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14911 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- hardhat/buildscripts/run_windmill.sh
+++ hardhat/buildscripts/run_windmill.sh
@@ -1,5 +1,5 @@
 cd ~/rel_0.1.2/windmill
 
-export WINDMILL_CONFIG_FILE=d:\\cygwin\\home\\builder\\hardhat\\windmill_settings.py
+export WINDMILL_CONFIG_FILE=d:\\cygwin\\home\\builder\\hardhat\\buildscripts\\windmill_settings.py
 
 /cygdrive/d/Python25/python.exe bin/windmill-admin.py tbox firefox 
