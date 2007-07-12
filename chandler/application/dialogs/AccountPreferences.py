8bc0853dfd62493f854e85b2b163bd3f50c3e78e
pbossut <pbossut@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-07-12 19:51:26 +0000

    Fix bug 9518: Actually points the Account Dialog to the "guide" instead of the "faq"
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@14999 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/dialogs/AccountPreferences.py
+++ chandler/application/dialogs/AccountPreferences.py
@@ -71,7 +71,7 @@ REMOVE_FOLDERS = _(u"Chandler will now attempt to remove the\nfollowing IMAP fol
 # Will print out saved account changes
 # before exiting the dialog when set to True
 DEBUG = False
-FOLDERS_URL = "http://chandlerproject.org/faq"
+FOLDERS_URL = "http://chandlerproject.org/guide"
 SHARING_URL = "http://hub.chandlerproject.org/signup"
 
 # Special handlers referenced in the panelsInfo dictionary below:
