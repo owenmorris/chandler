cbeed1f09765b70293338d89948f05232aa95c48
stearns <stearns@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-07-18 02:24:16 +0000

    Oops: r15046 should've contained a schema bump (thanks, gbaillie!).
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@15053 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -35,7 +35,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "444" # morgen: new inbound shared items collection
+SCHEMA_VERSION = "445" # stearns: transparency block change
 
 logger = None # initialized in initLogging()
 
