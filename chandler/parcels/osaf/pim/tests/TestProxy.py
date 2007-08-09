f66c7125f30f1e2c30f3a0ac87acfb7563e930e9
bkirsch <bkirsch@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-08-09 21:36:50 +0000

    fixes failing code related to bug 10381
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@15211 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/parcels/osaf/pim/tests/TestProxy.py
+++ chandler/parcels/osaf/pim/tests/TestProxy.py
@@ -26,17 +26,17 @@ def additional_tests():
         'proxy.txt',
         optionflags=doctest.ELLIPSIS, package='osaf.pim',
     )
-    
+
 class ProxyTestCase(SingleRepositoryTestCase):
     sandbox = None
-    
+
     def makeRuleset(self, freq):
         rrule = pim.calendar.Recurrence.RecurrenceRule(itsParent=self.sandbox,
                                                        freq=freq)
         return pim.calendar.Recurrence.RecurrenceRuleSet(
                                 itsParent=self.sandbox, rrules=[rrule])
 
-    
+
     def setUp(self):
         if self.sandbox is None:
             super(ProxyTestCase, self).setUp()
@@ -63,7 +63,7 @@ class ProxyTestCase(SingleRepositoryTestCase):
     def tearDown(self):
         for child in self.sandbox.iterChildren():
             child.delete(recursive=True)
-            
+
 class ProxyChangeTestCase(ProxyTestCase):
 
     def testAdd_THIS(self):
@@ -73,38 +73,38 @@ class ProxyChangeTestCase(ProxyTestCase):
         self.event.rruleset = self.rruleset
         second = self.event.getFirstOccurrence().getNextOccurrence()
         proxiedSecond = pim.CHANGE_THIS(second)
-        
+
         if __debug__:
             self.failUnlessRaises(AssertionError,
                                   proxiedSecond.itsItem.collections.remove, 
                                   self.two)
         else:
             proxiedSecond.itsItem.collections.remove(self.two)
-            
+
         self.failUnless(second.itsItem in self.two)
         self.failUnless(self.event.itsItem in self.two)
         for modItem in self.event.modifications:
             self.failUnless(modItem in self.two)
 
-            
+
     def testRemove_ALL(self):
         self.event.itsItem.collections=[self.one, self.two]
         self.event.rruleset = self.rruleset
-        
+
         firstProxy = pim.CHANGE_ALL(self.event.getFirstOccurrence())
         firstProxy.itsItem.collections.remove(self.one)
-                
+
         self.failIf(self.event.itsItem in self.one)
         self.failUnless(self.event.itsItem in self.two)
 
     def testRemove_THISANDFUTURE(self):
         self.event.itsItem.collections=[self.one, self.two]
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
         secondProxy = pim.CHANGE_FUTURE(second)
         secondProxy.itsItem.collections.remove(self.two)
-                
+
         self.failUnless(self.event.itsItem in self.one)
         self.failUnless(self.event.itsItem in self.two)
         self.failIfEqual(self.event.getMaster(), second.getMaster())
@@ -118,16 +118,16 @@ class ProxyChangeTestCase(ProxyTestCase):
         self.event.rruleset = self.rruleset
         second = self.event.getFirstOccurrence().getNextOccurrence()
         proxiedSecond = pim.CHANGE_THIS(second)
-        
+
         proxiedSecond.itsItem.collections.add(self.two)
-            
+
         self.failUnless(second.itsItem in self.two)
         self.failIf(self.event.itsItem in self.two)
         for modItem in self.event.modifications:
             if modItem is not second.itsItem:
                 self.failIf(modItem in self.two)
-        
-        
+
+
     def testAdd_ALL(self):
         self.event.itsItem.collections=[self.one]
         self.event.rruleset = self.rruleset
@@ -136,18 +136,18 @@ class ProxyChangeTestCase(ProxyTestCase):
         pim.CHANGE_THIS(second).summary = u'New'
         secondProxy = pim.CHANGE_ALL(second)
         secondProxy.itsItem.collections.add(self.two)
-        
+
         self.failUnless(self.event.itsItem in self.two)
         self.failUnless(self.event.modifications, "No auto-generated occurrences?")
         for modItem in self.event.modifications:
             self.failUnless(modItem in self.two)
-        
+
     def test_THIS(self):
         self.event.rruleset = self.rruleset
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
-        
+
         pim.CHANGE_THIS(third).allDay = True
-        
+
         self.failUnless(third.modificationFor is not None)
         self.failUnless(third.allDay)
         self.failIf(self.event.allDay)
@@ -155,9 +155,9 @@ class ProxyChangeTestCase(ProxyTestCase):
     def testStamp_THIS(self):
         self.event.rruleset = self.rruleset
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
-        
+
         pim.TaskStamp(pim.CHANGE_THIS(third)).add()
-        
+
         self.failUnless(third.modificationFor is not None)
         self.failUnless(pim.has_stamp(third, pim.TaskStamp))
         self.failIf(pim.has_stamp(self.event, pim.TaskStamp))
@@ -168,9 +168,9 @@ class ProxyChangeTestCase(ProxyTestCase):
         pim.TaskStamp(self.event).add()
         self.event.rruleset = self.rruleset
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
-        
+
         pim.TaskStamp(pim.CHANGE_THIS(third)).remove()
-        
+
         self.failUnless(third.modificationFor is not None)
         self.failIf(pim.has_stamp(third, pim.TaskStamp))
         self.failUnless(pim.has_stamp(self.event, pim.TaskStamp))
@@ -180,9 +180,9 @@ class ProxyChangeTestCase(ProxyTestCase):
     def testStamp_THISANDFUTURE(self):
         self.event.rruleset = self.rruleset
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
-        
+
         pim.TaskStamp(pim.CHANGE_FUTURE(third)).add()
-        
+
         self.failIfEqual(third.getMaster(), self.event.getMaster())
         self.failUnless(pim.has_stamp(third, pim.TaskStamp))
         self.failUnless(pim.has_stamp(third.getNextOccurrence(), pim.TaskStamp))
@@ -194,9 +194,9 @@ class ProxyChangeTestCase(ProxyTestCase):
         pim.TaskStamp(self.event).add()
         self.event.rruleset = self.rruleset
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
-        
+
         pim.TaskStamp(pim.CHANGE_FUTURE(third)).remove()
-        
+
         self.failIfEqual(third.getMaster(), self.event.getMaster())
         self.failIf(pim.has_stamp(third, pim.TaskStamp))
         self.failIf(pim.has_stamp(third.getNextOccurrence(), pim.TaskStamp))
@@ -209,7 +209,7 @@ class ProxyChangeTestCase(ProxyTestCase):
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
 
         pim.CHANGE_ALL(third).remove()
-        
+
         self.failUnless(pim.isDead(third.itsItem))
         self.failIf(pim.isDead(self.event.itsItem))
         self.failIf(pim.has_stamp(self.event, pim.EventStamp))
@@ -220,7 +220,7 @@ class ProxyChangeTestCase(ProxyTestCase):
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
 
         pim.CHANGE_ALL(third).rruleset = self.makeRuleset('weekly')
-        
+
         self.failUnless(pim.isDead(third.itsItem))
         self.failIf(pim.isDead(self.event.itsItem))
         self.failUnlessEqual(self.event.rruleset.rrules.first().freq, 'weekly')
@@ -231,7 +231,7 @@ class ProxyChangeTestCase(ProxyTestCase):
         third = self.event.getFirstOccurrence().getNextOccurrence().getNextOccurrence()
 
         pim.CHANGE_FUTURE(third).rruleset = self.makeRuleset('weekly')
-        
+
         self.failIfEqual(third.getMaster(), self.event.getMaster())
         self.failIf(pim.isDead(self.event.itsItem))
         self.failIf(pim.isDead(third.itsItem))
@@ -245,12 +245,12 @@ class ProxyChangeTestCase(ProxyTestCase):
         third = first.getNextOccurrence().getNextOccurrence()
 
         del pim.CHANGE_ALL(third).rruleset
-        
+
         self.failUnless(pim.isDead(first.itsItem))
         self.failUnless(pim.isDead(third.itsItem))
         self.failIf(pim.isDead(self.event.itsItem))
         self.failUnless(self.event.rruleset is None)
-        
+
 class ProxyEditStateTestCase(ProxyTestCase):
     def setUp(self):
         view = super(ProxyEditStateTestCase, self).setUp()
@@ -258,10 +258,10 @@ class ProxyEditStateTestCase(ProxyTestCase):
                       datetime.timedelta(minutes=10))
         self.event.itsItem.changeEditState(pim.Modification.created,
                                            when=self.start)
-        
+
     def testNoRecurrence_THIS(self):
         pim.CHANGE_THIS(self.event.itsItem).changeEditState()
-        
+
         self.failUnlessEqual(self.event.itsItem.lastModification,
                              pim.Modification.edited)
         self.failUnless(self.event.itsItem.lastModified > self.start)
@@ -269,42 +269,42 @@ class ProxyEditStateTestCase(ProxyTestCase):
 
     def testNoRecurrence_FUTURE(self):
         pim.CHANGE_FUTURE(self.event.itsItem).changeEditState()
-        
+
         self.failUnlessEqual(self.event.itsItem.lastModification,
                              pim.Modification.edited)
         self.failUnless(self.event.itsItem.lastModified > self.start)
 
     def testNoRecurrence_ALL(self):
         pim.CHANGE_ALL(self.event.itsItem).changeEditState()
-        
+
         self.failUnlessEqual(self.event.itsItem.lastModification,
                              pim.Modification.edited)
         self.failUnless(self.event.itsItem.lastModified > self.start)
-        
+
     def testRecurrence(self):
         self.event.rruleset = self.rruleset
-        
+
         # This assumes that assigning rruleset auto-creates occurrences
         self.failUnless(self.event.occurrences)
-        
+
         for occurrence in self.event.occurrences:
             self.failIf(occurrence.hasLocalAttributeValue('lastModified'))
             self.failIf(occurrence.hasLocalAttributeValue('lastModification'))
-            
+
     def testChange_THIS(self):
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
-        
+
         pim.CHANGE_THIS(second).summary = u'I am so special'
         self.failUnlessEqual(self.event.itsItem.lastModified, self.start)
         self.failUnless(second.itsItem.lastModified > self.start)
 
     def testChange_FUTURE(self):
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
-        
+
         pim.CHANGE_FUTURE(second).startTime = second.startTime + datetime.timedelta(hours=1)
         self.failUnlessEqual(self.event.itsItem.lastModified, self.start)
         self.failUnless(second.itsItem.lastModified > self.start)
@@ -312,9 +312,9 @@ class ProxyEditStateTestCase(ProxyTestCase):
 
     def testChange_ALL(self):
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
-        
+
         pim.CHANGE_ALL(second).startTime = second.startTime + datetime.timedelta(hours=1)
         self.failUnless(self.event.itsItem.lastModified > self.start)
         self.failUnlessEqual(second.getNextOccurrence().itsItem.lastModified,
@@ -326,20 +326,20 @@ class ProxyEditStateTestCase(ProxyTestCase):
         # change of a different attribute to a different occurrence, and make
         # sure the lastModified is updated accordingly.
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
         third = second.getNextOccurrence()
-        
+
         # THIS change to summary (a.k.a. displayName) ...
         pim.CHANGE_THIS(third).summary = u'This has been changed'
         # Make sure the lastModified changed for this event (third) ...
         self.failUnless(third.itsItem.lastModified > self.start)
         # ... but not for second, a different event in the series
         self.failUnlessEqual(second.itsItem.lastModified, self.start)
-        
+
         # Now make an ALL change on duration
         pim.CHANGE_ALL(second).duration = datetime.timedelta(hours=4)
-        
+
         # self.event (the master) should have a changed lastModified
         self.failIfEqual(self.event.itsItem.lastModified, self.start)
         # ... which is the same as for second
@@ -353,16 +353,16 @@ class ProxyEditStateTestCase(ProxyTestCase):
 
     def testChangeOverlapping_THIS_ALL(self):
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
         third = second.getNextOccurrence()
-        
+
         pim.CHANGE_THIS(third).duration = datetime.timedelta(hours=2)
         self.failUnless(third.itsItem.lastModified > self.start)
-        
+
         saveLastModified = third.itsItem.lastModified
         time.sleep(0.1) # Make sure some time elapses for lastModified
-        
+
         pim.CHANGE_ALL(second).duration = datetime.timedelta(hours=4)
         # However, third wasn't altered by the last change, so its
         # lastModified should be unchanged.
@@ -373,26 +373,26 @@ class ProxyEditStateTestCase(ProxyTestCase):
         # change of a different attribute to a different occurrence, and make
         # sure the lastModified is updated accordingly.
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
         third = second.getNextOccurrence()
-        
+
         # THIS change to summary (a.k.a. displayName) ...
         pim.CHANGE_THIS(third).summary = u'This has been changed'
         # Make sure the lastModified changed for this event (third) ...
         self.failUnless(third.itsItem.lastModified > self.start)
         # ... but not for second, a different event in the series
         self.failUnlessEqual(second.itsItem.lastModified, self.start)
-        
+
         # Remember third's lastModified, so we can check that it changed
         # later.
         saveLastModified = third.itsItem.lastModified
-        
+
         time.sleep(0.1) # Make sure some time elapses for lastModified
-        
+
         # Now make an FUTURE change on duration
         pim.CHANGE_FUTURE(second).duration = datetime.timedelta(hours=4)
-        
+
         # self.event (the master) should have an unchanged lastModified
         self.failUnlessEqual(self.event.itsItem.lastModified, self.start)
         # second is now part of a new series, and so should have a new
@@ -408,13 +408,13 @@ class ProxyEditStateTestCase(ProxyTestCase):
 
     def testChangeOverlapping_THIS_FUTURE(self):
         self.event.rruleset = self.rruleset
-        
+
         second = self.event.getFirstOccurrence().getNextOccurrence()
         third = second.getNextOccurrence()
-        
+
         pim.CHANGE_THIS(third).duration = datetime.timedelta(hours=2)
         self.failUnless(third.itsItem.lastModified > self.start)
-        
+
         saveLastModified = third.itsItem.lastModified
 
         time.sleep(0.1) # Make sure some time elapses for lastModified        
@@ -422,12 +422,12 @@ class ProxyEditStateTestCase(ProxyTestCase):
         # However, third wasn't altered by the last change, so its
         # lastModified should be unchanged.
         self.failUnlessEqual(saveLastModified, third.itsItem.lastModified)
-        
+
     def testChangeCC(self):
 
 #        email = pim.mail.EmailAddress(itsParent=self.sandbox, fullName=u'Tommy Totoro',
 #                             emailAddress=u'totoro@example.com')
-                             
+
         mei = pim.mail.EmailAddress(itsParent=self.sandbox, fullName=u'Mei Kusakabe',
                           emailAddress=u'mei@totoro.example.com')
 
@@ -442,26 +442,26 @@ class ProxyEditStateTestCase(ProxyTestCase):
             "Content-Type: text/plain; charset='US-ASCII'"
             "\r\n"
             "I think it's raining!\r\n"
-        )
+        )[1]
         mail.itsItem.lastModification = pim.Modification.created
-        
+
         pim.mail.getCurrentMeEmailAddresses(mei.itsView).add(mei)
 
         event = pim.EventStamp(mail)
         event.add()
         event.rruleset = self.rruleset
-        
+
         pim.CHANGE_THIS(mail.itsItem).displayName = u"You bet I do!"
 
         firstItem = event.getFirstOccurrence().itsItem
-        
+
         self.failIf(firstItem.hasLocalAttributeValue(
                         pim.mail.MailStamp.ccAddress.name)
         )
-        
+
         self.failUnless(mei is mail.fromAddress)
         self.failUnless(catbus in mail.ccAddress)
-            
+
 if __name__ == "__main__":
     from util.test_finder import ScanningLoader
 
