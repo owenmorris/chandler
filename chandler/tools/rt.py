f4791b51369573705f89912aa2d6c5a412bd77cd
heikki <heikki@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2007-11-12 20:28:21 +0000

    Bug 11302, runLocalizationCheck does not follow rt.py conventions, r=bear,bkirsch.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@15793 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/rt.py
+++ chandler/tools/rt.py
@@ -520,18 +520,36 @@ def runTestCase(options):
 
 
 def runLocalizationCheck(options):
+    """
+    Run localization check.
+    
+    >>> options = parseOptions()
+    >>> checkOptions(options)
+    >>> options.dryrun  = True
+    >>> options.verbose = True
+    >>> options.modes   = ['release']
+    
+    >>> runLocalizationCheck(options)
+    /.../RunPython tools/l10nValidator.py
+    False
+    """
+    cmd  = options.runpython[options.modes[0]][:]
+    cmd += [os.path.join('tools', 'l10nValidator.py')]
+
+    if options.verbose:
+        log(' '.join(cmd))
+
     if options.dryrun:
-        failed = False
+        result = 0
     else:
-        cmd = ['python', os.path.join('tools', 'l10nValidator.py')]
-
         result = build_lib.runCommand(cmd, timeout=180)
-        failed = result != 0
+    
+    failed = result != 0
 
-        if failed:
-            log('Localization Check FAILED (%d)' % result)
-            failedTests.append('Localization Check')
-            log('- + ' * 15)
+    if failed:
+        log('***Error Localization Check FAILED with exit code=%d' % result)
+        failedTests.append('l10nValidator.py')
+        log('- + ' * 15)
 
     return failed
 
@@ -1534,6 +1552,7 @@ def main(options):
     >>> options.dryrun  = True
     >>> options.verbose = True
     >>> main(options)
+    /.../RunPython tools/l10nValidator.py
     False
     
     Try and run a test that does not exist
