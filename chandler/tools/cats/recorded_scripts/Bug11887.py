95bdfcdf22b69e214ae06dc2f4bc7581e3b25bf2
grant <grant@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2008-03-06 01:48:37 +0000

    Oops, I left out one file in my previous commit.
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@16277 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/tools/cats/recorded_scripts/Bug11887.py
+++ chandler/tools/cats/recorded_scripts/Bug11887.py
@@ -3,18 +3,23 @@ def run():
     """
         Regression test for Bug 11887 (Exception changing visible hours in multiweek (month) view)
         
-        Choose toolbar button 'Calendar' (0)
-        Left Mouse Down in genstattext (2)
-        Choose menu 'View > Visible Hours > 12 hours' (5)
+        Left Mouse Down in SidebarGridWindow (0)
+        Choose toolbar button 'Calendar' (4)
+        Left Mouse Down in genstattext (6)
+        Choose menu 'View > Visible Hours > 12 hours' (9)
     """
 
     wx.GetApp().RunRecordedScript ([
-        (0, wx.CommandEvent, {'associatedBlock':'ApplicationBarEventButton', 'eventType':wx.EVT_MENU, 'sentTo':u'ApplicationBar'}, {}),
-        (1, wx.FocusEvent, {'eventType':wx.EVT_SET_FOCUS, 'sentTo':u'TimedEventsFocusWindow'}, {}),
-        (2, wx.MouseEvent, {'eventType':wx.EVT_LEFT_DOWN, 'sentTo':u'genstattext'}, {'m_leftDown':True, 'm_x':52, 'm_y':11}),
-        (3, wx.FocusEvent, {'eventType':wx.EVT_SET_FOCUS, 'sentTo':u'MultiWeekCanvasFocusWindow'}, {}),
-        (4, wx.MouseEvent, {'eventType':wx.EVT_LEFT_UP, 'sentTo':u'genstattext'}, {'m_x':52, 'm_y':11}),
-        (5, wx.CommandEvent, {'associatedBlock':'VisibleHour12Item', 'eventType':wx.EVT_MENU, 'sentTo':'__block__VisibleHoursMenu'}, {}),
+        (0, wx.MouseEvent, {'eventType':wx.EVT_LEFT_DOWN, 'sentTo':u'SidebarGridWindow'}, {'m_leftDown':True, 'm_x':72, 'm_y':99}),
+        (1, wx.FocusEvent, {'eventType':wx.EVT_SET_FOCUS, 'sentTo':u'SidebarGridWindow'}, {}),
+        (2, wx.MouseEvent, {'eventType':wx.EVT_LEFT_UP, 'sentTo':u'SidebarGridWindow'}, {'m_x':72, 'm_y':99}),
+        (3, wx.FocusEvent, {'eventType':wx.EVT_SET_FOCUS, 'sentTo':u'DashboardSummaryViewGridWindow'}, {}),
+        (4, wx.CommandEvent, {'associatedBlock':'ApplicationBarEventButton', 'eventType':wx.EVT_MENU, 'sentTo':u'ApplicationBar'}, {}),
+        (5, wx.FocusEvent, {'eventType':wx.EVT_SET_FOCUS, 'sentTo':u'TimedEventsFocusWindow'}, {}),
+        (6, wx.MouseEvent, {'eventType':wx.EVT_LEFT_DOWN, 'sentTo':u'genstattext'}, {'m_leftDown':True, 'm_x':52, 'm_y':11}),
+        (7, wx.FocusEvent, {'eventType':wx.EVT_SET_FOCUS, 'sentTo':u'MultiWeekCanvasFocusWindow'}, {}),
+        (8, wx.MouseEvent, {'eventType':wx.EVT_LEFT_UP, 'sentTo':u'genstattext'}, {'m_x':52, 'm_y':11}),
+        (9, wx.CommandEvent, {'associatedBlock':'VisibleHour12Item', 'eventType':wx.EVT_MENU, 'sentTo':'__block__VisibleHoursMenu'}, {}),
     ])
     
     # Without the following, the framework seems happy to pass the
