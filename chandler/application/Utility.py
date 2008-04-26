560ad717741e003c2cbd8dfa094a215eb63d97da
capps <capps@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2008-04-26 03:31:33 +0000

    Menu fixes: Bug 11738 (import/export ics) and remove ... from 'Ask for Help'
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@16368 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/application/Utility.py
+++ chandler/application/Utility.py
@@ -34,7 +34,7 @@ import version
 # with your name (and some helpful text). The comment's really there just to
 # cause Subversion to warn you of a conflict when you update, in case someone 
 # else changes it at the same time you do (that's why it's on the same line).
-SCHEMA_VERSION = "486" # grant: Bug 11575 ("Ask for Help" menu item)
+SCHEMA_VERSION = "487" # capps: Bug 11738 (tweaking import/export menu text)
 
 logger = None # initialized in initLogging()
 
