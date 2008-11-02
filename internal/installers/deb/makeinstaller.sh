f096001ee9f3e6c66da873456fa635e2f11569eb
jared <jared@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2008-11-02 23:55:14 +0000

    Unindent main execution block in deb makeinstall.sh
    Just move the exception condition (DEB_PATH doesn't exist) to
    immediately exit instead of making the normal case be the "else"
    condition (thus requiring indenting most of the script).
    
    
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@16601 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- internal/installers/deb/makeinstaller.sh
+++ internal/installers/deb/makeinstaller.sh
@@ -26,42 +26,46 @@ else
   DISTRIB_MODE="_${DISTRIB_MODE}_"
 fi
 
-if [ -d "$DEB_PATH" ]; then
-    echo "Clearing debian chandler working image"
-    if [ -d "{$DEB_PATH}/chandler" ]; then
-        rm -rf ${DEB_PATH}/chandler
-    fi
-    rm -f ${DEB_PATH}/chandler_*.deb
-    echo "Preparing build tree"
-    cd ${DEB_PATH}
-    if [ -d chandler ]; then
-        rm -rf chandler
-    fi
+if [ ! -d "$DEB_PATH" ]; then
+    echo "ERROR: debian local environment does not seem to be setup"
+    exit 1
+fi
 
-    mkdir -p chandler
-    PKG_ROOT=$(cd chandler && pwd)
-    
-    echo "Creating man page"
-    mkdir -p "$PKG_ROOT"/usr/share/man/man1
-    cat ${DISTRIB_PATH}/chandler/distrib/linux/chandler.1 | sed "s/CHANDLER_VERSION/${DISTRIB_VERSION}-${DISTRIB_RELEASE}/" > chandler/usr/share/man/man1/chandler.1
-    gzip -9 chandler/usr/share/man/man1/chandler.1
-    
-    mkdir -p chandler/usr/lib
-    mkdir chandler/DEBIAN/
-    cd chandler/usr/lib
-    
-    echo "Creating build tree from distribution files"
-    if [ -f ${DISTRIB_PATH}/${DISTRIB_FILE}.tar.gz ]; then
-        tar xzf ${DISTRIB_PATH}/${DISTRIB_FILE}.tar.gz
-        mv ${DISTRIB_FILE} chandler
-    else
-        cp -a ${DISTRIB_PATH}/${DISTRIB_FILE} chandler
-    fi
+echo "Clearing debian chandler working image"
+if [ -d "{$DEB_PATH}/chandler" ]; then
+    rm -rf ${DEB_PATH}/chandler
+fi
+rm -f ${DEB_PATH}/chandler_*.deb
+echo "Preparing build tree"
+cd ${DEB_PATH}
+if [ -d chandler ]; then
+    rm -rf chandler
+fi
 
-    echo "Creating copyright/license file"
-    mkdir -p "$PKG_ROOT"/usr/share/doc/chandler
-    COPYRIGHT_PATH="$PKG_ROOT"/usr/share/doc/chandler/copyright
-    echo > "$COPYRIGHT_PATH" <<EOF
+mkdir -p chandler
+PKG_ROOT=$(cd chandler && pwd)
+
+echo "Creating man page"
+mkdir -p "$PKG_ROOT"/usr/share/man/man1
+cat ${DISTRIB_PATH}/chandler/distrib/linux/chandler.1 | sed "s/CHANDLER_VERSION/${DISTRIB_VERSION}-${DISTRIB_RELEASE}/" > chandler/usr/share/man/man1/chandler.1
+gzip -9 chandler/usr/share/man/man1/chandler.1
+
+mkdir -p chandler/usr/lib
+mkdir chandler/DEBIAN/
+cd chandler/usr/lib
+
+echo "Creating build tree from distribution files"
+if [ -f ${DISTRIB_PATH}/${DISTRIB_FILE}.tar.gz ]; then
+    tar xzf ${DISTRIB_PATH}/${DISTRIB_FILE}.tar.gz
+    mv ${DISTRIB_FILE} chandler
+else
+    cp -a ${DISTRIB_PATH}/${DISTRIB_FILE} chandler
+fi
+
+echo "Creating copyright/license file"
+mkdir -p "$PKG_ROOT"/usr/share/doc/chandler
+COPYRIGHT_PATH="$PKG_ROOT"/usr/share/doc/chandler/copyright
+echo > "$COPYRIGHT_PATH" <<EOF
 Copyright (c) 2003-2008 Open Source Applications Foundation
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -77,65 +81,61 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 EOF
-    cat chandler/LICENSE.txt chandler/NOTICE.txt >> "$COPYRIGHT_PATH"
-    rm chandler/LICENSE.txt chandler/NOTICE.txt
+cat chandler/LICENSE.txt chandler/NOTICE.txt >> "$COPYRIGHT_PATH"
+rm chandler/LICENSE.txt chandler/NOTICE.txt
 
-    echo "Writing changelog"
-    cat ${DISTRIB_PATH}/chandler/distrib/linux/changelog | 
-       gzip -c -9 > "$PKG_ROOT"/usr/share/doc/chandler/changelog.Debian.gz
+echo "Writing changelog"
+cat ${DISTRIB_PATH}/chandler/distrib/linux/changelog | 
+   gzip -c -9 > "$PKG_ROOT"/usr/share/doc/chandler/changelog.Debian.gz
 
-    echo "Creating /usr/bin/chandler symlink"
-    mkdir -p "$PKG_ROOT"/usr/bin
-    (cd "$PKG_ROOT"/usr/bin && ln -s ../lib/chandler/chandler)
+echo "Creating /usr/bin/chandler symlink"
+mkdir -p "$PKG_ROOT"/usr/bin
+(cd "$PKG_ROOT"/usr/bin && ln -s ../lib/chandler/chandler)
 
-    echo "Removing test code"
-    find chandler -type d -name 'test*' -prune -exec rm -rf '{}' \;
+echo "Removing test code"
+find chandler -type d -name 'test*' -prune -exec rm -rf '{}' \;
 
-    echo "Correcting permissions"
-    for file in chandler/release/db/bin/*; do
-        chmod u+w "$file"
-        strip "$file"
-    done
-    find chandler/release/site-packages/lucene-* \
-         chandler/release/site-packages/wx/tools/Editra \
-             -type f -exec chmod -x {} \;
+echo "Correcting permissions"
+for file in chandler/release/db/bin/*; do
+    chmod u+w "$file"
+    strip "$file"
+done
+find chandler/release/site-packages/lucene-* \
+     chandler/release/site-packages/wx/tools/Editra \
+         -type f -exec chmod -x {} \;
 
-    echo "Stripping executables and libraries"
-    find chandler -type f -name '*.so' -print -exec strip {} \; -exec chmod -x {} \;
-    find chandler -type f -name 'lib*.so.*' -print -exec strip {} \; -exec chmod -x {} \;
-    find chandler/release/j2re-image/bin chandler/release/j2re-image/lib/jexec -type f -exec strip {} \; -print
+echo "Stripping executables and libraries"
+find chandler -type f -name '*.so' -print -exec strip {} \; -exec chmod -x {} \;
+find chandler -type f -name 'lib*.so.*' -print -exec strip {} \; -exec chmod -x {} \;
+find chandler/release/j2re-image/bin chandler/release/j2re-image/lib/jexec -type f -exec strip {} \; -print
 
-    echo "Copying shared icons"
-    mkdir -p "$PKG_ROOT"/usr/share/icons/hicolor/scalable/apps
-    mv -v chandler/Chandler.egg-info/resources/images/Logos/Superdoge.svg \
-          "$PKG_ROOT"/usr/share/icons/hicolor/scalable/apps/chandler.svg
-    mkdir -p "$PKG_ROOT"/usr/share/icons/hicolor/64x64/apps
-    mv -v chandler/Chandler.egg-info/resources/images/Logos/Chandler_64.png \
-          "$PKG_ROOT"/usr/share/icons/hicolor/64x64/apps/chandler.png
-    rm -rf chandler/Chandler.egg-info/resources/images/Logos
+echo "Copying shared icons"
+mkdir -p "$PKG_ROOT"/usr/share/icons/hicolor/scalable/apps
+mv -v chandler/Chandler.egg-info/resources/images/Logos/Superdoge.svg \
+      "$PKG_ROOT"/usr/share/icons/hicolor/scalable/apps/chandler.svg
+mkdir -p "$PKG_ROOT"/usr/share/icons/hicolor/64x64/apps
+mv -v chandler/Chandler.egg-info/resources/images/Logos/Chandler_64.png \
+      "$PKG_ROOT"/usr/share/icons/hicolor/64x64/apps/chandler.png
+rm -rf chandler/Chandler.egg-info/resources/images/Logos
 
-    echo "Ensuring all files have a+r set"
-    chmod -R a+r chandler/
+echo "Ensuring all files have a+r set"
+chmod -R a+r chandler/
 
-    echo "Making all files owner writable"
-    find chandler -type f -perm 0444 -exec chmod u+w {} \;
+echo "Making all files owner writable"
+find chandler -type f -perm 0444 -exec chmod u+w {} \;
 
-    cd ${DEB_PATH}
-    CHANDLER_SIZE=`du -c -b ${DEB_PATH}/chandler/usr/lib/chandler | tail -n1 | awk '{ print $1 }'`
-    CHANDLER_ARCH=$(eval $(dpkg-architecture); echo $DEB_HOST_ARCH)
-    sed -e "s/CHANDLER_VERSION/${DISTRIB_VERSION}-${DISTRIB_RELEASE}/" \
-        -e "s/CHANDLER_SIZE/${CHANDLER_SIZE}/" \
-        -e "s/CHANDLER_ARCH/${CHANDLER_ARCH}/" \
+cd ${DEB_PATH}
+CHANDLER_SIZE=`du -c -b ${DEB_PATH}/chandler/usr/lib/chandler | tail -n1 | awk '{ print $1 }'`
+CHANDLER_ARCH=$(eval $(dpkg-architecture); echo $DEB_HOST_ARCH)
+sed -e "s/CHANDLER_VERSION/${DISTRIB_VERSION}-${DISTRIB_RELEASE}/" \
+    -e "s/CHANDLER_SIZE/${CHANDLER_SIZE}/" \
+    -e "s/CHANDLER_ARCH/${CHANDLER_ARCH}/" \
 	-e "s/LIBICU/${LIBICU}/" \
-            < ${DEB_PATH}/control.in > ${DEB_PATH}/chandler/DEBIAN/control
-    echo `pwd`
+        < ${DEB_PATH}/control.in > ${DEB_PATH}/chandler/DEBIAN/control
+echo `pwd`
 
-    echo "Calling fakeroot dpkg-deb -b chandler chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb"
-    fakeroot -- dpkg-deb -b chandler chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb
-    echo "Moving ${DEB_PATH}/chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb to ${DISTRIB_PATH}/Chandler_linux${DISTRIB_MODE}${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb"
-    mv ${DEB_PATH}/chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb ${DISTRIB_PATH}/Chandler_linux${DISTRIB_MODE}${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb
-    echo "deb generation done"
-else
-    echo "ERROR: debian local environment does not seem to be setup"
-    exit 1
-fi
+echo "Calling fakeroot dpkg-deb -b chandler chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb"
+fakeroot -- dpkg-deb -b chandler chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb
+echo "Moving ${DEB_PATH}/chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb to ${DISTRIB_PATH}/Chandler_linux${DISTRIB_MODE}${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb"
+mv ${DEB_PATH}/chandler_${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb ${DISTRIB_PATH}/Chandler_linux${DISTRIB_MODE}${DISTRIB_VERSION}-${DISTRIB_RELEASE}_i386.deb
+echo "deb generation done"
