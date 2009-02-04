c9e2480a408b219cf24c1ff4d9e9f7fe4c5913e5
grant <grant@3c49585b-f0f7-0310-b5f9-dfe92a88fbfe>
2009-02-04 09:05:22 +0000

    Use the contents of README.txt as long_description to setup()
    git-svn-id: http://svn.osafoundation.org/chandler/trunk@16640 3c49585b-f0f7-0310-b5f9-dfe92a88fbfe

--- chandler/setup.py
+++ chandler/setup.py
@@ -1,44 +1,129 @@
-#!/bin/env python
-# -*- coding: utf-8 -*-
-
-#   Copyright (c) 2006-2007 Open Source Applications Foundation
+# Copyright (c) 2005 Open Source Applications Foundation.
 #
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions: 
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software. 
 #
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
 
-from setuptools import setup
+import sys
+from ez_setup import use_setuptools
+
+use_setuptools()
+
+import setuptools
+
+class TestCommand(setuptools.Command):
+    """Command to run unit tests"""
+
+    description = "run unit tests using twisted.trial"
+
+    user_options = [
+        ('suite=','s',"Test suite to run (e.g. 'some_module.test_suite')"),
+    ]
+
+    suite = None
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        self.suite = self.suite or self.distribution.test_suite
+        self.test_args = [self.suite]
+        
+    def run(self):
+
+        if self.suite:
+            cmd = ' '.join(self.test_args)
+            if self.dry_run:
+                self.announce('skipping running twisted.trial on %s" (dry run)' % cmd)
+            else:
+                self.announce('running "skipping running twisted.trial %s"' % cmd)
+                from twisted.scripts.trial import run
+                
+                sys.argv = [sys.argv[0]] + self.test_args
+
+                run()
+
+class MakeDocsCommand(setuptools.Command):
+    """Command to generate documentation"""
+
+    description = "create html documentation"
+
+    user_options = [
+        ('output-dir=','o',"Output directory for html tree"),
+    ]
+
+    output_dir = None
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        self.output_dir = self.output_dir or "doc"
+
+    def run(self):
+        if self.output_dir:
+            import epydoc.cli
+            import sys
+            import os.path
+            import glob
+            
+            srcDir = os.path.join('zanshin')
+
+            modules = [os.path.splitext(os.path.basename(path))[0]
+                       for path in glob.glob(os.path.join(srcDir, '*.py'))]
+
+            try:
+                modules.remove('__init__')
+            except ValueError:
+                pass
+                
+            modules = ['zanshin.' + path for path in modules]
+
+            sys.argv = ["epydoc.py", "--html", "--no-private", "--verbose",
+                        "--output", self.output_dir]
+            sys.argv += modules
+            
+            if self.dry_run:
+                self.announce('skipping running %s (dry run)' % (sys.argv))
+            else:
+                self.announce('running %s' % (sys.argv))
+                epydoc.cli.cli()
 
 
-setup(
-    name         = "Chandler",
-    version      = "0.1",
-    author       = "Brian Kirsch",
-    author_email = "bkirsch@osafoundation.org",
-    description  = "Default resource and localization egg for Chandler",
-    license      = "Apache License, Version 2.0",
-    test_suite   = 'unittest.TestCase',
-    include_package_data = True,
-    zip_safe             = True,
-    entry_points = {"chandler.parcels":
-                        ["Script Recording = osaf.framework.script_recording"],
-                    "chandler.quick_entry":
-                        ['event   = osaf.pim:EventCommand',
-                         'task    = osaf.pim:TaskCommand',
-                         'request = osaf.pim:RequestCommand',
-                         'invite  = osaf.pim:InviteCommand',
-                         'note    = osaf.pim:NoteCommand',
-                         'message = osaf.pim:MailCommand',
-                         'search  = osaf.views.main.Main:SearchCommand',
-                         'lucene  = osaf.views.main.Main:LuceneCommand',]
-                   }
+setuptools.setup(
+    name="zanshin",
+    version="0.6",
+    zip_safe=True,
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Environment :: Console",
+                 "Framework :: Chandler",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python",
+                 "Topic :: Software Development :: Libraries :: Python Modules"],
+    description="High-level library for HTTP, WebDAV and CalDAV operations",
+    long_description=open("README.txt").read(),
+    author="Grant Baillie",
+    author_email="grant@osafoundation.org",
+    url="http://chandlerproject.org/Projects/ZanshinProject",
+    test_suite="tests",
+    packages=["zanshin"],
+    include_package_data=True,
+    cmdclass={'test':TestCommand, 'doc':MakeDocsCommand},
 )
-
